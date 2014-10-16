f30b418bdecf18392ba3c134ebce345a066bc63d
Co0sh <coosheck@gmail.com>
2014-10-16 12:28:25 +0200

    New project BetonQuest

---
+++ BetonQuest/src/main/java/pl/betoncraft/betonquest/BetonQuest.java
@@ -0,0 +1,11 @@
+package pl.betoncraft.betonquest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Represents BetonQuest plugin
+ * @author Co0sh
+ */
+public final class BetonQuest extends JavaPlugin {
+
+}
