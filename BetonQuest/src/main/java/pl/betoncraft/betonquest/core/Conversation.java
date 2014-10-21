85f08826ddc198a9dbb5466774bd4783ac6a35da
Co0sh <coosheck@gmail.com>
2014-10-21 22:14:04 +0200

    Prevented players to start conversation if they already have one
    Signed-off-by: Co0sh <coosheck@gmail.com>

--- BetonQuest/src/main/java/pl/betoncraft/betonquest/core/Conversation.java
+++ BetonQuest/src/main/java/pl/betoncraft/betonquest/core/Conversation.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
 import pl.betoncraft.betonquest.BetonQuest;
 import pl.betoncraft.betonquest.inout.ConfigInput;
 import pl.betoncraft.betonquest.inout.ConversationListener;
+import pl.betoncraft.betonquest.inout.NPCListener;
 import pl.betoncraft.betonquest.inout.NPCLocation;
 import pl.betoncraft.betonquest.inout.SimpleTextOutput;
 
@@ -161,6 +162,8 @@ public class Conversation {
 	public void endConversation() {
 		// print message
 		SimpleTextOutput.sendSystemMessage(playerID, ConfigInput.getString("messages."+ ConfigInput.getString("config.language") +".conversation_end").replaceAll("%quester%", quester));
+		// delete conversation
+		NPCListener.removePlayerConversation(playerID);
 		// unregister listener
 		listener.unregisterListener();
 	}
--- BetonQuest/src/main/java/pl/betoncraft/betonquest/inout/NPCListener.java
+++ BetonQuest/src/main/java/pl/betoncraft/betonquest/inout/NPCListener.java
@@ -3,11 +3,15 @@
  */
 package pl.betoncraft.betonquest.inout;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import net.citizensnpcs.api.event.NPCRightClickEvent;
 
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 import pl.betoncraft.betonquest.BetonQuest;
 import pl.betoncraft.betonquest.core.Conversation;
@@ -17,15 +21,29 @@ import pl.betoncraft.betonquest.core.Conversation;
  * @author Co0sh
  */
 public class NPCListener implements Listener {
+
+	private static List<String> conversations = new ArrayList<String>();
 	
 	public NPCListener() {
 		Bukkit.getPluginManager().registerEvents(this, BetonQuest.getInstance());
 	}
 	
+	public static void removePlayerConversation(String playerID) {
+		conversations.remove(playerID);
+	}
+	
 	@EventHandler
 	public void onNPCClick(NPCRightClickEvent event) {
-		if (ConfigInput.getString("npcs." + String.valueOf(event.getNPC().getId())) != null) {
+		if (ConfigInput.getString("npcs." + String.valueOf(event.getNPC().getId())) != null && !conversations.contains(event.getClicker().getName())) {
 			new Conversation(event.getClicker().getName(), ConfigInput.getString("npcs." + String.valueOf(event.getNPC().getId())), new NPCLocation(event.getNPC().getEntity().getLocation()));
+			conversations.add(event.getClicker().getName());
 		};
+	}
+	
+	@EventHandler
+	public void onQuit(PlayerQuitEvent event) {
+		if (conversations.contains(event.getPlayer().getName())) {
+			removePlayerConversation(event.getPlayer().getName());
+		}
 	}
 }
