e5679f9f790a515b1d26227b1a8f00dbd1723b82
Dzejkop <jakubtrad@gmail.com>
2014-10-25 12:31:07 +0200

    Explosion and Lightning

---
+++ BetonQuest/src/main/java/pl/betoncraft/betonquest/events/LightningEvent.java
@@ -0,0 +1,45 @@
+package pl.betoncraft.betonquest.events;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import pl.betoncraft.betonquest.core.QuestEvent;
+
+/**
+ * Created by Dzejkop
+ */
+public class LightningEvent extends QuestEvent {
+
+    /**
+     * Spawns a lightning in a given location
+     *
+     * Takes in instructions in format: location(x;y;z;world)
+     *
+     * @param playerID
+     * @param instructions
+     */
+    public LightningEvent(String playerID, String instructions) {
+        super(playerID, instructions);
+
+        String [] instr = instructions.split(" ");
+
+        Location loc = decodeLocation(instr[1]);
+
+        (loc.getWorld()).strikeLightning(loc);
+    }
+
+    private Location decodeLocation(String locStr) {
+
+        String [] coords = locStr.split(";");
+
+        Location loc = new Location(
+                Bukkit.getWorld(coords[3]),
+                Double.parseDouble(coords[0]),
+                Double.parseDouble(coords[1]),
+                Double.parseDouble(coords[2]));
+
+        return loc;
+    }
+
+
+
+}
