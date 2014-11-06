d6ca19fb96d5dff2208c16ff32476543bba8c5f4
Co0sh <coosheck@gmail.com>
2014-11-06 07:58:17 +0100

    Added untested die objective
    Signed-off-by: Co0sh <coosheck@gmail.com>

--- BetonQuest/src/main/java/pl/betoncraft/betonquest/BetonQuest.java
+++ BetonQuest/src/main/java/pl/betoncraft/betonquest/BetonQuest.java
@@ -66,6 +66,7 @@ import pl.betoncraft.betonquest.inout.ObjectiveSaving;
 import pl.betoncraft.betonquest.inout.QuestCommand;
 import pl.betoncraft.betonquest.objectives.ActionObjective;
 import pl.betoncraft.betonquest.objectives.BlockObjective;
+import pl.betoncraft.betonquest.objectives.DieObjective;
 import pl.betoncraft.betonquest.objectives.LocationObjective;
 import pl.betoncraft.betonquest.objectives.MobKillObjective;
 
@@ -169,6 +170,7 @@ public final class BetonQuest extends JavaPlugin {
 		registerObjectives("block", BlockObjective.class);
 		registerObjectives("mobkill", MobKillObjective.class);
 		registerObjectives("action", ActionObjective.class);
+		registerObjectives("die", DieObjective.class);
 		
 		// load objectives for all online players (in case of reload)
 		for (Player player : Bukkit.getOnlinePlayers()) {
---
+++ BetonQuest/src/main/java/pl/betoncraft/betonquest/objectives/DieObjective.java
@@ -0,0 +1,70 @@
+/**
+ * 
+ */
+package pl.betoncraft.betonquest.objectives;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
+
+import pl.betoncraft.betonquest.BetonQuest;
+import pl.betoncraft.betonquest.core.Objective;
+
+/**
+ * 
+ * @author Co0sh
+ */
+public class DieObjective extends Objective implements Listener {
+	
+	private boolean cancel = false;
+
+	/**
+	 * Constructor method
+	 * @param playerID
+	 * @param instructions
+	 */
+	public DieObjective(String playerID, String instructions) {
+		super(playerID, instructions);
+		if (instructions.contains("cancel")) {
+			cancel = true;
+		}
+		Bukkit.getPluginManager().registerEvents(this, BetonQuest.getInstance());
+	}
+	
+	@EventHandler
+	public void onDeath(EntityDeathEvent event) {
+		if (cancel) {
+			return;
+		}
+		if (event.getEntity() instanceof Player && ((Player) event.getEntity()).getName().equals(playerID) && checkConditions()) {
+			HandlerList.unregisterAll(this);
+			completeObjective();
+		}
+	}
+	
+	@EventHandler
+	public void onLastDamage(EntityDamageEvent event) {
+		if (!cancel) {
+			return;
+		}
+		if (event.getEntity() instanceof Player) {
+			Player player = (Player) event.getEntity();
+			// TODO przetestowaæ ile zdrowia mo¿e mieæ gracz zanim umrze
+			if (player.getName().equals(playerID) && player.getHealth() - event.getDamage() < 0 && checkConditions()) {
+				player.setHealth(player.getMaxHealth());
+				HandlerList.unregisterAll(this);
+				completeObjective();
+			}
+		}
+	}
+
+	@Override
+	public String getInstructions() {
+		return instructions;
+	}
+
+}
