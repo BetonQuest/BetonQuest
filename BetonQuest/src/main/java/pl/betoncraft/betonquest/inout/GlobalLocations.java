cfed2a53cce82129ce01978f7ccb7d972d1ad81d
Co0sh <coosheck@gmail.com>
2014-11-24 14:42:59 +0100

    Global locations now automatically run only once

--- BetonQuest/src/main/java/pl/betoncraft/betonquest/inout/GlobalLocations.java
+++ BetonQuest/src/main/java/pl/betoncraft/betonquest/inout/GlobalLocations.java
@@ -38,31 +38,39 @@ public class GlobalLocations extends BukkitRunnable{
 	
 	@Override
 	public void run() {
+		// do nothing if there is no defined locations
 		if (finalLocations == null) {
 			this.cancel();
 			return;
 		}
+		// loop all online players
 		Player[] players = Bukkit.getOnlinePlayers();
 		for (Player player : players) {
+			// for each player loop all available locations
 			locations:
 			for (GlobalLocation location : finalLocations) {
+				// if location is not set, stop everything, there is an error in config
 				if (location.getLocation() == null) {
-					this.cancel();
-					return;
+					continue locations;
 				}
-				
-				if (player.getLocation().getWorld().equals(location.getLocation().getWorld()) && player.getLocation().distance(new Location(Bukkit.getWorld(location.getLocation().getWorld()), location.getLocation().getX(), location.getLocation().getY(), location.getLocation().getZ())) < location.getDistance()) {
-					if (BetonQuest.condition(player.getName(), "tag tag:global_" + location.getTag())) {
+				// if player is inside location, do stuff
+				if (player.getLocation().getWorld().getName().equals(location.getLocation().getWorld()) && player.getLocation().distance(new Location(Bukkit.getWorld(location.getLocation().getWorld()), location.getLocation().getX(), location.getLocation().getY(), location.getLocation().getZ())) < location.getDistance()) {
+					// check if player has already triggered this location
+					if (BetonQuest.getInstance().havePlayerTag(player.getName(), "global_" + location.getTag())) {
 						continue locations;
 					}
+					// check all conditions
 					if (location.getConditions() != null) {
 						for (String condition : location.getConditions()) {
 							if (!BetonQuest.condition(player.getName(), condition)) {
+								// if some conditions are not met, skip to next location
 								continue locations;
 							}
 						}
 					}
-					BetonQuest.event(player.getName(), "tag add global_" + location.getTag());
+					// set the tag, player has triggered this location
+					BetonQuest.getInstance().putPlayerTag(player.getName(), "global_" + location.getTag());
+					// fire all events for the location
 					for (String event : location.getEvents()) {
 						BetonQuest.event(player.getName(), event);
 					}
