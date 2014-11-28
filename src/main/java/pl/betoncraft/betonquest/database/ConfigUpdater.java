3c6bd96ba25c3fd7ff2c0c52b63a6a504a93ba34
Co0sh <coosheck@gmail.com>
2014-11-28 23:59:19 +0100

    Added messages updater, not tested

--- src/main/java/pl/betoncraft/betonquest/database/ConfigUpdater.java
+++ src/main/java/pl/betoncraft/betonquest/database/ConfigUpdater.java
@@ -2,7 +2,6 @@ package pl.betoncraft.betonquest.database;
 
 import java.util.Set;
 
-import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 
 import pl.betoncraft.betonquest.BetonQuest;
@@ -13,9 +12,6 @@ import pl.betoncraft.betonquest.inout.ConfigInput;
  * @author co0sh
  */
 public class ConfigUpdater {
-	
-	private String[] knownVersionsUUID = new String[]{"", ""};
-	private String[] knownVersionsName = new String[]{"", ""};
 
 	public ConfigUpdater() {
 		FileConfiguration config = BetonQuest.getInstance().getConfig();
@@ -23,57 +19,54 @@ public class ConfigUpdater {
 		// if the version is null the plugin is updated from pre-1.3 version (which can be 1.0, 1.1 or 1.2)
 		if (version == null) {
 			BetonQuest.getInstance().getLogger().info("Started converting configuration files from unknown version to v1.3!");
-			config.set("version", "1.3");
-			String bukkitVersion = Bukkit.getServer().getVersion();
-			boolean set = false;
-			// if the version supports UUID then we automatically convert names to UUIDs
-			for (String knownVersion : knownVersionsUUID) {
-				if (bukkitVersion.equals(knownVersion)) {
-					BetonQuest.getInstance().getLogger().info("Found version " + version + ", using UUID by default.");
-					config.set("uuid", "true");
-					convertNamesToUUID();
-					set = true;
-				}
-			}
-			// if the version does not support UUID then we set UUID to false and do nothing
-			for (String knownVersion : knownVersionsName) {
-				if (bukkitVersion.equals(knownVersion)) {
-					BetonQuest.getInstance().getLogger().info("Found version " + version + ", using Names by default.");
-					config.set("uuid", "false");
-					set = true;
-				}
-			}
-			// if we didn't recognize the version we will allow user to decide which one he wants to use.
-			if (!set) {
-				BetonQuest.getInstance().getLogger().info("Didn't recognize the version, using Names by for safety. If you run UUID compatible server and want to use UUID change it manually in the config file and reload the plugin.");
-				config.set("uuid", "false");
-				// this will alert the plugin that the conversion should be done if UUID is set to true
-				config.set("convert", "true");
-				set = true;
-			}
+			// add conversion options
+			BetonQuest.getInstance().getLogger().info("Using Names by for safety. If you run UUID compatible server and want to use UUID, change it manually in the config file and reload the plugin.");
+			config.set("uuid", "false");
+			// this will alert the plugin that the conversion should be done if UUID is set to true
+			config.set("convert", "true");
 			// add metrics if they are not set yet
 			if (!config.isSet("metrics")) {
+				BetonQuest.getInstance().getLogger().info("Added metrics option.");
 				config.set("metrics", "true");
 			}
 			// add stop to conversation if not done already
+			BetonQuest.getInstance().getLogger().info("Adding stop nodes to conversations...");
+			int count = 0;
 			ConfigAccessor conversations = ConfigInput.getConfigs().get("conversations");
 			Set<String> convNodes = conversations.getConfig().getKeys(false);
 			for (String convNode : convNodes) {
 				if (!conversations.getConfig().isSet(convNode + ".stop")) {
 					conversations.getConfig().set(convNode + ".stop", "false");
+					count++;
 				}
 			}
-			// TODO add new languages
-//			ConfigAccessor messages = ConfigInput.getConfigs().get("messages");
-//			if (!messages.getConfig().isSet("de")) {
-//				messages.getConfig().createSection("de", messages.getConfig().getDefaultSection().getConfigurationSection("de").getValues(false));
-//			}
-//			if (!messages.getConfig().isSet("fr")) {
-//				messages.getConfig().createSection("fr", messages.getConfig().getDefaultSection().getConfigurationSection("fr").getValues(false));
-//			}
+			BetonQuest.getInstance().getLogger().info("Done, modified " + count + " conversations!");
+			// add new languages
+			ConfigAccessor messages = ConfigInput.getConfigs().get("messages");
+			for (String path : messages.getConfig().getDefaultSection().getKeys(false)) {
+				BetonQuest.getInstance().getLogger().info("found "+path);
+				if (messages.getConfig().isSet(path)) {
+					BetonQuest.getInstance().getLogger().info("entered "+path);
+					for (String messageNode : messages.getConfig().getDefaults().getConfigurationSection(path).getKeys(false)) {
+						BetonQuest.getInstance().getLogger().info("found "+messageNode);
+						if (!messages.getConfig().isSet(path + "." + messageNode)) {
+							BetonQuest.getInstance().getLogger().info("entered "+messageNode);
+							messages.getConfig().set(path + "." + messageNode, messages.getConfig().getDefaults().get(path + "." + messageNode));
+						}
+					}
+				} else {
+					for (String messageNode : messages.getConfig().getDefaults().getConfigurationSection(path).getKeys(false)) {
+						BetonQuest.getInstance().getLogger().info("set "+messageNode);
+						messages.getConfig().set(path + "." + messageNode, messages.getConfig().getDefaults().get(path + "." + messageNode));
+					}
+				}
+			}
+			messages.saveConfig();
+			BetonQuest.getInstance().getLogger().info("Updated language files!");
 			// end of updating to 1.3
+			config.set("version", "1.3");
 			conversations.saveConfig();
-			BetonQuest.getInstance().getLogger().info("Conversion to v1.3 finished. You're ready to go!");
+			BetonQuest.getInstance().getLogger().info("Conversion to v1.3 finished.");
 			// start update recursively for next versions
 			new ConfigUpdater();
 		} else if (version.equals("1.3")) {
