2f03ac5949176c2ddbf677a291597b1c64160007
Co0sh <coosheck@gmail.com>
2014-11-29 23:32:00 +0100

    Added permissions to creating NPCs from blocks
    Changed NPC's sign syntax a little bit
    
    Signed-off-by: Co0sh <coosheck@gmail.com>

--- src/main/java/pl/betoncraft/betonquest/inout/CubePeopleListener.java
+++ src/main/java/pl/betoncraft/betonquest/inout/CubePeopleListener.java
@@ -10,8 +10,11 @@ import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.block.Sign;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.block.SignChangeEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
@@ -35,22 +38,30 @@ public class CubePeopleListener implements Listener {
 	}
 	
 	@EventHandler
+	public void onSignPlace(SignChangeEvent event) {
+		if (event.getLine(0).equalsIgnoreCase("[NPC]") && !event.getPlayer().hasPermission("betonquest.createnpc")) {
+			event.setCancelled(true);
+			event.getPlayer().sendMessage(ConfigInput.getString("messages." + ConfigInput.getString("config.language") + ".no_permission").replaceAll("&", "§"));
+		}
+	}
+	
+	@EventHandler
 	public void onNPCClick(PlayerInteractEvent event) {
 		String conversationID = null;
-		if (event.getClickedBlock().getType().equals(Material.SKULL)) {
+		if (event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && event.getClickedBlock().getType().equals(Material.SKULL)) {
 			Block block = event.getClickedBlock().getLocation().clone().add(0, -1, 0).getBlock();
 			if (block.getType().equals(Material.STAINED_CLAY)) {
 				Block[] signs = new Block[]{block.getRelative(BlockFace.EAST),block.getRelative(BlockFace.WEST),block.getRelative(BlockFace.NORTH),block.getRelative(BlockFace.SOUTH)};
-				org.bukkit.block.Sign theSign = null;
+				Sign theSign = null;
 				byte count = 0;
 				for (Block sign : signs) {
-					if (sign.getType().equals(Material.WALL_SIGN) && sign.getState() instanceof org.bukkit.block.Sign) {
-						theSign = (org.bukkit.block.Sign) sign.getState();
+					if (sign.getType().equals(Material.WALL_SIGN) && sign.getState() instanceof Sign) {
+						theSign = (Sign) sign.getState();
 						count++;
 					}
 				}
-				if (count == 1 && theSign != null) {
-					conversationID = theSign.getLines()[1];
+				if (count == 1 && theSign != null && theSign.getLine(0).equalsIgnoreCase("[NPC]")) {
+					conversationID = theSign.getLine(1);
 				}
 			}
 			
