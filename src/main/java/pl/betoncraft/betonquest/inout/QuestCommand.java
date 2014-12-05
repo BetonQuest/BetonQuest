13711656f78fb01604785b80f3d3f5d231b1aa35
Co0sh <coosheck@gmail.com>
2014-12-05 12:01:47 +0100

    Added /q item command for saving items to config
    Fixed bug with conversion after clean install
    
    Signed-off-by: Co0sh <coosheck@gmail.com>

--- src/main/java/pl/betoncraft/betonquest/inout/QuestCommand.java
+++ src/main/java/pl/betoncraft/betonquest/inout/QuestCommand.java
@@ -9,9 +9,16 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BookMeta;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.PotionMeta;
+import org.bukkit.potion.PotionEffect;
 
 import pl.betoncraft.betonquest.BetonQuest;
+import pl.betoncraft.betonquest.database.ConfigAccessor;
 
 /**
  * 
@@ -28,13 +35,14 @@ public class QuestCommand implements CommandExecutor {
 		if (cmd.getName().equalsIgnoreCase("q")) {
 			if (args.length < 1) {
 				sender.sendMessage("§e----- §aBetonQuest §e-----");
-				sender.sendMessage("§c/q reload §b- reload the plugin");
-				sender.sendMessage("§c/q objectives §b- display your objectives");
-				sender.sendMessage("§c/q tags §b- display your tags");
-				sender.sendMessage("§c/q points §b- display your points");
-				sender.sendMessage("§c/q condition <id> §b- check if you meet condition");
-				sender.sendMessage("§c/q event <id> §b- fires an event");
-				sender.sendMessage("§c/q purge <player> §b- reset all player's data");
+				sender.sendMessage("§c/q reload §b- " + getMessage("command_reload"));
+				sender.sendMessage("§c/q objectives §b- " + getMessage("command_objectives"));
+				sender.sendMessage("§c/q tags §b- " + getMessage("command_tags"));
+				sender.sendMessage("§c/q points §b- " + getMessage("command_tags"));
+				sender.sendMessage("§c/q condition <id> §b- " + getMessage("command_condition"));
+				sender.sendMessage("§c/q event <id> §b- " + getMessage("command_event"));
+				sender.sendMessage("§c/q item <id> §b- " + getMessage("command_event"));
+				sender.sendMessage("§c/q purge <player> §b- " + getMessage("command_purge"));
 				return true;
 			}
 			switch (args[0]) {
@@ -86,6 +94,84 @@ public class QuestCommand implements CommandExecutor {
 				}
 				BetonQuest.event(PlayerConverter.getID((Player) sender), args[1]);
 				sender.sendMessage(getMessage("player_event").replaceAll("%event%", ConfigInput.getString("events." + args[1])));
+				break;
+			case "item":
+				if (!(sender instanceof Player)) {
+					return true;
+				}
+				if (args.length < 2) {
+					sender.sendMessage(getMessage("specify_item"));
+					return true;
+				}
+				Player player = (Player) sender;
+				ItemStack item = player.getItemInHand();
+				if (item == null) {
+					sender.sendMessage(getMessage("no_item"));
+					return true;
+				}
+				ConfigAccessor config = ConfigInput.getConfigs().get("items");
+				String name = "";
+				String lore = "";
+				String enchants = "";
+				String title = "";
+				String text = "";
+				String author = "";
+				String effects = "";
+				
+				// TODO item creation
+				ItemMeta meta = item.getItemMeta();
+				if (meta.hasDisplayName()) {
+					name = " name:" +meta.getDisplayName().replace(" ", "_");
+				}
+				if (meta.hasLore()) {
+					StringBuilder string = new StringBuilder();
+					for (String line : meta.getLore()) {
+						string.append(line + ";");
+					}
+					lore = " lore:" + string.substring(0, string.length()-1).replace(" ", "_");
+				}
+				if (meta.hasEnchants()) {
+					StringBuilder string = new StringBuilder();
+					for (Enchantment enchant : meta.getEnchants().keySet()) {
+						string.append(enchant.getName() + ":" + meta.getEnchants().get(enchant) + ",");
+					}
+					enchants = " enchants:" + string.substring(0, string.length()-1);
+				}
+				if (meta instanceof BookMeta) {
+					BookMeta bookMeta = (BookMeta) meta;
+					if (bookMeta.hasAuthor()) {
+						author = " author:" + bookMeta.getAuthor().replace(" ", "_");
+					}
+					if (bookMeta.hasTitle()) {
+						title = " title:" + bookMeta.getTitle().replace(" ", "_");
+					}
+					if (bookMeta.hasPages()) {
+						StringBuilder string = new StringBuilder();
+						BetonQuest.getInstance().getLogger().info(bookMeta.getPages().toString());
+						for (String page : bookMeta.getPages()) {
+							string.append(page.trim().replace(" ", "_") + "_");
+						}
+						text = " text:" + string.substring(0, string.length()-1);
+					}
+				}
+				if (meta instanceof PotionMeta) {
+					PotionMeta potionMeta = (PotionMeta) meta;
+					if (potionMeta.hasCustomEffects()) {
+						StringBuilder string = new StringBuilder();
+						for (PotionEffect effect : potionMeta.getCustomEffects()) {
+							int power = effect.getAmplifier() + 1;
+							int duration = (effect.getDuration() - (effect.getDuration() % 20)) / 20;
+							string.append(effect.getType().getName() + ":" + power + ":" + duration + ",");
+						}
+						effects = " effects:" + string.substring(0, string.length()-1);
+					}
+				}
+				
+				@SuppressWarnings("deprecation")
+				String instructions = item.getType() + " data:" + item.getData().getData() + name + lore + enchants + title + author + text + effects;
+				config.getConfig().set(args[1], instructions.trim());
+				config.saveConfig();
+				sender.sendMessage(getMessage("item_created").replace("%item%", args[1]));
 				break;
 			case "points":
 				if (!(sender instanceof Player)) {
