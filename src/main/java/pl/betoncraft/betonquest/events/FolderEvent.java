8af846e784c1a92c1523ae7eb26590ce0a261f53
Co0sh <coosheck@gmail.com>
2015-04-12 16:53:09 +0200

    Add "random" argument to "folder" event
    Close #98
    Fix the issue with special characters on some servers
    Change version to 1.7-dev
    Set default Maven encoding to UTF-8
    
    Signed-off-by: Jakub Sapalski <coosheck@gmail.com>

--- src/main/java/pl/betoncraft/betonquest/events/FolderEvent.java
+++ src/main/java/pl/betoncraft/betonquest/events/FolderEvent.java
@@ -17,32 +17,78 @@
  */
 package pl.betoncraft.betonquest.events;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 import org.bukkit.scheduler.BukkitRunnable;
 
 import pl.betoncraft.betonquest.BetonQuest;
 import pl.betoncraft.betonquest.api.QuestEvent;
 import pl.betoncraft.betonquest.utils.Debug;
 
+/**
+ * Folder event is a collection of other events, that can be run after
+ * a delay and the events can be randomly choosen to run or not
+ * 
+ * @author Coosh
+ */
 public class FolderEvent extends QuestEvent {
 
     public FolderEvent(String playerID, String instructions) {
         super(playerID, instructions);
+        // declare variables used later
         String[] parts = instructions.split(" ");
         String[] events = null;
         int delay = 0;
+        int random = 0;
+        // parse the instuction string
         for (String part : parts) {
             if (part.contains("events:")) {
                 events = part.substring(7).split(",");
             }
             if (part.contains("delay:")) {
-                delay = Integer.parseInt(part.substring(6));
+                try {
+                    delay = Integer.parseInt(part.substring(6));
+                } catch (NumberFormatException e) {
+                    // if the delay is incorrect, there is an error
+                    Debug.error("Wrong number format in folder event! " + instructions);
+                    return;
+                }
+            }
+            if (part.contains("random:")) {
+                try {
+                    random = Integer.parseInt(part.substring(7));
+                } catch (NumberFormatException e) {
+                    // if the random number is incorrect, there is an error
+                    Debug.error("Wrong number format in folder event! " + instructions);
+                    return;
+                }
             }
         }
+        // if there are no events, there is an error
         if (events == null) {
             BetonQuest.getInstance().getLogger()
                     .severe("Error in folder event: events not defined! " + instructions);
             return;
         }
+        // choose randomly which events should be fired
+        if (random > 0 && random <= events.length) {
+            // copy events into the modifyable ArrayList
+            ArrayList<String> eventsList = new ArrayList<>();
+            for (String event : events) {
+                eventsList.add(event);
+            }
+            // remove choosen events from that ArrayList and place them in a new list
+            ArrayList<String> chosenList = new ArrayList<>();
+            for (int i = random; i > 0; i--) {
+                int chosen = new Random().nextInt(eventsList.size());
+                chosenList.add(eventsList.remove(chosen));
+            }
+            // convert that new list into the array and replace "events" with it
+            events = new String[chosenList.size()];
+            events = chosenList.toArray(events);
+        }
+        // execute events after the delay
         final String[] finalEvents = events;
         final String player = playerID;
         new BukkitRunnable() {
@@ -53,7 +99,7 @@ public class FolderEvent extends QuestEvent {
                     BetonQuest.event(player, event);
                 }
             }
-        }.runTaskLater(BetonQuest.getInstance(), delay * 20);
+        }.runTaskLater(BetonQuest.getInstance(), delay * 20); // 20 ticks is a second
     }
 
 }
