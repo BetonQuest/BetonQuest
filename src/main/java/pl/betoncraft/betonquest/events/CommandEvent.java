272f3cf8bd47d6938891c519b2ed7796255ecf71
Jakub Sapalski <coosheck@gmail.com>
2016-06-18 10:07:20 +0200

    Fix #357 - Make "command" event resolve all variables

--- src/main/java/pl/betoncraft/betonquest/events/CommandEvent.java
+++ src/main/java/pl/betoncraft/betonquest/events/CommandEvent.java
@@ -17,28 +17,35 @@
  */
 package pl.betoncraft.betonquest.events;
 
+import java.util.List;
+
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
+import pl.betoncraft.betonquest.BetonQuest;
 import pl.betoncraft.betonquest.InstructionParseException;
 import pl.betoncraft.betonquest.api.QuestEvent;
 import pl.betoncraft.betonquest.utils.PlayerConverter;
 
 /**
- * Fires a list of commands for the player
+ * Fires a list of commands for the player.
  * 
  * @author Jakub Sapalski
  */
 public class CommandEvent extends QuestEvent {
 
-	private final String[] commands;
+	private final Command[] commands;
 
 	public CommandEvent(String packName, String instructions) throws InstructionParseException {
 		super(packName, instructions);
 		staticness = true;
 		persistent = true;
 		try {
-			commands = instructions.trim().substring(instructions.indexOf(" ") + 1).split("\\|");
+			String[] rawCommands = instructions.trim().substring(instructions.indexOf(" ") + 1).split("\\|");
+			commands = new Command[rawCommands.length];
+			for (int i = 0; i < rawCommands.length; i++) {
+				commands[i] = new Command(rawCommands[i]);
+			}
 		} catch (Exception e) {
 			throw new InstructionParseException("Could not parse commands");
 		}
@@ -46,20 +53,50 @@ public class CommandEvent extends QuestEvent {
 
 	@Override
 	public void run(String playerID) {
-		for (String command : commands) {
-			if (playerID == null) {
-				if (command.contains("%player%")) {
+		for (Command command : commands) {
+			if (command.variables.isEmpty()) {
+				// if there are no variables, this is a global command
+				Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), command.command);
+			} else {
+				if (playerID == null) {
+					// this is a static command, run for each player 
 					for (Player player : Bukkit.getOnlinePlayers()) {
-						Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(),
-								command.replaceAll("%player%", player.getName()));
+						String com = command.command;
+						for (String var : command.variables) {
+							com = com.replace(var, BetonQuest.getInstance().getVariableValue(
+									pack.getName(), var, PlayerConverter.getID(player)));
+						}
+						Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), com);
 					}
 				} else {
-					Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), command);
+					Player player = PlayerConverter.getPlayer(playerID);
+					if (player == null) {
+						// the player is offline, cannot resolve variables, at least replace %player%
+						Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), command.command
+								.replaceAll("%player%", PlayerConverter.getName(playerID)));
+					} else {
+						// run the command for the single player
+						String com = command.command;
+						for (String var : command.variables) {
+							com = com.replace(var, BetonQuest.getInstance().getVariableValue(
+									pack.getName(), var, playerID));
+						}
+						Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), com);
+					}
 				}
-			} else {
-				Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(),
-						command.replaceAll("%player%", PlayerConverter.getName(playerID)));
 			}
 		}
+	}
+
+	private class Command {
+
+		String command;
+		List<String> variables;
+
+		public Command(String command) {
+			this.command = command;
+			variables = BetonQuest.resolveVariables(command);
+		}
+
 	}
 }
