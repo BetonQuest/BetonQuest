a07563d915b164c1818570a7c0c6fff93668df48
joblo.2213 <joblo.2213@gmail.com>
2017-10-30 22:33:40 +0100

    Fixed documentation and file header

--- src/main/java/pl/betoncraft/betonquest/commands/SimpleTabCompleter.java
+++ src/main/java/pl/betoncraft/betonquest/commands/SimpleTabCompleter.java
@@ -1,3 +1,20 @@
+/**
+ * BetonQuest - advanced quests for Bukkit
+ * Copyright (C) 2016  Jakub "Co0sh" Sapalski
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package pl.betoncraft.betonquest.commands;
 
 import org.bukkit.command.Command;
@@ -8,10 +25,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Licensed under GNU GENERAL PUBLIC LICENSE Version 3
- * https://gitlab.com/ungefroren/SchemGui/blob/master/LICENSE
- * <p>
- * Created by Jonas on 10.10.2017.
+ * Interface which handles tab complete for commands.
+ *
+ * @author Jonas Blocher
  */
 public interface SimpleTabCompleter extends TabCompleter {
 
