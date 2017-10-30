fec764deded2db80dab69f7e74d4cad42063a482
joblo.2213 <joblo.2213@gmail.com>
2017-10-30 20:20:18 +0100

    Add tab complete for quest command

---
+++ src/main/java/pl/betoncraft/betonquest/commands/SimpleTabCompleter.java
@@ -0,0 +1,34 @@
+package pl.betoncraft.betonquest.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Licensed under GNU GENERAL PUBLIC LICENSE Version 3
+ * https://gitlab.com/ungefroren/SchemGui/blob/master/LICENSE
+ * <p>
+ * Created by Jonas on 10.10.2017.
+ */
+public interface SimpleTabCompleter extends TabCompleter {
+
+
+    @Override
+    default List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        List<String> completations = this.simpleTabComplete(sender, command, alias, args);
+        if (completations == null) return null;
+        List<String> out = new ArrayList<>();
+        String lastArg = args[args.length - 1];
+        for (String completation : completations) {
+            if (lastArg == null || lastArg.matches(" *") || completation.startsWith(lastArg)) {
+                out.add(completation);
+            }
+        }
+        return out;
+    }
+
+    List<String> simpleTabComplete(CommandSender sender, Command command, String alias, String[] args);
+}
