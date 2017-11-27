18d1389f08bcca0abcce3bc06ce1b2796939ae06
joblo.2213 <joblo.2213@gmail.com>
2017-11-27 23:09:32 +0100

    Add DayOfWeekCondition - fix issues with java.util.Calendar

--- src/main/java/pl/betoncraft/betonquest/conditions/DayOfWeekCondition.java
+++ src/main/java/pl/betoncraft/betonquest/conditions/DayOfWeekCondition.java
@@ -1,17 +1,17 @@
 /**
  * BetonQuest - advanced quests for Bukkit
  * Copyright (C) 2016  Jakub "Co0sh" Sapalski
- *
+ * <p>
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * <p>
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -55,6 +55,9 @@ public class DayOfWeekCondition extends Condition {
     @Override
     public boolean check(String playerID) throws QuestRuntimeException {
         Calendar calendar = Calendar.getInstance();
-        return calendar.get(Calendar.DAY_OF_WEEK) == day.getValue();
+        int day = calendar.get(Calendar.DAY_OF_WEEK);
+        day = day == 1 ? 7: day - 1;
+        //As calendar.get(Calendar.DAY_OF_WEEK) returns 1 on sunday, 2 oin monday, and so on this has to be fixed
+        return day == this.day.getValue();
     }
 }
