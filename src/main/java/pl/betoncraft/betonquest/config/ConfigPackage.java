BetonQuest/BetonQuest

commit 4fadb3b89787126780f30be61c244ad675fa7ee7

Fix #792 - Now $ or % should display correctly in all texts

src/main/java/pl/betoncraft/betonquest/config/ConfigPackage.java
@@ -20,6 +20,8 @@ package pl.betoncraft.betonquest.config;
 import java.io.File;
 import java.util.HashMap;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import pl.betoncraft.betonquest.config.ConfigAccessor.AccessorType;
 import pl.betoncraft.betonquest.utils.Debug;
@@ -165,13 +167,11 @@ public class ConfigPackage {
 		// handle "$this$" variables
 		value = value.replace("$this$", name);
 		// handle the rest
+		Pattern global_variable_regex = Pattern.compile("\\$[^ $\\s]\\$");
 		while (true) {
-			int start = value.indexOf('$');
-			int end = value.indexOf('$', start + 1);
-			if (start == -1 || end == -1) {
-				break;
-			}
-			String varName = value.substring(start + 1, end);
+			Matcher matcher = global_variable_regex.matcher(value);
+			if (!matcher.find()) break;
+			String varName = matcher.group();
 			String varVal = main.getConfig().getString("variables." + varName);
 			if (varVal == null) {
 				Debug.error(String.format("Variable %s not defined in package %s", varName, name));

src/main/java/pl/betoncraft/betonquest/BetonQuest.java
@@ -23,6 +23,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map.Entry;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.Logger;
@@ -1084,32 +1086,10 @@ public final class BetonQuest extends JavaPlugin {
 	 */
 	public static ArrayList<String> resolveVariables(String text) {
 		ArrayList<String> variables = new ArrayList<>();
-		boolean inside = false;
-		char[] charArr = text.toCharArray();
-		StringBuilder variable = new StringBuilder();
-		for (int i = 0; i < text.length(); i++) {
-			if (inside) {
-				if (charArr[i] == ' ') {
-					// it's not a variable if it contains a space
-					inside = false;
-					variable = new StringBuilder();
-				}
-				variable.append(charArr[i]);
-				if (charArr[i] == '%') {
-					// end of the variable
-					inside = false;
-					String finalVariable = variable.toString();
-					variable = new StringBuilder();
-					if (!variables.contains(finalVariable)) {
-						variables.add(finalVariable);
-					}
-				}
-			} else {
-				if (charArr[i] == '%') {
-					inside = true;
-					variable.append('%');
-				}
-			}
+		Matcher matcher = Pattern.compile("%[^ %\\s]+%").matcher(text);
+		while (matcher.find()) {
+			final String variable = matcher.group();
+			if (!variables.contains(variable)) variables.add(variable);
 		}
 		return variables;
 	}

commit b00eb5273ba412aa927e671074d1ee4f519410d4

Fix issues with global variables caused by commit 4fadb3b

src/main/java/pl/betoncraft/betonquest/config/ConfigPackage.java
@@ -167,11 +167,11 @@ public class ConfigPackage {
 		// handle "$this$" variables
 		value = value.replace("$this$", name);
 		// handle the rest
-		Pattern global_variable_regex = Pattern.compile("\\$[^ $\\s]\\$");
+		Pattern global_variable_regex = Pattern.compile("\\$([^ $\\s]+)\\$");
 		while (true) {
 			Matcher matcher = global_variable_regex.matcher(value);
 			if (!matcher.find()) break;
-			String varName = matcher.group();
+			String varName = matcher.group(1);
 			String varVal = main.getConfig().getString("variables." + varName);
 			if (varVal == null) {
 				Debug.error(String.format("Variable %s not defined in package %s", varName, name));