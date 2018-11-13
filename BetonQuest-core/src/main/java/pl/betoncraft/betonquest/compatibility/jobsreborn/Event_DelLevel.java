a2be2c4552bb3df0b2ebaf15a9ade2159e95f83d
Brendan Grieve <brendan.grieve@zepli.com.au>
2018-11-13 14:55:05 +0800

    Integrate JobsReborn per request of Sir_Nutty
    Changes:
      * Added to compatibility
      * Updated Documenation
      * Update for MultiVersion plugin v1.1
      * Update Changelog

---
+++ BetonQuest-core/src/main/java/pl/betoncraft/betonquest/compatibility/jobsreborn/Event_DelLevel.java
@@ -0,0 +1,70 @@
+/*
+ * BetonQuest - advanced quests for Bukkit
+ * Copyright (C) 2016  Jakub "Co0sh" Sapalski
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package pl.betoncraft.betonquest.compatibility.jobsreborn;
+
+import com.gamingmesh.jobs.Jobs;
+import com.gamingmesh.jobs.container.Job;
+import com.gamingmesh.jobs.container.JobProgression;
+import org.bukkit.entity.Player;
+import pl.betoncraft.betonquest.Instruction;
+import pl.betoncraft.betonquest.InstructionParseException;
+import pl.betoncraft.betonquest.api.QuestEvent;
+import pl.betoncraft.betonquest.utils.PlayerConverter;
+
+import java.util.List;
+
+public class Event_DelLevel extends QuestEvent {
+    private String sJobName;
+    private Integer nAddLevel;
+
+    public Event_DelLevel(Instruction instructions) throws InstructionParseException {
+        super(instructions);
+
+        if (instructions.size() < 3) {
+            throw new InstructionParseException("Not enough arguments");
+        }
+        for (Job job : Jobs.getJobs()) {
+            if (job.getName().equalsIgnoreCase(instructions.getPart(1))) {
+                sJobName = job.getName();
+                try {
+                    this.nAddLevel = Integer.parseInt(instructions.getPart(2));
+                } catch (Exception err) {
+                    throw new InstructionParseException("NUJobs_DelLevel: Unable to parse the level amount");
+                }
+                return;
+            }
+        }
+        throw new InstructionParseException("Jobs Reborn job " + instructions.getPart(1) + " does not exist");
+    }
+
+    @Override
+    public void run(String playerID) {
+        Player oPlayer = PlayerConverter.getPlayer(playerID);
+
+        List<JobProgression> oJobs = Jobs.getPlayerManager().getJobsPlayer(oPlayer).getJobProgression();
+        for (JobProgression oJob : oJobs) {
+            if (oJob.getJob().getName().equalsIgnoreCase(sJobName)) {
+                //User has the job, return true
+                oJob.setLevel(oJob.getLevel() - this.nAddLevel);
+                if (oJob.getLevel() < 1)
+                    oJob.setLevel(1);
+            }
+        }
+    }
+}
