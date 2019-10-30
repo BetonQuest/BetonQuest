9e3783a28e5f82caa2687917c9e51f16cabf0b87
Brendan Grieve <brendan.grieve@zepli.com.au>
2019-10-30 20:36:00 +0800

    Update discord notify script to add Credits and tidy up description

--- discord.sh
+++ discord.sh
@@ -6,6 +6,17 @@ if [ $TRAVIS_PULL_REQUEST == "false" ] && [ "$TRAVIS_BRANCH" == "master" ] ; the
 
     WEBHOOK_URL="https://discordapp.com/api/webhooks/$DISCORD_WEBHOOK_ID/$DISCORD_WEBHOOK_TOKEN"
 
+    AUTHOR_NAME="$(git log -1 "$TRAVIS_COMMIT" --pretty="%aN")"
+    COMMITTER_NAME="$(git log -1 "$TRAVIS_COMMIT" --pretty="%cN")"
+    COMMIT_SUBJECT="$(git log -1 "$TRAVIS_COMMIT" --pretty="%s")"
+    COMMIT_MESSAGE="$(git log -1 "$TRAVIS_COMMIT" --pretty="%b")" | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g'
+
+    if [ "$AUTHOR_NAME" == "$COMMITTER_NAME" ]; then
+      CREDITS="$AUTHOR_NAME authored & committed"
+    else
+      CREDITS="$AUTHOR_NAME authored & $COMMITTER_NAME committed"
+    fi
+
     TIMESTAMP=$(date --utc +%FT%TZ)
 
     WEBHOOK_DATA='{
@@ -17,9 +28,21 @@ if [ $TRAVIS_PULL_REQUEST == "false" ] && [ "$TRAVIS_BRANCH" == "master" ] ; the
           "name": "Dev build #'"$TRAVIS_BUILD_NUMBER"' is now available",
           "url": "https://betonquest.pl/"
         },
-        "title": "Commit '"${TRAVIS_COMMIT:0:7}"'",
+        "title": "'"$COMMIT_SUBJECT"'",
         "url": "https://github.com/'"$TRAVIS_REPO_SLUG"'/commit/'"$TRAVIS_COMMIT"'",
-        "description": "'"${TRAVIS_COMMIT_MESSAGE//$'\n'/'\\n'}"'",
+        "description": "'"${COMMIT_MESSAGE//$'\n'/ }"\\n\\n"$CREDITS"'",
+        "fields": [
+          {
+            "name": "Commit",
+            "value": "'"[\`${TRAVIS_COMMIT:0:7}\`](https://github.com/$TRAVIS_REPO_SLUG/commit/$TRAVIS_COMMIT)"'",
+            "inline": true
+          },
+          {
+            "name": "Branch",
+            "value": "'"[\`$TRAVIS_BRANCH\`](https://github.com/$TRAVIS_REPO_SLUG/tree/$TRAVIS_BRANCH)"'",
+            "inline": true
+          }
+        ],
         "thumbnail": {
           "url": "https://cdn.discordapp.com/app-icons/494162764403572748/d7680ea0dfa2d1da1a95676435f526c1.png?size=64"
         },
@@ -27,7 +50,7 @@ if [ $TRAVIS_PULL_REQUEST == "false" ] && [ "$TRAVIS_BRANCH" == "master" ] ; the
         } ]
     }'
 
-    (curl --fail --progress-bar -A "TravisCI-Webhook" -H Content-Type:application/json -H X-Author:ungefroren#7524 -d "$WEBHOOK_DATA" "$WEBHOOK_URL" \
-      && echo -e "\\n[Webhook]: Successfully sent the webhook.") || echo -e "\\n[Webhook]: Unable to send webhook:\\n\\nWEBHHOK_DATA:\\n$WEBHHOK_DATA"
+    (curl --fail --progress-bar -A "TravisCI-Webhook" -H Content-Type:application/json -H X-Author:ungefroren#7524 -d "${WEBHOOK_DATA//	/ }" "$WEBHOOK_URL" \
+      && echo -e "\\n[Webhook]: Successfully sent the webhook.") || echo -e "\\n[Webhook]: Unable to send webhook:\\n\\nWEBHHOK_DATA:\\n$WEBHOOK_DATA"
 
 fi
