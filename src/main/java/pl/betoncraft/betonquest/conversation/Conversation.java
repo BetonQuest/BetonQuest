BetonQuest/BetonQuest

commit cb0a8d52c2f85987ff1e3d4969234f67dab940bb

added PMD rule ShortVariable

src/main/java/pl/betoncraft/betonquest/conversation/Conversation.java
@@ -248,7 +248,7 @@ public class Conversation implements Listener {
      */
     private void printOptions(final String[] options) {
         // i is for counting replies, like 1. something, 2. something else
-        int i = 0;
+        int optionsCount = 0;
         answers:
         for (final String option : options) {
             for (final ConditionID condition : data.getConditionIDs(option, OptionType.PLAYER)) {
@@ -256,9 +256,8 @@ public class Conversation implements Listener {
                     continue answers;
                 }
             }
-            i++;
             // print reply and put it to the hashmap
-            current.put(Integer.valueOf(i), option);
+            current.put(optionsCount++, option);
             // replace variables with their values
             String text = data.getText(playerID, language, option, OptionType.PLAYER);
             for (final String variable : BetonQuest.resolveVariables(text)) {
@@ -506,8 +505,8 @@ public class Conversation implements Listener {
             // started, causing it to display "null" all the time
             try {
                 final String name = data.getConversationIO();
-                final Class<? extends ConversationIO> c = plugin.getConvIO(name);
-                conv.inOut = c.getConstructor(Conversation.class, String.class).newInstance(conv, playerID);
+                final Class<? extends ConversationIO> convIO = plugin.getConvIO(name);
+                conv.inOut = convIO.getConstructor(Conversation.class, String.class).newInstance(conv, playerID);
             } catch (InstantiationException | IllegalAccessException | IllegalArgumentException
                     | InvocationTargetException | NoSuchMethodException | SecurityException e) {
                 LogUtils.getLogger().log(Level.WARNING, "Error when loading conversation IO");
@@ -522,8 +521,8 @@ public class Conversation implements Listener {
             if (messagesDelaying) {
                 try {
                     final String name = data.getInterceptor();
-                    final Class<? extends Interceptor> c = plugin.getInterceptor(name);
-                    conv.interceptor = c.getConstructor(Conversation.class, String.class).newInstance(conv, playerID);
+                    final Class<? extends Interceptor> interceptor = plugin.getInterceptor(name);
+                    conv.interceptor = interceptor.getConstructor(Conversation.class, String.class).newInstance(conv, playerID);
                 } catch (InstantiationException | IllegalAccessException | IllegalArgumentException
                         | InvocationTargetException | NoSuchMethodException | SecurityException e) {
                     LogUtils.getLogger().log(Level.WARNING, "Error when loading interceptor");
@@ -564,13 +563,13 @@ public class Conversation implements Listener {
 
             // print NPC's text
             printNPCText();
-            final ConversationOptionEvent e = new ConversationOptionEvent(player, conv, option, conv.option);
+            final ConversationOptionEvent optionEvent = new ConversationOptionEvent(player, conv, option, conv.option);
 
             new BukkitRunnable() {
 
                 @Override
                 public void run() {
-                    Bukkit.getPluginManager().callEvent(e);
+                    Bukkit.getPluginManager().callEvent(optionEvent);
                 }
             }.runTask(BetonQuest.getInstance());
 

commit de57739b28ee983bf1166d4b44720c02e02da3c0

fixed conversation option miss alignment
fixing bug introduced in cb0a8d52

src/main/java/pl/betoncraft/betonquest/conversation/Conversation.java
@@ -256,8 +256,9 @@ public class Conversation implements Listener {
                     continue answers;
                 }
             }
+            optionsCount++;
             // print reply and put it to the hashmap
-            current.put(optionsCount++, option);
+            current.put(optionsCount, option);
             // replace variables with their values
             String text = data.getText(playerID, language, option, OptionType.PLAYER);
             for (final String variable : BetonQuest.resolveVariables(text)) {