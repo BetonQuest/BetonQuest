BetonQuest/BetonQuest

commit 0131c4f801345f6cafd0085568f4392e1c1d679f

Improve chestput waring for invalid locations.

src/main/java/pl/betoncraft/betonquest/objectives/ChestPutObjective.java
@@ -18,6 +18,8 @@
 package pl.betoncraft.betonquest.objectives;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -39,6 +41,7 @@ import pl.betoncraft.betonquest.id.NoID;
 import pl.betoncraft.betonquest.utils.LocationData;
 import pl.betoncraft.betonquest.utils.LogUtils;
 import pl.betoncraft.betonquest.utils.PlayerConverter;
+import pl.betoncraft.betonquest.utils.Utils;
 
 import java.util.logging.Level;
 
@@ -88,19 +91,22 @@ public class ChestPutObjective extends Objective implements Listener {
             return;
         }
         try {
-            final Block block = loc.getLocation(playerID).getBlock();
-            final InventoryHolder chest;
-            try {
-                chest = (InventoryHolder) block.getState();
-            } catch (ClassCastException e) {
-                LogUtils.getLogger().log(Level.WARNING, "Could not cast the chest inventory: " + e.getMessage());
-                LogUtils.logThrowable(e);
+            final Location targetChestLocation = loc.getLocation(playerID);
+            final Block block = targetChestLocation.getBlock();
+            if (!(block instanceof InventoryHolder)) {
+                final World world = targetChestLocation.getWorld();
+                final String worldName = world == null ? "null" : world.getName();
+                LogUtils.getLogger().log(Level.WARNING,
+                        String.format("Error in '%s' chestput objective: Block at location x:%d y:%d z:%d in world '%s' isn't a chest!",
+                                instruction.getID().getFullID(),
+                                targetChestLocation.getBlockX(),
+                                targetChestLocation.getBlockY(),
+                                targetChestLocation.getBlockZ(),
+                                worldName));
                 return;
             }
-            if (event.getInventory() == null || event.getInventory().getHolder() == null) {
-                return;
-            }
-            if (!event.getInventory().getHolder().equals(chest)) {
+            final InventoryHolder chest = (InventoryHolder) block.getState();
+            if (!chest.equals(event.getInventory().getHolder())) {
                 return;
             }
             if (chestItemCondition.handle(playerID) && checkConditions(playerID)) {

commit fbc0b89259f3f367934940b6dd6417d9b269b4f1

fixed issue with ChestPut Objective introduced in 0131c4f8

src/main/java/pl/betoncraft/betonquest/objectives/ChestPutObjective.java
@@ -92,7 +92,7 @@ public class ChestPutObjective extends Objective implements Listener {
         try {
             final Location targetChestLocation = loc.getLocation(playerID);
             final Block block = targetChestLocation.getBlock();
-            if (!(block instanceof InventoryHolder)) {
+            if (!(block.getState() instanceof InventoryHolder)) {
                 final World world = targetChestLocation.getWorld();
                 LogUtils.getLogger().log(Level.WARNING,
                         String.format("Error in '%s' chestput objective: Block at location x:%d y:%d z:%d in world '%s' isn't a chest!",