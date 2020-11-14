f75bb19670fb93b5f14cd54700384a202c13aec9
SaltyAimbOtter <saltyaimbotter@gmail.com>
2020-11-14 13:29:26 +0100

    Revert "fix TitleNotifyIO's "subTitle:" option"
    This reverts commit bd43ae5e
    It introduced a new way of setting a subtitle in the TitleNotifyIO. This was already possible using much easier code though...

--- src/main/java/pl/betoncraft/betonquest/notify/TitleNotifyIO.java
+++ src/main/java/pl/betoncraft/betonquest/notify/TitleNotifyIO.java
@@ -1,11 +1,9 @@
 package pl.betoncraft.betonquest.notify;
 
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import pl.betoncraft.betonquest.exceptions.InstructionParseException;
 
 import java.util.Map;
-import java.util.regex.Pattern;
 
 public class TitleNotifyIO extends NotifyIO {
 
@@ -13,26 +11,19 @@ public class TitleNotifyIO extends NotifyIO {
     private final int stay;
     private final int fadeOut;
 
-    private static final Pattern REPLACE_PATTERN = Pattern.compile("((?:^|\\G|[^\\\\])(?:\\\\\\\\)*)_");
-    private String subtitle;
-
     public TitleNotifyIO(final Map<String, String> data) throws InstructionParseException {
         super(data);
 
         fadeIn = getIntegerData("fadein", 10);
         stay = getIntegerData("stay", 70);
         fadeOut = getIntegerData("fadeout", 20);
-
-        subtitle = data.getOrDefault("subtitle", "");
-        subtitle = REPLACE_PATTERN.matcher(subtitle).replaceAll("$1 ");
-        subtitle = subtitle.replace("\\_", "_");
-        subtitle = subtitle.replace("\\\\", "\\");
     }
 
     @Override
     protected void notifyPlayer(final String message, final Player player) {
-        //For some reason this needs to be done manually for subtitles...
-        subtitle = ChatColor.translateAlternateColorCodes('&', subtitle);
-        player.sendTitle(message, subtitle, fadeIn, stay, fadeOut);
+        final String[] messageParts = message.split("\n");
+        final String title = messageParts[0];
+        final String subtitle = messageParts.length > 1 ? messageParts[1] : "";
+        player.sendTitle(title, subtitle, fadeIn, stay, fadeOut);
     }
 }
