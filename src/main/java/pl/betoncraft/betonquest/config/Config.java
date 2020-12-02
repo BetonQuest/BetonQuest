5b609ec73a053d3f76621b7afd4614993ae99dbb
Wolf2323 <gabrielpatrikurban@gmail.com>
2020-12-02 23:06:10 +0100

    added PMD rule UseObjectForClearerAPI

--- src/main/java/pl/betoncraft/betonquest/config/Config.java
+++ src/main/java/pl/betoncraft/betonquest/config/Config.java
@@ -19,7 +19,7 @@ import java.util.logging.Level;
 /**
  * Handles the configuration of the plugin
  */
-@SuppressWarnings({"PMD.CyclomaticComplexity", "PMD.GodClass", "PMD.TooManyMethods"})
+@SuppressWarnings({"PMD.CyclomaticComplexity", "PMD.GodClass", "PMD.TooManyMethods", "PMD.UseObjectForClearerAPI"})
 public class Config {
 
     private final static List<String> UTIL_DIR_NAMES = Arrays.asList("logs", "backups", "conversations");
