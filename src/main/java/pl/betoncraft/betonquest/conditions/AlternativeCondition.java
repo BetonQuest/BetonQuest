fb413afaa585bf28d1698a10c4d1c17fad15e9b8
Wolf2323 <gabrielpatrikurban@gmail.com>
2021-01-07 22:57:58 +0100

    fixed AlternativeCondition

--- src/main/java/pl/betoncraft/betonquest/conditions/AlternativeCondition.java
+++ src/main/java/pl/betoncraft/betonquest/conditions/AlternativeCondition.java
@@ -1,12 +1,17 @@
 package pl.betoncraft.betonquest.conditions;
 
+import org.bukkit.Bukkit;
 import pl.betoncraft.betonquest.BetonQuest;
 import pl.betoncraft.betonquest.Instruction;
 import pl.betoncraft.betonquest.api.Condition;
 import pl.betoncraft.betonquest.exceptions.InstructionParseException;
 import pl.betoncraft.betonquest.id.ConditionID;
+import pl.betoncraft.betonquest.utils.LogUtils;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 /**
  * One of specified conditions has to be true
@@ -14,15 +19,39 @@ import java.util.List;
 @SuppressWarnings("PMD.CommentRequired")
 public class AlternativeCondition extends Condition {
 
-    private final List<ConditionID> conditions;
+    private final List<ConditionID> conditionIDs;
 
     public AlternativeCondition(final Instruction instruction) throws InstructionParseException {
         super(instruction, false);
-        conditions = instruction.getList(instruction::getCondition);
+        conditionIDs = instruction.getList(instruction::getCondition);
     }
 
     @Override
     protected Boolean execute(final String playerID) {
-        return BetonQuest.conditions(playerID, conditions);
+        if (Bukkit.isPrimaryThread()) {
+            for (final ConditionID id : conditionIDs) {
+                if (BetonQuest.condition(playerID, id)) {
+                    return true;
+                }
+            }
+        } else {
+            final List<CompletableFuture<Boolean>> conditions = new ArrayList<>();
+            for (final ConditionID id : conditionIDs) {
+                final CompletableFuture<Boolean> future = CompletableFuture.supplyAsync(
+                        () -> BetonQuest.condition(playerID, id));
+                conditions.add(future);
+            }
+            for (final CompletableFuture<Boolean> condition : conditions) {
+                try {
+                    if (condition.get()) {
+                        return true;
+                    }
+                } catch (final InterruptedException | ExecutionException e) {
+                    LogUtils.logThrowableReport(e);
+                    return false;
+                }
+            }
+        }
+        return false;
     }
 }
