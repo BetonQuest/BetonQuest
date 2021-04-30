0411fc9018d49ae29c0d01bf0d2609820aca7998
SaltyAimbOtter <saltyaimbotter@gmail.com>
2021-04-30 22:17:46 +0200

    added player attribute to QuestCompassTargetChangeEvent

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -17,6 +17,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - FastAsyncWorldEdit compatibility
 - craft objective variable `total`
 - curly braces in math.calc variables for using variables with math symbols
+- player attribute to QuestCompassTargetChangeEvent
 ### Changed
 - math variable now allows rounding output with the ~ operator
 - changed package names from `pl.betoncraft.betonquest` to `org.betonquest.betonquest`
--- src/main/java/org/betonquest/betonquest/api/QuestCompassTargetChangeEvent.java
+++ src/main/java/org/betonquest/betonquest/api/QuestCompassTargetChangeEvent.java
@@ -1,6 +1,8 @@
 package org.betonquest.betonquest.api;
 
+import lombok.Getter;
 import org.bukkit.Location;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
@@ -13,20 +15,20 @@ public class QuestCompassTargetChangeEvent extends Event implements Cancellable 
 
     private static final HandlerList HANDLERS = new HandlerList();
 
+    @Getter
+    private final Player player;
+    @Getter
     private final Location location;
     private boolean cancelled;
 
-    public QuestCompassTargetChangeEvent(final Location location) {
+    public QuestCompassTargetChangeEvent(final Player player, final Location location) {
         super();
+        this.player = player;
         this.location = location;
     }
 
     public static HandlerList getHandlerList() {
         return HANDLERS;
-    }
-
-    public Location getLocation() {
-        return location;
     }
 
     @Override
