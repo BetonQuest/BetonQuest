5105dfb59009a36952f00d70010781af6ba14e34
Jonas <joblo.2213@gmail.com>
2021-07-11 20:25:30 +0200

    HookExceptions should not print stack trace

--- src/main/java/pl/betoncraft/betonquest/compatibility/Compatibility.java
+++ src/main/java/pl/betoncraft/betonquest/compatibility/Compatibility.java
@@ -162,15 +162,25 @@ public class Compatibility implements Listener {
             try {
                 integrator.hook();
                 hooked.add(name);
-            } catch (final HookException | RuntimeException | LinkageError exception) {
-                final String message = String.format("There was an error while hooking into %s %s (BetonQuest %s, Spigot %s)! %s",
+            }  catch (final HookException exception) {
+                final String message = String.format("Could not hook into %s %s! %s",
+                        hookedPlugin.getName(),
+                        hookedPlugin.getDescription().getVersion(),
+                        exception.getMessage());
+                LogUtils.getLogger().log(Level.WARNING, message);
+                LogUtils.getLogger().log(Level.FINE, message, exception);
+                LogUtils.getLogger().log(Level.WARNING, "BetonQuest will work correctly, except for that single integration. "
+                        + "You can turn it off by setting 'hook." + name.toLowerCase(Locale.ROOT)
+                        + "' to false in config.yml file.");
+            } catch (final RuntimeException | LinkageError exception) {
+                final String message = String.format("There was an unexpected error while hooking into %s %s (BetonQuest %s, Spigot %s)! %s",
                         hookedPlugin.getName(),
                         hookedPlugin.getDescription().getVersion(),
                         BetonQuest.getInstance().getDescription().getVersion(),
                         Bukkit.getVersion(),
                         exception.getMessage());
                 LogUtils.getLogger().log(Level.WARNING, message, exception);
-                LogUtils.getLogger().log(Level.WARNING, "BetonQuest will work correctly save for that single integration. "
+                LogUtils.getLogger().log(Level.WARNING, "BetonQuest will work correctly, except for that single integration. "
                         + "You can turn it off by setting 'hook." + name.toLowerCase(Locale.ROOT)
                         + "' to false in config.yml file.");
             }
