e4e645eb646083e3c4b01b8db029708e35c0e64c
Wolf2323 <gabrielpatrikurban@gmail.com>
2022-03-19 20:47:09 +0100

    added assert for log messages in PlayerLogHandlerTest

--- src/test/java/org/betonquest/betonquest/modules/logger/custom/PlayerLogHandlerTest.java
+++ src/test/java/org/betonquest/betonquest/modules/logger/custom/PlayerLogHandlerTest.java
@@ -3,17 +3,21 @@ package org.betonquest.betonquest.modules.logger.custom;
 import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.platform.bukkit.BukkitAudiences;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import org.betonquest.betonquest.api.config.QuestPackage;
 import org.betonquest.betonquest.modules.logger.BetonQuestLogRecord;
 import org.betonquest.betonquest.modules.logger.util.BetonQuestLoggerService;
 import org.betonquest.betonquest.modules.logger.util.LogValidator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 
+import java.util.List;
 import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 /**
@@ -37,17 +41,19 @@ class PlayerLogHandlerTest {
             playerHandler.setFormatter(new ChatLogFormatter());
             logger.addHandler(playerHandler);
 
-            final Audience audience1 = getAudience(playerHandler, "Package1", audiences);
-            final Audience audience2 = getAudience(playerHandler, "Package2", audiences);
+            final Audience audience1 = getAudience(playerHandler, audiences, "Package1", "Package3");
+            final Audience audience2 = getAudience(playerHandler, audiences, "Package2", "Package3");
 
             createLogMessages(logger);
             assertLogMessages(audience1, audience2);
         }
     }
 
-    private Audience getAudience(final PlayerLogHandler playerHandler, final String pack, final BukkitAudiences audiences) {
+    private Audience getAudience(final PlayerLogHandler playerHandler, final BukkitAudiences audiences, final String... packs) {
         final UUID uuid = UUID.randomUUID();
-        playerHandler.addFilter(uuid, pack, Level.INFO);
+        for (final String pack : packs) {
+            playerHandler.addFilter(uuid, pack, Level.INFO);
+        }
 
         final Audience audience = mock(Audience.class);
         when(audiences.player(uuid)).thenReturn(audience);
@@ -57,22 +63,32 @@ class PlayerLogHandlerTest {
     private void createLogMessages(final Logger logger) {
         final QuestPackage pack1 = mock(QuestPackage.class);
         final QuestPackage pack2 = mock(QuestPackage.class);
+        final QuestPackage pack3 = mock(QuestPackage.class);
         when(pack1.getPackagePath()).thenReturn("Package1");
         when(pack2.getPackagePath()).thenReturn("Package2");
+        when(pack3.getPackagePath()).thenReturn("Package3");
 
-        final BetonQuestLogRecord record = new BetonQuestLogRecord(null, null, Level.INFO, "Message");
         final BetonQuestLogRecord record1 = new BetonQuestLogRecord(null, pack1, Level.INFO, "Message 1");
         final BetonQuestLogRecord record2 = new BetonQuestLogRecord(null, pack2, Level.INFO, "Message 2");
-        logger.log(record);
+        final BetonQuestLogRecord record3 = new BetonQuestLogRecord(null, pack3, Level.INFO, "Message 3");
         logger.log(record1);
         logger.log(record2);
+        logger.log(record3);
     }
 
     private void assertLogMessages(final Audience audience1, final Audience audience2) {
-        verify(audience1, times(1)).sendMessage(any(Component.class));
-        verify(audience1, times(1)).sendMessage(any(Component.class));
+        final ArgumentCaptor<Component> argumentCaptor = ArgumentCaptor.forClass(Component.class);
 
-        verify(audience2, times(1)).sendMessage(any(Component.class));
-        verify(audience2, times(1)).sendMessage(any(Component.class));
+        verify(audience1, times(2)).sendMessage(argumentCaptor.capture());
+        verify(audience2, times(2)).sendMessage(argumentCaptor.capture());
+
+        final List<Component> components = argumentCaptor.getAllValues();
+
+        final PlainTextComponentSerializer serializer = PlainTextComponentSerializer.plainText();
+
+        assertEquals("§7[§8BQ | §7]§r <Package1> §fMessage 1", serializer.serialize(components.get(0)), "");
+        assertEquals("§7[§8BQ | §7]§r <Package3> §fMessage 3", serializer.serialize(components.get(1)), "");
+        assertEquals("§7[§8BQ | §7]§r <Package2> §fMessage 2", serializer.serialize(components.get(2)), "");
+        assertEquals("§7[§8BQ | §7]§r <Package3> §fMessage 3", serializer.serialize(components.get(3)), "");
     }
 }
