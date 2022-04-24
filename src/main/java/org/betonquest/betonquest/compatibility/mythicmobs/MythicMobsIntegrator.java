c0719a1ab27f2f3da08eb25ad95cb7739cd4c251
saltyaimbotter <saltyaimbotter@gmail.com>
2022-04-24 14:02:56 +0200

    revert faulty integrator swap

--- src/main/java/org/betonquest/betonquest/compatibility/mythicmobs/MythicMobsIntegrator.java
+++ src/main/java/org/betonquest/betonquest/compatibility/mythicmobs/MythicMobsIntegrator.java
@@ -2,7 +2,6 @@ package org.betonquest.betonquest.compatibility.mythicmobs;
 
 import org.betonquest.betonquest.BetonQuest;
 import org.betonquest.betonquest.compatibility.Integrator;
-import org.betonquest.betonquest.compatibility.mmogroup.mmocore.MMOCoreCastSkillObjective;
 
 @SuppressWarnings("PMD.CommentRequired")
 public class MythicMobsIntegrator implements Integrator {
@@ -17,7 +16,6 @@ public class MythicMobsIntegrator implements Integrator {
     public void hook() {
         plugin.registerConditions("mythicmobdistance", MythicMobDistanceCondition.class);
         plugin.registerObjectives("mmobkill", MythicMobKillObjective.class);
-        plugin.registerObjectives("mmocorecastskill", MMOCoreCastSkillObjective.class);
         plugin.registerEvents("mspawnmob", MythicSpawnMobEvent.class);
     }
 
