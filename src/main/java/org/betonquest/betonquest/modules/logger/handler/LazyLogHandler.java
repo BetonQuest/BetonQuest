96686582fbf7667e20bd00a3c48bcae5c297f468
Niklas Seyfarth <niklas@seyfarth.de>
2022-06-08 09:57:54 +0200

    Make LazyLogHandler thread safe.

--- src/main/java/org/betonquest/betonquest/modules/logger/handler/LazyLogHandler.java
+++ src/main/java/org/betonquest/betonquest/modules/logger/handler/LazyLogHandler.java
@@ -7,72 +7,96 @@ import java.util.logging.Handler;
 import java.util.logging.LogRecord;
 
 /**
- * {@link Handler} that creates the target {@link Handler}, when needed.
+ * Lazy {@link Handler} that creates the target {@link Handler} on demand.
+ * Only {@link #publish(LogRecord)} will trigger initialization, all other
+ * methods are silently ignored when the internal {@link Handler} was not
+ * yet initialized.
  */
 public class LazyLogHandler extends Handler {
 
     /**
-     * {@link Lock} instance, when the target {@link Handler} is created
+     * {@link Lock} to ensure mutual exclusion of initialization and
+     * closing of the internal {@link Handler}.
      */
-    private final Lock publishLock;
+    private final Lock lock;
 
     /**
-     * {@link Supplier} for the {@link Handler} creation.
+     * Factory that lazily creates the internal {@link Handler}.
      */
     private final Supplier<Handler> handlerFactory;
 
     /**
-     * Weather this {@link Handler} is closed.
+     * Marker that the internal {@link Handler} is closed for the case
+     * that it wasn't initialized.
      */
     private boolean closed;
-    /**
-     * The created target {@link Handler}.
-     */
-    private Handler target;
 
     /**
-     * Create a new {@link LazyLogHandler} with a given {@link Supplier}.
+     * Lazily created internal {@link Handler}.
+     */
+    private Handler internalHandler;
+
+    /**
+     * Create a new {@link LazyLogHandler} that will create the actual
+     * {@link Handler} on demand by calling the given {@link Supplier}.
      *
      * @param handlerFactory the {@link Supplier} to use for creation
      */
     public LazyLogHandler(final Supplier<Handler> handlerFactory) {
         super();
-        this.publishLock = new ReentrantLock();
+        this.lock = new ReentrantLock();
         this.handlerFactory = handlerFactory;
     }
 
     @Override
     public void publish(final LogRecord record) {
-        if (closed) {
-            return;
+        requireNotClosed();
+        if (internalHandler == null) {
+            initializeInternalHandler();
         }
-        if (target == null) {
-            publishLock.lock();
-            try {
-                if (target == null) {
-                    target = handlerFactory.get();
-                }
-            } finally {
-                publishLock.unlock();
+        internalHandler.publish(record);
+    }
+
+    private void initializeInternalHandler() {
+        lock.lock();
+        try {
+            requireNotClosed();
+            if (internalHandler == null) {
+                internalHandler = handlerFactory.get();
             }
+        } finally {
+            lock.unlock();
         }
-        if (target != null) {
-            target.publish(record);
+    }
+
+    private void requireNotClosed() {
+        if (closed) {
+            throw new IllegalStateException(
+                    "Cannot publish log record: LazyLogHandler was closed and had not been initialized before closing."
+            );
         }
     }
 
     @Override
     public void flush() {
-        if (target != null) {
-            target.flush();
+        if (internalHandler != null) {
+            internalHandler.flush();
         }
     }
 
     @Override
     public void close() {
-        if (target != null) {
-            target.close();
-            closed = true;
+        if (closed) {
+            return;
+        }
+        closed = true;
+        lock.lock();
+        try {
+            if (internalHandler != null) {
+                internalHandler.close();
+            }
+        } finally {
+            lock.unlock();
         }
     }
 }
