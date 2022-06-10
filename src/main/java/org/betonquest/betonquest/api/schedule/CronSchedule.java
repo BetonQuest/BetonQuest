f7fe81922f65ed8b33f08502ff922d7752ca12d8
Jonas <17405009+J0B10@users.noreply.github.com>
2022-06-10 21:25:29 +0200

    Add support for @hourly, @daily, ..., @yearly cron expressions

--- src/main/java/org/betonquest/betonquest/api/schedule/CronSchedule.java
+++ src/main/java/org/betonquest/betonquest/api/schedule/CronSchedule.java
@@ -1,7 +1,6 @@
 package org.betonquest.betonquest.api.schedule;
 
 import com.cronutils.model.Cron;
-import com.cronutils.model.CronType;
 import com.cronutils.model.SingleCron;
 import com.cronutils.model.definition.CronDefinition;
 import com.cronutils.model.definition.CronDefinitionBuilder;
@@ -25,7 +24,16 @@ public abstract class CronSchedule extends Schedule {
     /**
      * The unix cron syntax shall be used by default
      */
-    public static final CronDefinition DEFAULT_CRON_DEFINITION = CronDefinitionBuilder.instanceDefinitionFor(CronType.UNIX);
+    public static final CronDefinition DEFAULT_CRON_DEFINITION = CronDefinitionBuilder.defineCron()
+            .withMinutes().withValidRange(0, 59).withStrictRange().and()
+            .withHours().withValidRange(0, 23).withStrictRange().and()
+            .withDayOfMonth().withValidRange(1, 31).withStrictRange().and()
+            .withMonth().withValidRange(1, 12).withStrictRange().and()
+            .withDayOfWeek().withValidRange(0, 7).withMondayDoWValue(1).withIntMapping(7, 0).withStrictRange().and()
+            .withSupportedNicknameYearly().withSupportedNicknameAnnually()
+            .withSupportedNicknameMonthly().withSupportedNicknameWeekly()
+            .withSupportedNicknameDaily().withSupportedNicknameHourly()
+            .instance();
 
     /**
      * Statement that means that the schedule should run on reboot
