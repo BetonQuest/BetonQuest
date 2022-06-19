fa25d9fc58d69d75396c5dfe6f05341b8ad16cc5
Wolf2323 <gabrielpatrikurban@gmail.com>
2022-06-19 18:50:38 +0200

    fixes for LogRecordTypeFilterTest

--- src/test/java/org/betonquest/betonquest/modules/logger/filter/LogRecordTypeFilterTest.java
+++ src/test/java/org/betonquest/betonquest/modules/logger/filter/LogRecordTypeFilterTest.java
@@ -4,10 +4,10 @@ import org.betonquest.betonquest.modules.logger.BetonQuestLogRecord;
 import org.junit.jupiter.api.Test;
 
 import java.util.logging.Filter;
+import java.util.logging.Level;
 import java.util.logging.LogRecord;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 /**
  * A test for the {@link LogRecordTypeFilterTest}.
@@ -16,12 +16,12 @@ class LogRecordTypeFilterTest {
     @Test
     void testInstanceOf() {
         final Filter filter = new LogRecordTypeFilter(BetonQuestLogRecord.class);
-        assertTrue(filter.isLoggable(mock(BetonQuestLogRecord.class)), "BetonQuestLogRecord should be loggable");
+        assertTrue(filter.isLoggable(new BetonQuestLogRecord(Level.INFO, "", "", null)), "BetonQuestLogRecord should be loggable");
     }
 
     @Test
     void testNotInstanceOf() {
         final Filter filter = new LogRecordTypeFilter(BetonQuestLogRecord.class);
-        assertFalse(filter.isLoggable(mock(LogRecord.class)), "BetonQuestLogRecord should not be loggable");
+        assertFalse(filter.isLoggable(new LogRecord(Level.INFO, "")), "LogRecord should not be loggable");
     }
 }
