022fadce29ef2649529c66eb0e42f00225e40327
Niklas Seyfarth <niklas@seyfarth.de>
2022-07-01 19:17:13 +0200

    Write package documentation for quest.event packages.

---
+++ src/main/java/org/betonquest/betonquest/quest/event/journal/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * {@link org.betonquest.betonquest.api.quest.event.Event Event} implementation
+ * for {@link org.betonquest.betonquest.Journal Journal}-related events.
+ */
+package org.betonquest.betonquest.quest.event.journal;
---
+++ src/main/java/org/betonquest/betonquest/quest/event/legacy/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Adapters to allow cross-usage between the old and new event API while some
+ * events are still left for migration.
+ */
+package org.betonquest.betonquest.quest.event.legacy;
