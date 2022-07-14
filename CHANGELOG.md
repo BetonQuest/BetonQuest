28af38a34f8a13cc9169e0dd4c7607c3b38b6e30
Wolf2323 <gabrielpatrikurban@gmail.com>
2022-07-14 10:19:15 +0200

    fixed conversation IO chest did not show the correct NPC heads

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Deprecated
 ### Removed
 ### Fixed
+- conversation IO chest did not show the correct NPC heads
 ### Security
 
 ## [1.12.9] - 2022-06-22
--- src/main/java/pl/betoncraft/betonquest/compatibility/citizens/CitizensInventoryConvIO.java
+++ src/main/java/pl/betoncraft/betonquest/compatibility/citizens/CitizensInventoryConvIO.java
@@ -1,5 +1,7 @@
 package pl.betoncraft.betonquest.compatibility.citizens;
 
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
 import net.citizensnpcs.trait.SkinTrait;
 import org.bukkit.Bukkit;
 import org.bukkit.inventory.meta.SkullMeta;
@@ -9,9 +11,9 @@ import pl.betoncraft.betonquest.conversation.InventoryConvIO;
 import pl.betoncraft.betonquest.utils.LogUtils;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
+import java.util.logging.Level;
 
 @SuppressWarnings("PMD.CommentRequired")
 public class CitizensInventoryConvIO extends InventoryConvIO {
@@ -30,38 +32,22 @@ public class CitizensInventoryConvIO extends InventoryConvIO {
 
             final CitizensConversation citizensConv = (CitizensConversation) conv;
             try {
-                // read the texture from the NPC
                 final SkinTrait skinTrait = Bukkit.getScheduler().callSyncMethod(BetonQuest.getInstance(), () -> citizensConv.getNPC().getOrAddTrait(SkinTrait.class)).get();
                 final String texture = skinTrait.getTexture();
 
-                if (texture != null) { // Can be null if not cached yet
-                    // prepare reflection magics
-                    final Class<?> profileClass = Class.forName("com.mojang.authlib.GameProfile");
-                    final Class<?> propertyClass = Class.forName("com.mojang.authlib.properties.Property");
-                    final Class<?> propertyMapClass = Class.forName("com.google.common.collect.ForwardingMultimap");
+                if (texture != null) {
+                    final GameProfile gameProfile = new GameProfile(UUID.randomUUID(), null);
+                    final Property property = new Property("textures", texture, skinTrait.getSignature());
+                    gameProfile.getProperties().put("textures", property);
 
-                    // abracadabra
-                    final Object profile = profileClass
-                            .getConstructor(UUID.class, String.class)
-                            .newInstance(UUID.randomUUID(), null);
-                    final Object property = propertyClass
-                            .getConstructor(String.class, String.class, String.class)
-                            .newInstance("textures", texture, skinTrait.getSignature());
-                    final Object propertyMap = profileClass
-                            .getMethod("getProperties")
-                            .invoke(profile);
-                    propertyMapClass
-                            .getMethod("put", Object.class, Object.class)
-                            .invoke(propertyMap, "textures", property);
                     final Field field = meta.getClass().getDeclaredField("profile");
                     field.setAccessible(true);
-                    field.set(meta, profile);
+                    field.set(meta, gameProfile);
                     return meta;
                 }
             } catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException
-                    | InstantiationException | InvocationTargetException | NoSuchMethodException
-                    | ClassNotFoundException | InterruptedException | ExecutionException e) {
-                LogUtils.logThrowableIgnore(e);
+                     | InterruptedException | ExecutionException e) {
+                LogUtils.getLogger().log(Level.FINE, "Could not resolve a skin Texture!", e);
             }
         }
         return super.updateSkullMeta(meta);
