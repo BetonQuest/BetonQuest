7065570d183745d9fb04bd9fe0df3167f399044a
Wolf2323 <gabrielpatrikurban@gmail.com>
2022-07-14 10:19:15 +0200

    fixed conversation IO chest did not show the correct NPC heads

--- src/main/java/org/betonquest/betonquest/compatibility/citizens/CitizensInventoryConvIO.java
+++ src/main/java/org/betonquest/betonquest/compatibility/citizens/CitizensInventoryConvIO.java
@@ -1,5 +1,7 @@
 package org.betonquest.betonquest.compatibility.citizens;
 
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
 import lombok.CustomLog;
 import net.citizensnpcs.trait.SkinTrait;
 import org.betonquest.betonquest.BetonQuest;
@@ -9,7 +11,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.inventory.meta.SkullMeta;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
@@ -32,37 +33,21 @@ public class CitizensInventoryConvIO extends InventoryConvIO {
 
             final CitizensConversation citizensConv = (CitizensConversation) conv;
             try {
-                // read the texture from the NPC
                 final SkinTrait skinTrait = Bukkit.getScheduler().callSyncMethod(BetonQuest.getInstance(), () -> citizensConv.getNPC().getOrAddTrait(SkinTrait.class)).get();
                 final String texture = skinTrait.getTexture();
 
-                if (texture != null) { // Can be null if not cached yet
-                    // prepare reflection magics
-                    final Class<?> profileClass = Class.forName("com.mojang.authlib.GameProfile");
-                    final Class<?> propertyClass = Class.forName("com.mojang.authlib.properties.Property");
-                    final Class<?> propertyMapClass = Class.forName("com.google.common.collect.ForwardingMultimap");
+                if (texture != null) {
+                    final GameProfile gameProfile = new GameProfile(UUID.randomUUID(), null);
+                    final Property property = new Property("textures", texture, skinTrait.getSignature());
+                    gameProfile.getProperties().put("textures", property);
 
-                    // abracadabra
-                    final Object profile = profileClass
-                            .getConstructor(UUID.class, String.class)
-                            .newInstance(UUID.randomUUID(), null);
-                    final Object property = propertyClass
-                            .getConstructor(String.class, String.class, String.class)
-                            .newInstance("textures", texture, skinTrait.getSignature());
-                    final Object propertyMap = profileClass
-                            .getMethod("getProperties")
-                            .invoke(profile);
-                    propertyMapClass
-                            .getMethod("put", Object.class, Object.class)
-                            .invoke(propertyMap, "textures", property);
                     final Field field = meta.getClass().getDeclaredField("profile");
                     field.setAccessible(true);
-                    field.set(meta, profile);
+                    field.set(meta, gameProfile);
                     return meta;
                 }
             } catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException
-                     | InstantiationException | InvocationTargetException | NoSuchMethodException
-                     | ClassNotFoundException | InterruptedException | ExecutionException e) {
+                     | InterruptedException | ExecutionException e) {
                 LOG.debug(citizensConv.getPackage(), "Could not resolve a skin Texture!", e);
             }
         }
