8bee09cbd27f872e604a3d59562f29b1aef9e728
RAVINGAR <81618382+RAVINGAR@users.noreply.github.com>
2022-07-19 17:10:07 +0800

    Apply suggestions from code review
    Added suggestions
    
    Co-authored-by: Wolf2323 <gabrielpatrikurban@gmail.com>

--- src/main/java/org/betonquest/betonquest/compatibility/heroes/HeroesAttributeCondition.java
+++ src/main/java/org/betonquest/betonquest/compatibility/heroes/HeroesAttributeCondition.java
@@ -13,24 +13,12 @@ import org.betonquest.betonquest.utils.PlayerConverter;
 
 /**
  * Checks an attribute of a player and if greater than or equal to a level
- * Formatted as such "heroesattribute {attribute} {level}
  */
 @SuppressWarnings("PMD.CommentRequired")
 public class HeroesAttributeCondition extends Condition {
     private final AttributeType attribute;
     private final VariableNumber level;
 
-    /**
-     * Creates new instance of the condition. The condition should parse
-     * instruction string at this point and extract all the data from it. If
-     * anything goes wrong, throw {@link InstructionParseException} with an
-     * error message describing the problem.
-     *
-     * @param instruction the Instruction object; you can get one from ID instance with
-     *                    {@link ID#generateInstruction()
-     *                    ID.generateInstruction()} or create it from an instruction
-     *                    string
-     */
     public HeroesAttributeCondition(final Instruction instruction) throws InstructionParseException {
         super(instruction, true);
         attribute = findAttribute(instruction.next());
