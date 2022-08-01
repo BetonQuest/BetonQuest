1f534b9edca38442c1efbe720407d5c3927dfbf0
Firlefunke <barbara@seyfarth.de>
2022-08-01 22:47:42 +0200

    Add tests for new implementation of tag event.

---
+++ src/test/java/org/betonquest/betonquest/quest/event/tag/AddTagChangerTest.java
@@ -0,0 +1,34 @@
+package org.betonquest.betonquest.quest.event.tag;
+
+import org.betonquest.betonquest.database.TagData;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * Test {@link AddTagChanger}
+ */
+@ExtendWith(MockitoExtension.class)
+class AddTagChangerTest {
+    @Test
+    void testAddTagChangerAddNoTags(@Mock final TagData tagData) {
+        final AddTagChanger changer = new AddTagChanger();
+
+        changer.changeTags(tagData);
+        verifyNoInteractions(tagData);
+    }
+
+    @Test
+    void testAddTagChangerAddMultipleTags(@Mock final TagData tagData) {
+        final String[] tags = {"tag-1", "tag-2", "tag-3"};
+        final AddTagChanger changer = new AddTagChanger(tags);
+
+        changer.changeTags(tagData);
+        verify(tagData).addTag("tag-1");
+        verify(tagData).addTag("tag-2");
+        verify(tagData).addTag("tag-3");
+    }
+}
