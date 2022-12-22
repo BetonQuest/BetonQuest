ded156fd3518677ea0d62de26dca77ab06d48d6e
Niklas Seyfarth <niklas@seyfarth.de>
2022-12-22 11:29:15 +0100

    Correctly handle failure of parsing a double as value of a VariableNumber.

--- src/main/java/org/betonquest/betonquest/VariableNumber.java
+++ src/main/java/org/betonquest/betonquest/VariableNumber.java
@@ -10,34 +10,34 @@ import org.betonquest.betonquest.exceptions.QuestRuntimeException;
 /**
  * Represents a number which might also be a variable.
  */
-@SuppressWarnings("PMD.CommentRequired")
 @CustomLog
 public class VariableNumber {
 
+    /**
+     * The constant value of this variable number if no variable was set.
+     */
     private final double number;
-    private Variable variable;
 
     /**
-     * Parses the string as a number or saves it as a variable if it's not a
-     * number.
+     * The variable to parse to get the value of the variable number.
+     * If {@code null} then {@link #number} will be used.
+     */
+    private final Variable variable;
+
+    /**
+     * Parses the string as a variable or as a number if it's not a variable.
      *
      * @param packName the package in which the variable is defined
-     * @param variable the string to parse
+     * @param tmp the string to parse
      * @throws InstructionParseException If the variable could not be created.
      */
-    public VariableNumber(final String packName, final String variable) throws InstructionParseException {
-        if (variable.length() > 2 && variable.charAt(0) == '%' && variable.endsWith("%")) {
-            try {
-                this.variable = BetonQuest.createVariable(Config.getPackages().get(packName), variable);
-            } catch (final InstructionParseException e) {
-                throw new InstructionParseException("Could not create variable: " + e.getMessage(), e);
-            }
-            if (this.variable == null) {
-                throw new InstructionParseException("Could not create variable");
-            }
-            number = 0.0;
+    public VariableNumber(final String packName, final String tmp) throws InstructionParseException {
+        if (tmp.length() > 2 && tmp.charAt(0) == '%' && tmp.endsWith("%")) {
+            this.variable = parseAsVariable(packName, tmp);
+            this.number = 0.0;
         } else {
-            number = Double.parseDouble(variable);
+            this.variable = null;
+            this.number = parseAsNumber(tmp);
         }
     }
 
@@ -48,6 +48,7 @@ public class VariableNumber {
      */
     public VariableNumber(final int number) {
         this.number = number;
+        this.variable = null;
     }
 
     /**
@@ -57,6 +58,28 @@ public class VariableNumber {
      */
     public VariableNumber(final double number) {
         this.number = number;
+        this.variable = null;
+    }
+
+    private Variable parseAsVariable(final String packName, final String variable) throws InstructionParseException {
+        final Variable parsed;
+        try {
+            parsed = BetonQuest.createVariable(Config.getPackages().get(packName), variable);
+        } catch (final InstructionParseException e) {
+            throw new InstructionParseException("Could not create variable: " + e.getMessage(), e);
+        }
+        if (parsed == null) {
+            throw new InstructionParseException("Could not create variable");
+        }
+        return parsed;
+    }
+
+    private double parseAsNumber(final String variable) throws InstructionParseException {
+        try {
+            return Double.parseDouble(variable);
+        } catch (NumberFormatException e) {
+            throw new InstructionParseException("Not a number: " + variable, e);
+        }
     }
 
     /**
