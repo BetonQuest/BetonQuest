78d89d7056feb9285a9cbde3387d51243fc9792b
Wolf2323 <gabrielpatrikurban@gmail.com>
2023-01-11 11:27:52 +0100

    activated PMD ExcessivePublicCount for Test classes

--- src/test/java/org/betonquest/betonquest/api/bukkit/config/util/ConfigurationSectionBaseTest.java
+++ src/test/java/org/betonquest/betonquest/api/bukkit/config/util/ConfigurationSectionBaseTest.java
@@ -35,7 +35,8 @@ import static org.junit.jupiter.api.Assertions.*;
  * You only need to override methods with behaviours that differ from the default one.
  */
 @Tag("ConfigurationSection")
-@SuppressWarnings({"PMD.GodClass", "PMD.JUnitAssertionsShouldIncludeMessage", "PMD.JUnit5TestShouldBePackagePrivate"})
+@SuppressWarnings({"PMD.GodClass", "PMD.JUnitAssertionsShouldIncludeMessage", "PMD.JUnit5TestShouldBePackagePrivate",
+        "PMD.ExcessivePublicCount"})
 public class ConfigurationSectionBaseTest extends AbstractConfigBaseTest<ConfigurationSection> implements ConfigurationSectionTestInterface {
 
     @Override
