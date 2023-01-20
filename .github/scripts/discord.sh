ae833993d2939d7bb16bf01aad3b969db35f14a4
Wolf2323 <gabrielpatrikurban@gmail.com>
2023-01-20 20:43:33 +0100

    replaced hardcoded docs url in discord.sh script with a secret

--- .github/scripts/discord.sh
+++ .github/scripts/discord.sh
@@ -7,6 +7,10 @@ if [ -z "$WEBHOOK_URL" ]; then
   echo "[Webhook]: WARNING! You need to pass the WEBHOOK_URL environment variable."
   exit 1
 fi
+if [ -z "$DOCS_URL" ]; then
+  echo "[Webhook]: WARNING! You need to pass the DOCS_URL environment variable."
+  exit 1
+fi
 
 echo "[Webhook]: Preparing resources...";
 AVATAR="https://github.com/actions.png"
@@ -47,7 +51,7 @@ case "$VERSION_TYPE" in
 
   "development" )
     RELEASE_NAME="Dev-Build"
-    RELEASE_DOWNLOAD_URL="https://docs.betonquest.org/DEV/Downloads/"
+    RELEASE_DOWNLOAD_URL="${DOCS_URL}DEV/Downloads/"
     RELEASE_COMMIT_ICON_SUCCESS="$COMMIT_ICON_SUCCESS"
     ;;
 
@@ -63,7 +67,7 @@ case "$JOB_STATUS" in
     EMBED_COLOR=3066993
     DEV_BUILD_DOWNLOAD="Click to Download $VERSION!"
     STATUS_MESSAGE="$RELEASE_NAME is now available"
-    BUILD_DOWNLOAD_URL="https://docs.betonquest.org/DEV/Downloads/?path=$UPLOAD_PATH&filename=BetonQuest.jar"
+    BUILD_DOWNLOAD_URL="${DOCS_URL}DEV/Downloads/?path=$UPLOAD_PATH&filename=BetonQuest.jar"
     DESCRIPTION="${RELEASE_NAME}s available [HERE](${RELEASE_DOWNLOAD_URL}). Report bugs [HERE](https://github.com/BetonQuest/BetonQuest/issues)"
     COMMIT_ICON="$RELEASE_COMMIT_ICON_SUCCESS"
     ;;
