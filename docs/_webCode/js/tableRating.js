c09a0a301e4b8c5b24f8370f55e0d5c9aac60be7
saltyaimbotter <saltyaimbotter@gmail.com>
2023-01-26 21:31:24 +0100

    update tableRating.js to table contents

--- docs/_webCode/js/tableRating.js
+++ docs/_webCode/js/tableRating.js
@@ -6,14 +6,11 @@ async function updateRating() {
   let LMBquests = getRating(23696)
   let notQuests = getRating(95872)
   let beautyQuests = getRating(39255)
-  let mangoQuest = new Promise((resolve) => {
-    /* MangoQuest is not on Spigot */
-    resolve("-")
-  })
+
   let questCreator = getRating(38734)
   let proQuests = getRating(18249)
 
-  const ratings = Promise.all([betonQuest, quests, LMBquests, notQuests, beautyQuests, mangoQuest, questCreator, proQuests])
+  const ratings = Promise.all([betonQuest, quests, LMBquests, notQuests, beautyQuests, questCreator, proQuests])
 
   // Get the first row of the table, excluding the first cell (the description)
   let firstTableRow = document.querySelectorAll("table > tbody > tr:nth-child(1) > td:not(:first-child)");
@@ -27,6 +24,9 @@ async function updateRating() {
 
 function getRating(ressourceID) {
   return fetch("https://api.spiget.org/v2/resources/" + ressourceID).then(response => response.json())
-    .then(data => data["rating"]["average"])
+    .then(data => {
+      let rating = data["rating"]["average"]
+      return rating.toFixed(1)
+    })
 }
 
