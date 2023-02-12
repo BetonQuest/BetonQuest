d4b466394816b81a380b68a5de67d01c4c7a4eda
Todd Harrison <todd@eharrison.com>
2023-02-12 09:05:07 -0700

    Added missing import.

--- src/main/java/org/betonquest/betonquest/item/QuestItem.java
+++ src/main/java/org/betonquest/betonquest/item/QuestItem.java
@@ -25,6 +25,7 @@ import org.bukkit.Color;
 import org.bukkit.DyeColor;
 import org.bukkit.FireworkEffect;
 import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BookMeta;
