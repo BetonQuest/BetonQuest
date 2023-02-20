fa4a722f3af116f1fe73e33e385a21807dba57a0
MrGuitz <guitzmeyer@gmail.com>
2023-02-20 18:01:56 +0100

    migration of the TeleportEvent to the new system

---
+++ src/main/java/org/betonquest/betonquest/quest/event/teleport/TeleportEventFactory.java
@@ -0,0 +1,52 @@
+package org.betonquest.betonquest.quest.event.teleport;
+
+import org.betonquest.betonquest.Instruction;
+import org.betonquest.betonquest.api.quest.event.Event;
+import org.betonquest.betonquest.api.quest.event.EventFactory;
+import org.betonquest.betonquest.exceptions.InstructionParseException;
+import org.betonquest.betonquest.quest.event.OnlineProfileRequiredEvent;
+import org.betonquest.betonquest.quest.event.PrimaryServerThreadEvent;
+import org.betonquest.betonquest.utils.location.CompoundLocation;
+import org.bukkit.Server;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitScheduler;
+
+/**
+ * Factory to create teleport events from {@link Instruction}s.
+ */
+public class TeleportEventFactory implements EventFactory {
+    /**
+     * Server to use for syncing to the primary server thread.
+     */
+    private final Server server;
+    /**
+     * Scheduler to use for syncing to the primary server thread.
+     */
+    private final BukkitScheduler scheduler;
+    /**
+     * Plugin to use for syncing to the primary server thread.
+     */
+    private final Plugin plugin;
+
+    /**
+     * Create the door event factory.
+     *
+     * @param server    server to use
+     * @param scheduler scheduler to use
+     * @param plugin    plugin to use
+     */
+    public TeleportEventFactory(final Server server, final BukkitScheduler scheduler, final Plugin plugin) {
+        this.server = server;
+        this.scheduler = scheduler;
+        this.plugin = plugin;
+    }
+
+    @Override
+    public Event parseEvent(final Instruction instruction) throws InstructionParseException {
+        final CompoundLocation location = instruction.getLocation();
+        return new PrimaryServerThreadEvent(
+                new OnlineProfileRequiredEvent(
+                        new TeleportEvent(location), instruction.getPackage()),
+                server, scheduler, plugin);
+    }
+}
