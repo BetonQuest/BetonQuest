b1692a8594236ee83a7288d048e19200279abc19
Lorias-Aegorin <Jannik.k11@gmail.com>
2023-02-27 20:14:11 +0100

    added version check for fakeblock

--- src/main/java/org/betonquest/betonquest/compatibility/fakeblock/FakeBlockIntegrator.java
+++ src/main/java/org/betonquest/betonquest/compatibility/fakeblock/FakeBlockIntegrator.java
@@ -3,12 +3,22 @@ package org.betonquest.betonquest.compatibility.fakeblock;
 import org.betonquest.betonquest.BetonQuest;
 import org.betonquest.betonquest.compatibility.Integrator;
 import org.betonquest.betonquest.exceptions.HookException;
+import org.betonquest.betonquest.exceptions.UnsupportedVersionException;
+import org.betonquest.betonquest.modules.versioning.UpdateStrategy;
+import org.betonquest.betonquest.modules.versioning.Version;
+import org.betonquest.betonquest.modules.versioning.VersionComparator;
+import org.bukkit.Bukkit;
 import org.bukkit.Server;
+import org.bukkit.plugin.Plugin;
 
 /**
  * Integrates with FakeBlock
  */
 public class FakeBlockIntegrator implements Integrator {
+    /**
+     * The minimum required version of FakeBlock.
+     */
+    public static final String REQUIRED_VERSION = "2.0.1";
     /**
      * The instance of {@link BetonQuest}.
      */
@@ -23,8 +33,18 @@ public class FakeBlockIntegrator implements Integrator {
 
     @Override
     public void hook() throws HookException {
+        checkRequiredVersion();
         final Server server = plugin.getServer();
         plugin.registerNonStaticEvent("fakeblock", new FakeBlockEventFactory(server, server.getScheduler(), plugin));
+    }
+
+    private void checkRequiredVersion() throws UnsupportedVersionException {
+        final Plugin fakeBlockPlugin = Bukkit.getPluginManager().getPlugin("fake-block");
+        final Version version = new Version(fakeBlockPlugin.getDescription().getVersion());
+        final VersionComparator comparator = new VersionComparator(UpdateStrategy.MAJOR);
+        if (comparator.isOtherNewerThanCurrent(version, new Version(REQUIRED_VERSION))) {
+            throw new UnsupportedVersionException(plugin, REQUIRED_VERSION);
+        }
     }
 
     @Override
