b1ffbb377ed2f3e50ad9c4df788049958453dc70
MrGuitz <guitzmeyer@gmail.com>
2023-03-08 20:26:17 +0100

    migrated the DeletePointsEvent

---
+++ src/main/java/org/betonquest/betonquest/quest/event/points/DeletePointsEventFactory.java
@@ -0,0 +1,33 @@
+package org.betonquest.betonquest.quest.event.points;
+
+import org.betonquest.betonquest.Instruction;
+import org.betonquest.betonquest.api.quest.event.Event;
+import org.betonquest.betonquest.api.quest.event.EventFactory;
+import org.betonquest.betonquest.api.quest.event.StaticEvent;
+import org.betonquest.betonquest.api.quest.event.StaticEventFactory;
+import org.betonquest.betonquest.exceptions.InstructionParseException;
+import org.betonquest.betonquest.quest.event.NullStaticEventAdapter;
+import org.betonquest.betonquest.utils.Utils;
+
+/**
+ * Factory to create delete points events from {@link Instruction}s.
+ */
+public class DeletePointsEventFactory implements EventFactory, StaticEventFactory {
+
+    /**
+     * Create the delete points event factory.
+     */
+    public DeletePointsEventFactory() {
+    }
+
+    @Override
+    public Event parseEvent(final Instruction instruction) throws InstructionParseException {
+        final String category = Utils.addPackage(instruction.getPackage(), instruction.next());
+        return new DeletePointsEvent(category);
+    }
+
+    @Override
+    public StaticEvent parseStaticEvent(final Instruction instruction) throws InstructionParseException {
+        return new NullStaticEventAdapter(parseEvent(instruction));
+    }
+}
