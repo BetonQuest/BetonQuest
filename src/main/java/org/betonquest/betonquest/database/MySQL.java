e6a87f105c706335526b3165f0fb5e8a16af4adf
Wolf2323 <gabrielpatrikurban@gmail.com>
2023-04-23 20:01:58 +0200

    merged Alter Table statements in MySQL

--- src/main/java/org/betonquest/betonquest/database/MySQL.java
+++ src/main/java/org/betonquest/betonquest/database/MySQL.java
@@ -161,25 +161,22 @@ public class MySQL extends Database {
                     + "CHANGE COLUMN playerID profileID CHAR(36) NOT NULL, "
                     + "MODIFY COLUMN objective VARCHAR(510) NOT NULL, "
                     + "MODIFY COLUMN instructions VARCHAR(2046) NOT NULL, " + "DROP PRIMARY KEY," + "DROP COLUMN id");
-            statement.executeUpdate("ALTER TABLE " + prefix + "objectives ADD PRIMARY KEY (profileID, objective)");
-            statement.executeUpdate("ALTER TABLE " + prefix + "objectives "
+            statement.executeUpdate("ALTER TABLE " + prefix + "objectives " + "ADD PRIMARY KEY (profileID, objective) "
                     + "ADD FOREIGN KEY (profileID) REFERENCES " + prefix + "profile (profileID) ON DELETE CASCADE");
             statement.executeUpdate("DELETE FROM " + prefix + "points WHERE id NOT IN "
                     + "(SELECT MIN(id) FROM " + prefix + "points GROUP BY playerID, category)");
             statement.executeUpdate("ALTER TABLE " + prefix + "points "
                     + "CHANGE COLUMN playerID profileID CHAR(36) NOT NULL, "
                     + "MODIFY COLUMN category VARCHAR(255) NOT NULL, " + "DROP PRIMARY KEY," + "DROP COLUMN id");
-            statement.executeUpdate("ALTER TABLE " + prefix + "points " + "ADD PRIMARY KEY (profileID, category)");
-            statement.executeUpdate("ALTER TABLE " + prefix + "points " + "ADD FOREIGN KEY (profileID) REFERENCES "
-                    + prefix + "profile (profileID) ON DELETE CASCADE");
+            statement.executeUpdate("ALTER TABLE " + prefix + "points " + "ADD PRIMARY KEY (profileID, category) "
+                    + "ADD FOREIGN KEY (profileID) REFERENCES " + prefix + "profile (profileID) ON DELETE CASCADE");
             statement.executeUpdate("DELETE FROM " + prefix + "tags WHERE id NOT IN "
                     + "(SELECT MIN(id) FROM " + prefix + "tags GROUP BY playerID, tag)");
             statement.executeUpdate("ALTER TABLE " + prefix + "tags "
                     + "CHANGE COLUMN playerID profileID CHAR(36) NOT NULL, "
                     + "MODIFY COLUMN tag VARCHAR(510) NOT NULL, " + "DROP PRIMARY KEY," + "DROP COLUMN id");
-            statement.executeUpdate("ALTER TABLE " + prefix + "tags " + "ADD PRIMARY KEY (profileID, tag)");
-            statement.executeUpdate("ALTER TABLE " + prefix + "tags " + "ADD FOREIGN KEY (profileID) REFERENCES "
-                    + prefix + "profile (profileID) ON DELETE CASCADE");
+            statement.executeUpdate("ALTER TABLE " + prefix + "tags " + "ADD PRIMARY KEY (profileID, tag) "
+                    + "ADD FOREIGN KEY (profileID) REFERENCES " + prefix + "profile (profileID) ON DELETE CASCADE");
             statement.executeUpdate("DELETE FROM " + prefix + "player WHERE id NOT IN "
                     + "(SELECT MIN(id) FROM " + prefix + "player GROUP BY playerID)");
             statement.executeUpdate("ALTER TABLE " + prefix + "player "
@@ -187,8 +184,8 @@ public class MySQL extends Database {
                     + "ADD COLUMN active_profile CHAR(36) NOT NULL DEFAULT playerID AFTER playerID, "
                     + "ADD FOREIGN KEY (active_profile) REFERENCES " + prefix
                     + "profile (profileID) ON DELETE RESTRICT, " + "DROP PRIMARY KEY, " + "DROP COLUMN id");
-            statement.executeUpdate("ALTER TABLE " + prefix + "player " + "ADD PRIMARY KEY (playerID)");
-            statement.executeUpdate("ALTER TABLE " + prefix + "player " + "ALTER COLUMN active_profile DROP DEFAULT");
+            statement.executeUpdate("ALTER TABLE " + prefix + "player " + "ADD PRIMARY KEY (playerID) "
+                    + "ALTER COLUMN active_profile DROP DEFAULT");
             statement.executeUpdate("CREATE TABLE " + prefix + "player_profile " + "(playerID CHAR(36) NOT NULL, "
                     + "profileID CHAR(36) NOT NULL, " + "name VARCHAR(510), " + "PRIMARY KEY (profileID, playerID), "
                     + "FOREIGN KEY (playerID) REFERENCES " + prefix + "player (playerID) ON DELETE CASCADE, "
