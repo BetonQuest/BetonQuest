0dae20afc33e7e78789a6b44ddcfae3963cc0dc8
Wolf2323 <gabrielpatrikurban@gmail.com>
2023-05-14 21:44:53 +0200

    small improvements to discord.sh as requested in the review

--- .github/scripts/discord.sh
+++ .github/scripts/discord.sh
@@ -25,7 +25,7 @@ COMMITTER_NAME="$(git log -1 "$GITHUB_SHA" --pretty="%cN")"
 COMMIT_SUBJECT="$(git log -1 "$GITHUB_SHA" --pretty="%s")"
 COMMIT_MESSAGE="$(git log -1 "$GITHUB_SHA" --pretty="%b")"
 #Replace newlines with literal \n
-COMMIT_MESSAGE=$(echo "$COMMIT_MESSAGE" | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g;')
+COMMIT_MESSAGE="$(echo "$COMMIT_MESSAGE" | sed -E ':a;N;$!ba;s/\r?\n|\n?\r/\\n/g;')"
 #Replace double quotes with literal \"
 COMMIT_MESSAGE="${COMMIT_MESSAGE//\"/\\\"}"
 COMMIT_URL="https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
