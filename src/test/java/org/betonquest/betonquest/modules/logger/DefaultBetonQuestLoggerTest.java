49df554a1fd94b2cb7cdf235e33ea66e8e47b8d4
Wolf2323 <gabrielpatrikurban@gmail.com>
2023-06-24 17:52:30 +0200

    improved DefaultBetonQuestLoggerTest

--- src/test/java/org/betonquest/betonquest/modules/logger/DefaultBetonQuestLoggerTest.java
+++ src/test/java/org/betonquest/betonquest/modules/logger/DefaultBetonQuestLoggerTest.java
@@ -7,10 +7,12 @@ import org.bukkit.plugin.Plugin;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatcher;
 
 import java.io.IOException;
 import java.util.logging.Handler;
 import java.util.logging.Level;
+import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 
 import static org.mockito.Mockito.*;
@@ -22,17 +24,28 @@ class DefaultBetonQuestLoggerTest {
     /**
      * The {@link QuestPackage} name.
      */
-    public static final String CUSTOM_CONFIG_PACKAGE = "CustomTestPackage";
+    public static final String PACKAGE_NAME = "CustomTestPackage";
+
+    /**
+     * The additional stacktrace message.
+     */
+    public static final String STACKTRACE_MESSAGE = "Additional stacktrace:";
+
+    /**
+     *
+     */
+    public static final String REPORT_MESSAGE = "This is an exception that should never occur. "
+            + "If you don't know why this occurs please report it to the author.";
+
+    /**
+     * The {@link IOException} for testing.
+     */
+    public static final IOException IO_EXCEPTION = new IOException("Test Exception");
 
     /**
      * The log message.
      */
     private static final String LOG_MESSAGE = "Test Message";
-
-    /**
-     * The exception message.
-     */
-    private static final String EXCEPTION_MESSAGE = "Test Exception";
 
     /**
      * The {@link Handler} for testing.
@@ -56,7 +69,7 @@ class DefaultBetonQuestLoggerTest {
         parentLogger.addHandler(handler);
         this.logger = new DefaultBetonQuestLogger(mock(Plugin.class), parentLogger, getClass(), null);
         this.questPackage = mock(QuestPackage.class);
-        when(this.questPackage.getQuestPath()).thenReturn(CUSTOM_CONFIG_PACKAGE);
+        when(this.questPackage.getQuestPath()).thenReturn(PACKAGE_NAME);
     }
 
     @AfterEach
@@ -67,115 +80,182 @@ class DefaultBetonQuestLoggerTest {
     @Test
     void debug() {
         logger.debug(LOG_MESSAGE);
-        verify(handler, times(1)).publish(argThat(record -> record.getLevel() == Level.FINE && LOG_MESSAGE.equals(record.getMessage())));
+        verify(handler, times(1)).publish(argThat(new RecordMatcher(Level.FINE, LOG_MESSAGE)));
     }
 
     @Test
     void debugWithPackage() {
         logger.debug(questPackage, LOG_MESSAGE);
-        verify(handler, times(1)).publish(argThat(record -> record.getLevel() == Level.FINE && LOG_MESSAGE.equals(record.getMessage())));
-        verifyNoMoreInteractions(handler);
+        verify(handler, times(1)).publish(argThat(new RecordMatcher(PACKAGE_NAME, Level.FINE, LOG_MESSAGE)));
     }
 
     @Test
     void debugException() {
-        logger.debug(LOG_MESSAGE, new IOException(EXCEPTION_MESSAGE));
-        verify(handler, times(1)).publish(argThat(record -> record.getLevel() == Level.FINE && LOG_MESSAGE.equals(record.getMessage())));
-        verifyNoMoreInteractions(handler);
+        logger.debug(LOG_MESSAGE, IO_EXCEPTION);
+        verify(handler, times(1)).publish(argThat(new RecordMatcher(Level.FINE, LOG_MESSAGE, IO_EXCEPTION)));
     }
 
     @Test
     void debugExceptionWithPackage() {
-        logger.debug(questPackage, LOG_MESSAGE, new IOException(EXCEPTION_MESSAGE));
-        verify(handler, times(1)).publish(argThat(record -> record.getLevel() == Level.FINE && LOG_MESSAGE.equals(record.getMessage())));
-        verifyNoMoreInteractions(handler);
+        logger.debug(questPackage, LOG_MESSAGE, IO_EXCEPTION);
+        verify(handler, times(1)).publish(argThat(new RecordMatcher(PACKAGE_NAME, Level.FINE, LOG_MESSAGE, IO_EXCEPTION)));
     }
 
     @Test
     void info() {
         logger.info(LOG_MESSAGE);
-        verify(handler, times(1)).publish(argThat(record -> record.getLevel() == Level.INFO && LOG_MESSAGE.equals(record.getMessage())));
-        verifyNoMoreInteractions(handler);
+        verify(handler, times(1)).publish(argThat(new RecordMatcher(Level.INFO, LOG_MESSAGE)));
     }
 
     @Test
     void infoWithPackage() {
         logger.info(questPackage, LOG_MESSAGE);
-        verify(handler, times(1)).publish(argThat(record -> record.getLevel() == Level.INFO && LOG_MESSAGE.equals(record.getMessage())));
-        verifyNoMoreInteractions(handler);
+        verify(handler, times(1)).publish(argThat(new RecordMatcher(PACKAGE_NAME, Level.INFO, LOG_MESSAGE)));
     }
 
     @Test
     void warn() {
         logger.warn(LOG_MESSAGE);
-        verify(handler, times(1)).publish(argThat(record -> record.getLevel() == Level.WARNING && LOG_MESSAGE.equals(record.getMessage())));
-        verifyNoMoreInteractions(handler);
+        verify(handler, times(1)).publish(argThat(new RecordMatcher(Level.WARNING, LOG_MESSAGE)));
     }
 
     @Test
     void warnWithPackage() {
         logger.warn(questPackage, LOG_MESSAGE);
-        verify(handler, times(1)).publish(argThat(record -> record.getLevel() == Level.WARNING && LOG_MESSAGE.equals(record.getMessage())));
-        verifyNoMoreInteractions(handler);
+        verify(handler, times(1)).publish(argThat(new RecordMatcher(PACKAGE_NAME, Level.WARNING, LOG_MESSAGE)));
     }
 
     @Test
     void warnException() {
-        logger.warn(LOG_MESSAGE, new IOException(EXCEPTION_MESSAGE));
-        verify(handler, times(1)).publish(argThat(record -> record.getLevel() == Level.WARNING && LOG_MESSAGE.equals(record.getMessage())));
-        verify(handler, times(1)).publish(argThat(record -> record.getLevel() == Level.FINE && "Additional stacktrace:".equals(record.getMessage())));
-        verifyNoMoreInteractions(handler);
+        logger.warn(LOG_MESSAGE, IO_EXCEPTION);
+        verify(handler, times(1)).publish(argThat(new RecordMatcher(Level.WARNING, LOG_MESSAGE)));
+        verify(handler, times(1)).publish(argThat(new RecordMatcher(Level.FINE, STACKTRACE_MESSAGE, IO_EXCEPTION)));
     }
 
     @Test
     void warnExceptionWithPackage() {
-        logger.warn(questPackage, LOG_MESSAGE, new IOException(EXCEPTION_MESSAGE));
-        verify(handler, times(1)).publish(argThat(record -> record.getLevel() == Level.WARNING && LOG_MESSAGE.equals(record.getMessage())));
-        verify(handler, times(1)).publish(argThat(record -> record.getLevel() == Level.FINE && "Additional stacktrace:".equals(record.getMessage())));
-        verifyNoMoreInteractions(handler);
+        logger.warn(questPackage, LOG_MESSAGE, IO_EXCEPTION);
+        verify(handler, times(1)).publish(argThat(new RecordMatcher(PACKAGE_NAME, Level.WARNING, LOG_MESSAGE)));
+        verify(handler, times(1)).publish(argThat(new RecordMatcher(PACKAGE_NAME, Level.FINE, STACKTRACE_MESSAGE, IO_EXCEPTION)));
     }
 
     @Test
     void error() {
         logger.error(LOG_MESSAGE);
-        verify(handler, times(1)).publish(argThat(record -> record.getLevel() == Level.SEVERE && LOG_MESSAGE.equals(record.getMessage())));
-        verifyNoMoreInteractions(handler);
+        verify(handler, times(1)).publish(argThat(new RecordMatcher(Level.SEVERE, LOG_MESSAGE)));
     }
 
     @Test
     void errorWithPackage() {
         logger.error(questPackage, LOG_MESSAGE);
-        verify(handler, times(1)).publish(argThat(record -> record.getLevel() == Level.SEVERE && LOG_MESSAGE.equals(record.getMessage())));
-        verifyNoMoreInteractions(handler);
+        verify(handler, times(1)).publish(argThat(new RecordMatcher(PACKAGE_NAME, Level.SEVERE, LOG_MESSAGE)));
     }
 
     @Test
     void errorException() {
-        logger.error(LOG_MESSAGE, new IOException(EXCEPTION_MESSAGE));
-        verify(handler, times(1)).publish(argThat(record -> record.getLevel() == Level.SEVERE && LOG_MESSAGE.equals(record.getMessage())));
-        verifyNoMoreInteractions(handler);
+        logger.error(LOG_MESSAGE, IO_EXCEPTION);
+        verify(handler, times(1)).publish(argThat(new RecordMatcher(Level.SEVERE, LOG_MESSAGE, IO_EXCEPTION)));
     }
 
     @Test
     void errorExceptionWithPackage() {
-        logger.error(questPackage, LOG_MESSAGE, new IOException(EXCEPTION_MESSAGE));
-        verify(handler, times(1)).publish(argThat(record -> record.getLevel() == Level.SEVERE && LOG_MESSAGE.equals(record.getMessage())));
-        verifyNoMoreInteractions(handler);
+        logger.error(questPackage, LOG_MESSAGE, IO_EXCEPTION);
+        verify(handler, times(1)).publish(argThat(new RecordMatcher(PACKAGE_NAME, Level.SEVERE, LOG_MESSAGE, IO_EXCEPTION)));
     }
 
     @Test
     void reportException() {
-        logger.reportException(new IOException(EXCEPTION_MESSAGE));
-        verify(handler, times(1)).publish(argThat(record -> record.getLevel() == Level.SEVERE && record.getMessage().equals("This is an exception that should never occur. "
-                + "If you don't know why this occurs please report it to the author.")));
-        verifyNoMoreInteractions(handler);
+        logger.reportException(IO_EXCEPTION);
+        verify(handler, times(1)).publish(argThat(new RecordMatcher(Level.SEVERE, REPORT_MESSAGE, IO_EXCEPTION)));
     }
 
     @Test
     void reportExceptionWithPackage() {
-        logger.reportException(questPackage, new IOException(EXCEPTION_MESSAGE));
-        verify(handler, times(1)).publish(argThat(record -> record.getLevel() == Level.SEVERE && record.getMessage().equals("This is an exception that should never occur. "
-                + "If you don't know why this occurs please report it to the author.")));
-        verifyNoMoreInteractions(handler);
+        logger.reportException(questPackage, IO_EXCEPTION);
+        verify(handler, times(1)).publish(argThat(new RecordMatcher(PACKAGE_NAME, Level.SEVERE, REPORT_MESSAGE, IO_EXCEPTION)));
+    }
+
+    /**
+     * Argument matcher for {@link LogRecord}s.
+     */
+    private static class RecordMatcher implements ArgumentMatcher<LogRecord> {
+        /**
+         * The package name to match.
+         */
+        private final String pack;
+
+        /**
+         * The level to match.
+         */
+        private final Level level;
+
+        /**
+         * The message to match.
+         */
+        private final String message;
+
+        /**
+         * The exception to match.
+         */
+        private final Exception exception;
+
+        /**
+         * Creates a new record matcher.
+         *
+         * @param level   The level
+         * @param message The message
+         */
+        public RecordMatcher(final Level level, final String message) {
+            this(null, level, message, null);
+        }
+
+        /**
+         * Creates a new record matcher.
+         *
+         * @param level     The level
+         * @param message   The message
+         * @param exception The exception
+         */
+        public RecordMatcher(final Level level, final String message, final Exception exception) {
+            this(null, level, message, exception);
+        }
+
+        /**
+         * Creates a new record matcher.
+         *
+         * @param pack    The package name
+         * @param level   The level
+         * @param message The message
+         */
+        public RecordMatcher(final String pack, final Level level, final String message) {
+            this(pack, level, message, null);
+        }
+
+        /**
+         * Creates a new record matcher.
+         *
+         * @param pack      The package name
+         * @param level     The level
+         * @param message   The message
+         * @param exception The exception
+         */
+        public RecordMatcher(final String pack, final Level level, final String message, final Exception exception) {
+            this.pack = pack;
+            this.level = level;
+            this.message = message;
+            this.exception = exception;
+        }
+
+        @Override
+        public boolean matches(final LogRecord record) {
+            if (!(record instanceof final BetonQuestLogRecord betonQuestLogRecord)) {
+                return false;
+            }
+            final String pack = betonQuestLogRecord.getPack().orElse(null);
+
+            final boolean packMatch = this.pack == null && pack == null || this.pack != null && this.pack.equals(pack);
+            final boolean exceptionMatch = exception == null && record.getThrown() == null || exception != null && exception.equals(record.getThrown());
+            return packMatch && exceptionMatch && record.getLevel().equals(level) && message.equals(record.getMessage());
+        }
     }
 }
