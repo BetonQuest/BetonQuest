cc4ff4a8f59edb60fe9c9b8dbf882b04fc59e1e6
MrGuitz <guitzmeyer@gmail.com>
2023-08-13 14:51:34 +0200

    Changeover from the ChestGiveEvent to the new API

---
+++ src/main/java/org/betonquest/betonquest/quest/event/chest/ChestGiveEvent.java
@@ -0,0 +1,80 @@
+package org.betonquest.betonquest.quest.event.chest;
+
+import org.betonquest.betonquest.Instruction.Item;
+import org.betonquest.betonquest.api.profiles.Profile;
+import org.betonquest.betonquest.api.quest.event.Event;
+import org.betonquest.betonquest.exceptions.QuestRuntimeException;
+import org.betonquest.betonquest.item.QuestItem;
+import org.betonquest.betonquest.utils.location.CompoundLocation;
+import org.bukkit.block.Block;
+import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Puts the items in the inventory of a block or drops them if the inventory is full.
+ */
+public class ChestGiveEvent implements Event {
+    /**
+     * The items to put in the blocks inventory.
+     */
+    private final Item[] questItems;
+
+    /**
+     * The location of the block.
+     */
+    private final CompoundLocation location;
+
+    /**
+     * Create the chest give event.
+     *
+     * @param questItems the items to put in the blocks inventory
+     * @param location   the location of the block
+     */
+    public ChestGiveEvent(final CompoundLocation location, final Item... questItems) {
+        this.questItems = Arrays.copyOf(questItems, questItems.length);
+        this.location = location;
+    }
+
+    @Override
+    public void execute(final Profile profile) throws QuestRuntimeException {
+        final Block block = location.getLocation(profile).getBlock();
+        final InventoryHolder chest;
+        try {
+            chest = (InventoryHolder) block.getState();
+        } catch (final ClassCastException e) {
+            throw new QuestRuntimeException("Trying to put items in chest, but there's no chest! Location: X"
+                    + block.getX() + " Y" + block.getY() + " Z" + block.getZ(), e);
+        }
+        final HashMap<Integer, ItemStack> left = chest.getInventory().addItem(getItemStacks(profile));
+        for (final ItemStack itemStack : left.values()) {
+            block.getWorld().dropItem(block.getLocation(), itemStack);
+        }
+    }
+
+    /**
+     * Converts the quest items to item stacks.
+     *
+     * @param profile the profile of the player
+     * @return the item stacks
+     */
+    private ItemStack[] getItemStacks(final Profile profile) {
+        final List<ItemStack> itemStacks = new ArrayList<>();
+        Arrays.stream(questItems).toList().forEach(item -> {
+            final QuestItem questItem = item.getItem();
+            int amount = item.getAmount().getInt(profile);
+            while (amount > 0) {
+                final ItemStack itemStackTemplate = questItem.generate(1, profile);
+                final int stackSize = Math.min(amount, itemStackTemplate.getMaxStackSize());
+                final ItemStack itemStack = questItem.generate(stackSize);
+                itemStacks.add(itemStack);
+                amount = amount - stackSize;
+            }
+        });
+        return itemStacks.toArray(ItemStack[]::new);
+    }
+}
