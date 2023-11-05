434515f7af6189c0cdbcb307048fcd001b75c089
Jonas <17405009+J0B10@users.noreply.github.com>
2023-11-05 10:22:32 +0100

    Fixe uncaught Exception when parsing %player% for offline player

--- src/main/java/org/betonquest/betonquest/variables/PlayerNameVariable.java
+++ src/main/java/org/betonquest/betonquest/variables/PlayerNameVariable.java
@@ -1,9 +1,13 @@
 package org.betonquest.betonquest.variables;
 
+import org.betonquest.betonquest.BetonQuest;
 import org.betonquest.betonquest.Instruction;
 import org.betonquest.betonquest.api.Variable;
+import org.betonquest.betonquest.api.logger.BetonQuestLogger;
+import org.betonquest.betonquest.api.profiles.OnlineProfile;
 import org.betonquest.betonquest.api.profiles.Profile;
-import org.bukkit.entity.Player;
+
+import java.util.Optional;
 
 /**
  * This variable resolves into the player's name. It can has optional "display"
@@ -11,6 +15,8 @@ import org.bukkit.entity.Player;
  */
 @SuppressWarnings("PMD.CommentRequired")
 public class PlayerNameVariable extends Variable {
+
+    private final BetonQuestLogger log = BetonQuest.getInstance().getLoggerFactory().create(getClass());
 
     private final boolean display;
 
@@ -21,8 +27,16 @@ public class PlayerNameVariable extends Variable {
 
     @Override
     public String getValue(final Profile profile) {
-        final Player player = profile.getOnlineProfile().get().getPlayer();
-        return display ? player.getDisplayName() : player.getName();
+        if (display) {
+            final Optional<OnlineProfile> onlineProfile = profile.getOnlineProfile();
+            if (onlineProfile.isEmpty()) {
+                log.warn(instruction.getPackage(), profile.getPlayer().getName() + " is offline, cannot get display name");
+                return "";
+            }
+            return onlineProfile.get().getPlayer().getDisplayName();
+        } else {
+            return profile.getPlayer().getName();
+        }
     }
 
 }
