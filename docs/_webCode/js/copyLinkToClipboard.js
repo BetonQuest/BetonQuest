175247cef6e142df615e9026560263c3a181fdbc
CyberOtter <saltyaimbotter@gmail.com>
2023-12-23 18:43:02 +0100

    [DOCS] make custom JS compatible with instant loading

--- docs/_webCode/js/copyLinkToClipboard.js
+++ docs/_webCode/js/copyLinkToClipboard.js
@@ -1,14 +1,17 @@
+//mkdocs-material provided observable that is called when the page is fully loaded. Respects both default and "instant loading" of pages.
+document$.subscribe(function () {
 //Adds an EventListener to all "chain link" elements next to headings.
-let links = document.getElementsByClassName("headerlink");
-for (let i = 0; i < links.length; i++) {
-  links[i].addEventListener("click", onClick);
-}
+  let links = document.getElementsByClassName("headerlink");
+  for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", onClick);
+  }
 
 //Copies the current URL into the users clipboard.
 //This needs to be delayed by a bit since the browser needs to update the URL before it's copied.
-function onClick() {
-  setTimeout(() => {
-    let url = window.location.href;
-    navigator.clipboard.writeText(url);
-  }, 10);
-}
+  function onClick() {
+    setTimeout(() => {
+      let url = window.location.href;
+      navigator.clipboard.writeText(url);
+    }, 10);
+  }
+});
