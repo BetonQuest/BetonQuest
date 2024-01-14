1c06b2494b27d354be86618ff449d151175fc153
CyberOtter <saltyaimbotter@gmail.com>
2024-01-14 23:43:05 +0100

    [DOCS] bump mike to fix CI build

--- config/setup-docs-dependencies.py
+++ config/setup-docs-dependencies.py
@@ -5,7 +5,7 @@ import subprocess
 
 common_dependencies = [
     'mkdocs-snippets',
-    'mike==2.0.0',
+    f'git+https://github.com/jimporter/mike@01446f8a2ddd246cab1e2d72407576c941c6d702',
     'mkdocs-exclude==1.0.2',
 ]
 
@@ -22,12 +22,12 @@ def get_install_requires():
                 + "so if you serve mkdocs you will serve the normal version!"
                 + "\033[0m")
         return [
-            f'git+https://{mkdocs_token}@github.com/squidfunk/mkdocs-material-insiders.git@9.5.2-insiders-4.47.1',
+            f'git+https://{mkdocs_token}@github.com/squidfunk/mkdocs-material-insiders.git@9.5.3-insiders-4.49.2',
             'pillow',
             'cairosvg',
         ] + common_dependencies
     else:
-        return ['mkdocs-material==9.5.2'] + common_dependencies
+        return ['mkdocs-material==9.5.3'] + common_dependencies
 
 
 subprocess.run(['pip', 'install'] + get_install_requires())
