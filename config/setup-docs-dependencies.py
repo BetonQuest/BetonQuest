e9e48007869cefa4dccb7f3be2edd1ac1c28eb8d
CyberOtter <40303883+saltyaimbotter@users.noreply.github.com>
2024-02-04 21:18:07 +0100

    [DOCS] update mkdocs and re-enable instant loading
    * adjusted download.js script to work with instant loading

--- config/setup-docs-dependencies.py
+++ config/setup-docs-dependencies.py
@@ -22,12 +22,12 @@ def get_install_requires():
                 + "so if you serve mkdocs you will serve the normal version!"
                 + "\033[0m")
         return [
-            f'git+https://{mkdocs_token}@github.com/squidfunk/mkdocs-material-insiders.git@9.5.3-insiders-4.49.2',
+            f'git+https://{mkdocs_token}@github.com/squidfunk/mkdocs-material-insiders.git@9.5.9-insiders-4.52.2',
             'pillow',
             'cairosvg',
         ] + common_dependencies
     else:
-        return ['mkdocs-material==9.5.3'] + common_dependencies
+        return ['mkdocs-material==9.5.9'] + common_dependencies
 
 
 subprocess.run(['pip', 'install'] + get_install_requires())
