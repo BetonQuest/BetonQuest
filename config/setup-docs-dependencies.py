9d4976b99a3e4e1c88e3359e042ccda558d9cd6c
Wolf2323 <gabrielpatrikurban@gmail.com>
2024-05-18 11:37:18 +0200

    fixed version of docs during releases and version skips
    - DEV alias is included in the release version, so it still exists until the next dev build.
    - delete the correct version in case of a version skip with the dev alias on main.

--- config/setup-docs-dependencies.py
+++ config/setup-docs-dependencies.py
@@ -1,11 +1,9 @@
 import os
-import platform
 import subprocess
-
 
 common_dependencies = [
     'mkdocs-snippets',
-    f'git+https://github.com/jimporter/mike@01446f8a2ddd246cab1e2d72407576c941c6d702',
+    'mike==2.1.1',
     'mkdocs-exclude==1.0.2',
 ]
 
@@ -17,10 +15,10 @@ def get_install_requires():
         mkdocs_enabled = os.environ.get('MKDOCS_MATERIAL_INSIDERS_ENABLED')
         if not mkdocs_enabled:
             print("\033[93m"
-                + "Warning: 'MKDOCS_MATERIAL_INSIDERS' is set "
-                + "but 'MKDOCS_MATERIAL_INSIDERS_ENABLED' is not set to 'true', "
-                + "so if you serve mkdocs you will serve the normal version!"
-                + "\033[0m")
+                  + "Warning: 'MKDOCS_MATERIAL_INSIDERS' is set "
+                  + "but 'MKDOCS_MATERIAL_INSIDERS_ENABLED' is not set to 'true', "
+                  + "so if you serve mkdocs you will serve the normal version!"
+                  + "\033[0m")
         return [
             f'git+https://{mkdocs_token}@github.com/squidfunk/mkdocs-material-insiders.git@9.5.9-insiders-4.52.2',
             'pillow',
