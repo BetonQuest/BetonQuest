2db756709a01949257b3d6f5f74d670f98b07ab1
TheosRee <theosree@users.noreply.github.com>
2024-05-25 22:13:54 +0200

    fix PMD

--- src/main/java/org/betonquest/betonquest/quest/registry/CoreQuestTypes.java
+++ src/main/java/org/betonquest/betonquest/quest/registry/CoreQuestTypes.java
@@ -164,6 +164,7 @@ import java.time.InstantSource;
 /**
  * Registers the Conditions, Events, Objectives and Variables that come with BetonQuest.
  */
+@SuppressWarnings("PMD.CouplingBetweenObjects")
 public class CoreQuestTypes {
     /**
      * Logger Factory to create new custom Logger from.
