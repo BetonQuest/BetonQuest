484bbcf897793e77af76c93bcd80852fdbad7c0c
TheosRee <theosree@users.noreply.github.com>
2024-05-25 21:25:19 +0200

    move conditions a to h

--- src/main/java/org/betonquest/betonquest/quest/registry/CoreQuestTypes.java
+++ src/main/java/org/betonquest/betonquest/quest/registry/CoreQuestTypes.java
@@ -2,6 +2,28 @@ package org.betonquest.betonquest.quest.registry;
 
 import org.betonquest.betonquest.BetonQuest;
 import org.betonquest.betonquest.api.logger.BetonQuestLoggerFactory;
+import org.betonquest.betonquest.conditions.AdvancementCondition;
+import org.betonquest.betonquest.conditions.ArmorCondition;
+import org.betonquest.betonquest.conditions.BiomeCondition;
+import org.betonquest.betonquest.conditions.BurningCondition;
+import org.betonquest.betonquest.conditions.CheckCondition;
+import org.betonquest.betonquest.conditions.ChestItemCondition;
+import org.betonquest.betonquest.conditions.ConjunctionCondition;
+import org.betonquest.betonquest.conditions.ConversationCondition;
+import org.betonquest.betonquest.conditions.DayOfWeekCondition;
+import org.betonquest.betonquest.conditions.EffectCondition;
+import org.betonquest.betonquest.conditions.EmptySlotsCondition;
+import org.betonquest.betonquest.conditions.EntityCondition;
+import org.betonquest.betonquest.conditions.ExperienceCondition;
+import org.betonquest.betonquest.conditions.FacingCondition;
+import org.betonquest.betonquest.conditions.FlyingCondition;
+import org.betonquest.betonquest.conditions.GameModeCondition;
+import org.betonquest.betonquest.conditions.GlobalPointCondition;
+import org.betonquest.betonquest.conditions.GlobalTagCondition;
+import org.betonquest.betonquest.conditions.HandCondition;
+import org.betonquest.betonquest.conditions.HealthCondition;
+import org.betonquest.betonquest.conditions.HeightCondition;
+import org.betonquest.betonquest.conditions.HungerCondition;
 import org.bukkit.Server;
 import org.bukkit.scheduler.BukkitScheduler;
 
@@ -57,6 +79,28 @@ public class CoreQuestTypes {
     }
 
     private void registerConditions() {
+        plugin.registerConditions("advancement", AdvancementCondition.class);
+        plugin.registerConditions("and", ConjunctionCondition.class);
+        plugin.registerConditions("armor", ArmorCondition.class);
+        plugin.registerConditions("biome", BiomeCondition.class);
+        plugin.registerConditions("burning", BurningCondition.class);
+        plugin.registerConditions("check", CheckCondition.class);
+        plugin.registerConditions("chestitem", ChestItemCondition.class);
+        plugin.registerConditions("conversation", ConversationCondition.class);
+        plugin.registerConditions("dayofweek", DayOfWeekCondition.class);
+        plugin.registerConditions("effect", EffectCondition.class);
+        plugin.registerConditions("empty", EmptySlotsCondition.class);
+        plugin.registerConditions("entities", EntityCondition.class);
+        plugin.registerConditions("experience", ExperienceCondition.class);
+        plugin.registerConditions("facing", FacingCondition.class);
+        plugin.registerConditions("fly", FlyingCondition.class);
+        plugin.registerConditions("gamemode", GameModeCondition.class);
+        plugin.registerConditions("globalpoint", GlobalPointCondition.class);
+        plugin.registerConditions("globaltag", GlobalTagCondition.class);
+        plugin.registerConditions("hand", HandCondition.class);
+        plugin.registerConditions("health", HealthCondition.class);
+        plugin.registerConditions("height", HeightCondition.class);
+        plugin.registerConditions("hunger", HungerCondition.class);
     }
 
     private void registerEvents() {
