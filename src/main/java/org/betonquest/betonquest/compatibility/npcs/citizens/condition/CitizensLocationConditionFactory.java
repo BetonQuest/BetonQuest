04c39979e969f741dc8ae4cfacbf1877560866e8
TheosRee <theosree@users.noreply.github.com>
2024-07-09 20:04:25 +0200

    use supplierStandard as Factory parameter; suppressions; remove CitizensNPCSupplier.java

--- src/main/java/org/betonquest/betonquest/compatibility/npcs/citizens/condition/CitizensLocationConditionFactory.java
+++ src/main/java/org/betonquest/betonquest/compatibility/npcs/citizens/condition/CitizensLocationConditionFactory.java
@@ -3,9 +3,9 @@ package org.betonquest.betonquest.compatibility.npcs.citizens.condition;
 import org.betonquest.betonquest.Instruction;
 import org.betonquest.betonquest.api.quest.condition.PlayerCondition;
 import org.betonquest.betonquest.api.quest.condition.PlayerlessCondition;
+import org.betonquest.betonquest.compatibility.npcs.abstractnpc.NPCSupplierStandard;
 import org.betonquest.betonquest.compatibility.npcs.abstractnpc.condition.location.NPCLocationCondition;
 import org.betonquest.betonquest.compatibility.npcs.abstractnpc.condition.location.NPCLocationConditionFactory;
-import org.betonquest.betonquest.compatibility.npcs.citizens.CitizensNPCSupplier;
 import org.betonquest.betonquest.exceptions.InstructionParseException;
 import org.betonquest.betonquest.quest.PrimaryServerThreadData;
 import org.betonquest.betonquest.quest.condition.PrimaryServerThreadPlayerCondition;
@@ -23,10 +23,11 @@ public class CitizensLocationConditionFactory extends NPCLocationConditionFactor
     /**
      * Create a new factory for Citizens NPC location conditions.
      *
-     * @param data the data used for primary server thread access
+     * @param supplierStandard the supplier providing the npc adapter supplier
+     * @param data             the data used for primary server thread access
      */
-    public CitizensLocationConditionFactory(final PrimaryServerThreadData data) {
-        super(CitizensNPCSupplier::getSupplierByIDStatic);
+    public CitizensLocationConditionFactory(final NPCSupplierStandard supplierStandard, final PrimaryServerThreadData data) {
+        super(supplierStandard);
         this.data = data;
     }
 
--- src/main/java/org/betonquest/betonquest/compatibility/npcs/citizens/variable/npc/CitizensVariableFactory.java
+++ src/main/java/org/betonquest/betonquest/compatibility/npcs/citizens/variable/npc/CitizensVariableFactory.java
@@ -2,8 +2,8 @@ package org.betonquest.betonquest.compatibility.npcs.citizens.variable.npc;
 
 import net.citizensnpcs.api.npc.NPC;
 import org.betonquest.betonquest.api.logger.BetonQuestLoggerFactory;
+import org.betonquest.betonquest.compatibility.npcs.abstractnpc.NPCSupplierStandard;
 import org.betonquest.betonquest.compatibility.npcs.abstractnpc.variable.npc.NPCVariableFactory;
-import org.betonquest.betonquest.compatibility.npcs.citizens.CitizensNPCSupplier;
 
 /**
  * Factory to create NPCVariables with {@link NPC Citizens NPC}s from Instructions.
@@ -12,9 +12,10 @@ public class CitizensVariableFactory extends NPCVariableFactory {
     /**
      * Create a new NPCVariable factory for Citizens NPCs.
      *
-     * @param loggerFactory the logger factory creating new custom logger
+     * @param supplierStandard the supplier providing the npc adapter supplier
+     * @param loggerFactory    the logger factory creating new custom logger
      */
-    public CitizensVariableFactory(final BetonQuestLoggerFactory loggerFactory) {
-        super(loggerFactory, CitizensNPCSupplier::getSupplierByIDStatic);
+    public CitizensVariableFactory(final NPCSupplierStandard supplierStandard, final BetonQuestLoggerFactory loggerFactory) {
+        super(loggerFactory, supplierStandard);
     }
 }
