f0d63a6bee2fdeb811bf93ae8211c06e2f9a303c
TheosRee <theosree@users.noreply.github.com>
2024-07-09 22:35:11 +0200

    move Citizens to new npcs package

---
+++ src/main/java/org/betonquest/betonquest/compatibility/npcs/citizens/CitizensIntegrator.java
@@ -0,0 +1,128 @@
+package org.betonquest.betonquest.compatibility.npcs.citizens;
+
+import org.betonquest.betonquest.BetonQuest;
+import org.betonquest.betonquest.api.logger.BetonQuestLoggerFactory;
+import org.betonquest.betonquest.compatibility.Compatibility;
+import org.betonquest.betonquest.compatibility.Integrator;
+import org.betonquest.betonquest.compatibility.npcs.citizens.condition.distance.NPCDistanceConditionFactory;
+import org.betonquest.betonquest.compatibility.npcs.citizens.condition.location.NPCLocationConditionFactory;
+import org.betonquest.betonquest.compatibility.npcs.citizens.condition.region.NPCRegionConditionFactory;
+import org.betonquest.betonquest.compatibility.npcs.citizens.event.move.CitizensMoveController;
+import org.betonquest.betonquest.compatibility.npcs.citizens.event.move.CitizensMoveEvent;
+import org.betonquest.betonquest.compatibility.npcs.citizens.event.move.CitizensMoveEventFactory;
+import org.betonquest.betonquest.compatibility.npcs.citizens.event.move.CitizensStopEventFactory;
+import org.betonquest.betonquest.compatibility.npcs.citizens.event.teleport.NPCTeleportEventFactory;
+import org.betonquest.betonquest.compatibility.npcs.citizens.objective.NPCInteractObjective;
+import org.betonquest.betonquest.compatibility.npcs.citizens.objective.NPCKillObjective;
+import org.betonquest.betonquest.compatibility.npcs.citizens.objective.NPCRangeObjective;
+import org.betonquest.betonquest.compatibility.npcs.citizens.variable.npc.CitizensVariableFactory;
+import org.betonquest.betonquest.compatibility.protocollib.hider.NPCHider;
+import org.betonquest.betonquest.compatibility.protocollib.hider.UpdateVisibilityNowEvent;
+import org.betonquest.betonquest.quest.PrimaryServerThreadData;
+import org.betonquest.betonquest.quest.registry.QuestTypeRegistries;
+import org.betonquest.betonquest.quest.registry.type.ConditionTypeRegistry;
+import org.betonquest.betonquest.quest.registry.type.EventTypeRegistry;
+import org.bukkit.Server;
+import org.bukkit.event.HandlerList;
+import org.bukkit.scheduler.BukkitScheduler;
+
+/**
+ * Integrator for Citizens.
+ */
+@SuppressWarnings("NullAway.Init")
+public class CitizensIntegrator implements Integrator {
+    /**
+     * The active integrator instance.
+     */
+    private static CitizensIntegrator instance;
+
+    /**
+     * The BetonQuest plugin instance.
+     */
+    private final BetonQuest plugin;
+
+    /**
+     * Starts conversations on NPC interaction.
+     */
+    private CitizensConversationStarter citizensConversationStarter;
+
+    /**
+     * Handles NPC movement of the {@link CitizensMoveEvent}.
+     */
+    private CitizensMoveController citizensMoveController;
+
+    /**
+     * The default Constructor.
+     */
+    @SuppressWarnings("PMD.AssignmentToNonFinalStatic")
+    public CitizensIntegrator() {
+        instance = this;
+        plugin = BetonQuest.getInstance();
+    }
+
+    /**
+     * Gets the move controller used to start and stop NPC movement.
+     *
+     * @return the move controller of this NPC integration
+     */
+    public static CitizensMoveController getCitizensMoveInstance() {
+        return instance.citizensMoveController;
+    }
+
+    @Override
+    public void hook() {
+        final BetonQuestLoggerFactory loggerFactory = plugin.getLoggerFactory();
+        citizensMoveController = new CitizensMoveController(loggerFactory.create(CitizensMoveController.class));
+        citizensConversationStarter = new CitizensConversationStarter(loggerFactory, loggerFactory.create(CitizensConversationStarter.class), citizensMoveController);
+        new CitizensWalkingListener();
+
+        // if ProtocolLib is hooked, start NPCHider
+        if (Compatibility.getHooked().contains("ProtocolLib")) {
+            NPCHider.start(loggerFactory.create(NPCHider.class));
+            plugin.registerEvents("updatevisibility", UpdateVisibilityNowEvent.class);
+        }
+        plugin.registerObjectives("npckill", NPCKillObjective.class);
+        plugin.registerObjectives("npcinteract", NPCInteractObjective.class);
+        plugin.registerObjectives("npcrange", NPCRangeObjective.class);
+
+        final Server server = plugin.getServer();
+        final BukkitScheduler scheduler = server.getScheduler();
+        final PrimaryServerThreadData data = new PrimaryServerThreadData(server, scheduler, plugin);
+
+        server.getPluginManager().registerEvents(citizensMoveController, plugin);
+
+        final QuestTypeRegistries questRegistries = plugin.getQuestRegistries();
+        final EventTypeRegistry eventTypes = questRegistries.getEventTypes();
+        eventTypes.register("movenpc", new CitizensMoveEventFactory(data, citizensMoveController));
+        eventTypes.register("stopnpc", new CitizensStopEventFactory(data, citizensMoveController));
+        eventTypes.registerCombined("teleportnpc", new NPCTeleportEventFactory(data));
+
+        plugin.registerConversationIO("chest", CitizensInventoryConvIO.class);
+        plugin.registerConversationIO("combined", CitizensInventoryConvIO.CitizensCombined.class);
+
+        questRegistries.getVariableTypes().register("citizen", new CitizensVariableFactory(loggerFactory));
+
+        final ConditionTypeRegistry conditionTypes = questRegistries.getConditionTypes();
+        conditionTypes.register("npcdistance", new NPCDistanceConditionFactory(data, loggerFactory));
+        conditionTypes.registerCombined("npclocation", new NPCLocationConditionFactory(data));
+    }
+
+    @Override
+    public void postHook() {
+        if (Compatibility.getHooked().contains("WorldGuard")) {
+            final Server server = plugin.getServer();
+            final PrimaryServerThreadData data = new PrimaryServerThreadData(server, server.getScheduler(), plugin);
+            plugin.getQuestRegistries().getConditionTypes().register("npcregion", new NPCRegionConditionFactory(data));
+        }
+    }
+
+    @Override
+    public void reload() {
+        citizensConversationStarter.reload();
+    }
+
+    @Override
+    public void close() {
+        HandlerList.unregisterAll(citizensMoveController);
+    }
+}
---
+++ src/main/java/org/betonquest/betonquest/compatibility/npcs/citizens/CitizensInventoryConvIO.java
@@ -0,0 +1,117 @@
+package org.betonquest.betonquest.compatibility.npcs.citizens;
+
+import net.citizensnpcs.trait.SkinTrait;
+import org.betonquest.betonquest.BetonQuest;
+import org.betonquest.betonquest.api.logger.BetonQuestLogger;
+import org.betonquest.betonquest.api.profiles.OnlineProfile;
+import org.betonquest.betonquest.conversation.Conversation;
+import org.betonquest.betonquest.conversation.InventoryConvIO;
+import org.bukkit.Bukkit;
+import org.bukkit.inventory.meta.SkullMeta;
+import org.bukkit.profile.PlayerProfile;
+import org.bukkit.profile.PlayerTextures;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * A chest conversationIO that replaces the NPCs skull with the skin of the Citizen NPC.
+ */
+public class CitizensInventoryConvIO extends InventoryConvIO {
+
+    /**
+     * A regex pattern to match the skin URL in the base64 encoded skin texture.
+     * See <a href="https://wiki.vg/Mojang_API#UUID_to_Profile_and_Skin.2FCape">wiki.vg</a> for unofficial documentation.
+     */
+    private static final Pattern SKIN_JSON_URL_PATTERN = Pattern.compile("\"SKIN\" ?: ?\\{\\n *\"url\" ?: ?\"(?<url>.*)\"");
+
+    /**
+     * Custom {@link BetonQuestLogger} instance for this class.
+     */
+    private final BetonQuestLogger log;
+
+    /**
+     * Creates a new {@link CitizensInventoryConvIO} instance.
+     *
+     * @param conv          the conversation
+     * @param onlineProfile the online profile
+     */
+    public CitizensInventoryConvIO(final Conversation conv, final OnlineProfile onlineProfile) {
+        super(conv, onlineProfile);
+        this.log = BetonQuest.getInstance().getLoggerFactory().create(getClass());
+    }
+
+    @Override
+    protected SkullMeta updateSkullMeta(final SkullMeta meta) {
+        // this only applied to Citizens NPC conversations
+        if (conv instanceof final CitizensConversation citizensConv) {
+            if (Bukkit.isPrimaryThread()) {
+                throw new IllegalStateException("Must be called async!");
+            }
+
+            try {
+                final SkinTrait skinTrait = Bukkit.getScheduler().callSyncMethod(BetonQuest.getInstance(), () -> citizensConv.getNPC().getOrAddTrait(SkinTrait.class)).get();
+                final String texture = skinTrait.getTexture();
+                if (texture != null) {
+
+                    final PlayerProfile profile = Bukkit.createProfile(UUID.randomUUID(), "");
+                    final PlayerTextures skullTexture = profile.getTextures();
+                    skullTexture.setSkin(resolveSkinURL(texture), PlayerTextures.SkinModel.CLASSIC);
+                    profile.setTextures(skullTexture);
+                    meta.setOwnerProfile(profile);
+                    return meta;
+                }
+            } catch (InterruptedException | ExecutionException e) {
+                log.debug(citizensConv.getPackage(), "Could not resolve a skin Texture!", e);
+            } catch (final SkinFormatParseException e) {
+                log.reportException(conv.getPackage(), new IllegalStateException("Could not parse the skin metadata provided by the NPC plugin. The format may have changed."));
+            }
+        }
+        return super.updateSkullMeta(meta);
+    }
+
+    /**
+     * Resolves a base64 encoded skin JSON to a skin URL.
+     * See <a href="https://wiki.vg/Mojang_API#UUID_to_Profile_and_Skin.2FCape">wiki.vg</a> for unofficial documentation
+     * on the skin JSON format.
+     *
+     * @param base64Texture base64 encoded skin JSON metadata
+     * @return the skin URL
+     */
+    private URL resolveSkinURL(final String base64Texture) throws SkinFormatParseException {
+        final String decoded = new String(Base64.getDecoder().decode(base64Texture), StandardCharsets.UTF_8);
+        final Matcher matcher = SKIN_JSON_URL_PATTERN.matcher(decoded);
+        if (!matcher.find()) {
+            throw new SkinFormatParseException("Could not find the skin URL in the skin JSON!");
+        }
+        final String variable = matcher.group("url");
+        try {
+            return new URL(variable);
+        } catch (final MalformedURLException e) {
+            throw new SkinFormatParseException("Could not parse the skin URL!", e);
+        }
+    }
+
+    /**
+     * A CitizensInventoryConvIO that also prints the messages in the chat.
+     */
+    public static class CitizensCombined extends CitizensInventoryConvIO {
+
+        /**
+         * Creates a new {@link CitizensCombined} conversationIO instance.
+         *
+         * @param conv          the conversation
+         * @param onlineProfile the online profile
+         */
+        public CitizensCombined(final Conversation conv, final OnlineProfile onlineProfile) {
+            super(conv, onlineProfile);
+            super.printMessages = true;
+        }
+    }
+}
