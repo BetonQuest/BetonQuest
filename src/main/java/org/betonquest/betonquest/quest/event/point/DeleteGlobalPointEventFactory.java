27f887b708a8db15013a77331e3801d5ea440faa
TheosRee <theosree@users.noreply.github.com>
2024-07-16 00:48:21 +0200

    use direct EventTypeRegistry to avoid deprecated methods

--- src/main/java/org/betonquest/betonquest/quest/event/point/DeleteGlobalPointEventFactory.java
+++ src/main/java/org/betonquest/betonquest/quest/event/point/DeleteGlobalPointEventFactory.java
@@ -1,18 +1,15 @@
 package org.betonquest.betonquest.quest.event.point;
 
 import org.betonquest.betonquest.Instruction;
-import org.betonquest.betonquest.api.quest.event.Event;
-import org.betonquest.betonquest.api.quest.event.EventFactory;
 import org.betonquest.betonquest.api.quest.event.StaticEvent;
 import org.betonquest.betonquest.api.quest.event.StaticEventFactory;
 import org.betonquest.betonquest.exceptions.InstructionParseException;
-import org.betonquest.betonquest.quest.event.CallStaticEventAdapter;
 import org.betonquest.betonquest.utils.Utils;
 
 /**
  * Factory for the delete global point event.
  */
-public class DeleteGlobalPointEventFactory implements EventFactory, StaticEventFactory {
+public class DeleteGlobalPointEventFactory implements StaticEventFactory {
 
     /**
      * Creates a new DeleteGlobalPointEventFactory.
@@ -24,10 +21,5 @@ public class DeleteGlobalPointEventFactory implements EventFactory, StaticEventF
     public StaticEvent parseStaticEvent(final Instruction instruction) throws InstructionParseException {
         final String category = Utils.addPackage(instruction.getPackage(), instruction.next());
         return new DeleteGlobalPointEvent(category);
-    }
-
-    @Override
-    public Event parseEvent(final Instruction instruction) throws InstructionParseException {
-        return new CallStaticEventAdapter(parseStaticEvent(instruction));
     }
 }
--- src/main/java/org/betonquest/betonquest/quest/event/run/RunIndependentEventFactory.java
+++ src/main/java/org/betonquest/betonquest/quest/event/run/RunIndependentEventFactory.java
@@ -1,20 +1,17 @@
 package org.betonquest.betonquest.quest.event.run;
 
 import org.betonquest.betonquest.Instruction;
-import org.betonquest.betonquest.api.quest.event.Event;
-import org.betonquest.betonquest.api.quest.event.EventFactory;
 import org.betonquest.betonquest.api.quest.event.StaticEvent;
 import org.betonquest.betonquest.api.quest.event.StaticEventFactory;
 import org.betonquest.betonquest.exceptions.InstructionParseException;
 import org.betonquest.betonquest.id.EventID;
-import org.betonquest.betonquest.quest.event.CallStaticEventAdapter;
 
 import java.util.List;
 
 /**
  * Create new {@link RunIndependentEvent} from instruction.
  */
-public class RunIndependentEventFactory implements StaticEventFactory, EventFactory {
+public class RunIndependentEventFactory implements StaticEventFactory {
 
     /**
      * Create new {@link RunIndependentEventFactory}.
@@ -26,10 +23,5 @@ public class RunIndependentEventFactory implements StaticEventFactory, EventFact
     public StaticEvent parseStaticEvent(final Instruction instruction) throws InstructionParseException {
         final List<EventID> events = instruction.getList(instruction.getOptional("events"), instruction::getEvent);
         return new RunIndependentEvent(events);
-    }
-
-    @Override
-    public Event parseEvent(final Instruction instruction) throws InstructionParseException {
-        return new CallStaticEventAdapter(parseStaticEvent(instruction));
     }
 }
