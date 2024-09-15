4274c52da49309410bf3656786be7d4c429547a3
TheosRee <theosree@users.noreply.github.com>
2024-09-15 15:29:02 +0200

    rename to feature package

--- src/main/java/org/betonquest/betonquest/quest/registry/type/NpcTypeRegistry.java
+++ src/main/java/org/betonquest/betonquest/quest/registry/type/NpcTypeRegistry.java
@@ -3,7 +3,7 @@ package org.betonquest.betonquest.quest.registry.type;
 import org.betonquest.betonquest.api.logger.BetonQuestLogger;
 import org.betonquest.betonquest.api.quest.npc.NpcFactory;
 import org.betonquest.betonquest.api.quest.npc.NpcWrapper;
-import org.betonquest.betonquest.api.quest.npc.conversation.NpcInteractCatcher;
+import org.betonquest.betonquest.api.quest.npc.feature.NpcInteractCatcher;
 import org.betonquest.betonquest.quest.registry.FactoryRegistry;
 import org.jetbrains.annotations.Nullable;
 
