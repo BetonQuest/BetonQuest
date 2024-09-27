2e1c0cb4e7c9446bdbd9e96c4eb8d7ccf6790337
MrGuitz <guitzmeyer@gmail.com>
2024-09-27 12:32:30 +0200

    migrated RideCondition to new API

---
+++ src/main/java/org/betonquest/betonquest/quest/condition/ride/RideCondition.java
@@ -0,0 +1,35 @@
+package org.betonquest.betonquest.quest.condition.ride;
+
+import org.betonquest.betonquest.api.profiles.OnlineProfile;
+import org.betonquest.betonquest.api.quest.condition.online.OnlineCondition;
+import org.betonquest.betonquest.exceptions.QuestRuntimeException;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * A condition that checks if the player is riding a vehicle.
+ */
+public class RideCondition implements OnlineCondition {
+
+    /**
+     * The entity type to match.
+     */
+    @Nullable
+    private final EntityType vehicle;
+
+    /**
+     * Creates a new ride condition. If the entity type is null, any entity will match.
+     *
+     * @param vehicle the entity type to match
+     */
+    public RideCondition(@Nullable final EntityType vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    @Override
+    public boolean check(final OnlineProfile profile) throws QuestRuntimeException {
+        final Entity entity = profile.getPlayer().getVehicle();
+        return entity != null && (vehicle == null || entity.getType() == vehicle);
+    }
+}
---
+++ src/main/java/org/betonquest/betonquest/quest/condition/ride/RideConditionFactory.java
@@ -0,0 +1,62 @@
+package org.betonquest.betonquest.quest.condition.ride;
+
+import org.betonquest.betonquest.Instruction;
+import org.betonquest.betonquest.api.logger.BetonQuestLogger;
+import org.betonquest.betonquest.api.logger.BetonQuestLoggerFactory;
+import org.betonquest.betonquest.api.quest.condition.PlayerCondition;
+import org.betonquest.betonquest.api.quest.condition.PlayerConditionFactory;
+import org.betonquest.betonquest.api.quest.condition.online.OnlineConditionAdapter;
+import org.betonquest.betonquest.exceptions.InstructionParseException;
+import org.betonquest.betonquest.quest.PrimaryServerThreadData;
+import org.betonquest.betonquest.quest.condition.PrimaryServerThreadPlayerCondition;
+import org.bukkit.entity.EntityType;
+
+/**
+ * Factory to create ride conditions from {@link Instruction}s.
+ */
+public class RideConditionFactory implements PlayerConditionFactory {
+
+    /**
+     * The string to match any entity.
+     */
+    private static final String ANY_ENTITY = "any";
+
+    /**
+     * Logger factory to create a logger for condition.
+     */
+    private final BetonQuestLoggerFactory loggerFactory;
+
+    /**
+     * Data used for condition check on the primary server thread.
+     */
+    private final PrimaryServerThreadData data;
+
+    /**
+     * Create the ride condition factory.
+     *
+     * @param loggerFactory the logger factory to create a logger for the condition
+     * @param data          the data used for checking the condition on the main thread
+     */
+    public RideConditionFactory(final BetonQuestLoggerFactory loggerFactory, final PrimaryServerThreadData data) {
+        this.loggerFactory = loggerFactory;
+        this.data = data;
+    }
+
+    @Override
+    public PlayerCondition parsePlayer(final Instruction instruction) throws InstructionParseException {
+        final String name = instruction.next();
+        final EntityType vehicle;
+        if (ANY_ENTITY.equalsIgnoreCase(name)) {
+            vehicle = null;
+        } else {
+            try {
+                vehicle = instruction.getEnum(name, EntityType.class);
+            } catch (final IllegalArgumentException e) {
+                throw new InstructionParseException("Entity type " + name + " does not exist.", e);
+            }
+        }
+        final BetonQuestLogger logger = loggerFactory.create(RideCondition.class);
+        return new PrimaryServerThreadPlayerCondition(
+                new OnlineConditionAdapter(new RideCondition(vehicle), logger, instruction.getPackage()), data);
+    }
+}
