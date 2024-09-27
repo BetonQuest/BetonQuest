011af6fa3a96e510f1d36f0ff28706e86534638e
MrGuitz <guitzmeyer@gmail.com>
2024-09-27 16:15:39 +0200

    migrated RandomCondition to new API

---
+++ src/main/java/org/betonquest/betonquest/quest/condition/random/RandomCondition.java
@@ -0,0 +1,49 @@
+package org.betonquest.betonquest.quest.condition.random;
+
+import org.betonquest.betonquest.api.profiles.Profile;
+import org.betonquest.betonquest.api.quest.condition.nullable.NullableCondition;
+import org.betonquest.betonquest.exceptions.QuestRuntimeException;
+import org.betonquest.betonquest.instruction.variable.VariableNumber;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Random;
+
+/**
+ * The condition that is met randomly.
+ */
+public class RandomCondition implements NullableCondition {
+
+    /**
+     * The random object to generate random numbers.
+     */
+    private final Random random;
+
+    /**
+     * The range of the random number to be true.
+     */
+    private final VariableNumber rangeOfRandom;
+
+    /**
+     * The maximum value that the random number can be.
+     */
+    private final VariableNumber valueMax;
+
+    /**
+     * The constructor of the random condition.
+     *
+     * @param random        The random object to generate random numbers.
+     * @param valueMax      The maximum value that the random number can be.
+     * @param rangeOfRandom The range of the random number to be true.
+     */
+    public RandomCondition(final Random random, final VariableNumber valueMax, final VariableNumber rangeOfRandom) {
+        this.random = random;
+        this.valueMax = valueMax;
+        this.rangeOfRandom = rangeOfRandom;
+    }
+
+    @Override
+    public boolean check(@Nullable final Profile profile) throws QuestRuntimeException {
+        final int randomNumber = random.nextInt(rangeOfRandom.getValue(profile).intValue()) + 1;
+        return randomNumber <= valueMax.getValue(profile).intValue();
+    }
+}
---
+++ src/main/java/org/betonquest/betonquest/quest/condition/random/RandomConditionFactory.java
@@ -0,0 +1,60 @@
+package org.betonquest.betonquest.quest.condition.random;
+
+import org.betonquest.betonquest.Instruction;
+import org.betonquest.betonquest.api.quest.condition.PlayerCondition;
+import org.betonquest.betonquest.api.quest.condition.PlayerConditionFactory;
+import org.betonquest.betonquest.api.quest.condition.PlayerlessCondition;
+import org.betonquest.betonquest.api.quest.condition.PlayerlessConditionFactory;
+import org.betonquest.betonquest.api.quest.condition.nullable.NullableConditionAdapter;
+import org.betonquest.betonquest.exceptions.InstructionParseException;
+import org.betonquest.betonquest.instruction.variable.VariableNumber;
+import org.betonquest.betonquest.quest.registry.processor.VariableProcessor;
+
+import java.util.Random;
+
+/**
+ * Factory to create random conditions from {@link Instruction}s.
+ */
+public class RandomConditionFactory implements PlayerConditionFactory, PlayerlessConditionFactory {
+
+    /**
+     * The variable processor used to process variables.
+     */
+    private final VariableProcessor variableProcessor;
+
+    /**
+     * Create the random condition factory.
+     *
+     * @param variableProcessor the variable processor used to process variables
+     */
+    public RandomConditionFactory(final VariableProcessor variableProcessor) {
+        this.variableProcessor = variableProcessor;
+    }
+
+    @Override
+    public PlayerCondition parsePlayer(final Instruction instruction) throws InstructionParseException {
+        return new NullableConditionAdapter(parse(instruction));
+    }
+
+    @Override
+    public PlayerlessCondition parsePlayerless(final Instruction instruction) throws InstructionParseException {
+        return new NullableConditionAdapter(parse(instruction));
+    }
+
+    private RandomCondition parse(final Instruction instruction) throws InstructionParseException {
+        final String[] values = instruction.next().split("-");
+        final int expectedLength = 2;
+        if (values.length != expectedLength) {
+            throw new InstructionParseException("Wrong randomness format. Use <chance>-<max>");
+        }
+        final VariableNumber valueMax;
+        final VariableNumber rangeOfRandom;
+        try {
+            valueMax = new VariableNumber(variableProcessor, instruction.getPackage(), values[0]);
+            rangeOfRandom = new VariableNumber(variableProcessor, instruction.getPackage(), values[1]);
+        } catch (final InstructionParseException e) {
+            throw new InstructionParseException("Cannot parse randomness values", e);
+        }
+        return new RandomCondition(new Random(), valueMax, rangeOfRandom);
+    }
+}
