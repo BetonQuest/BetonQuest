19031f9776967389206310edb00e70eb0919c428
MrGuitz <guitzmeyer@gmail.com>
2024-09-27 11:34:01 +0200

    migrated ScoreboardCondition to new API

---
+++ src/main/java/org/betonquest/betonquest/quest/condition/scoreboard/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * {@link org.betonquest.betonquest.api.quest.condition.PlayerCondition PlayerCondition}
+ * implementations of scoreboard conditions.
+ */
+package org.betonquest.betonquest.quest.condition.scoreboard;
