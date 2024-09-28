0554bebc94b5d594f05a8ee099fa18f1c9905f6f
Wolf2323 <gabrielpatrikurban@gmail.com>
2024-09-28 16:22:15 +0200

    fixed setup-docs-dependencies.py not work correctly with MKDOCS_MATERIAL_INSIDERS_ENABLED set

--- config/setup-docs-dependencies.py
+++ config/setup-docs-dependencies.py
@@ -10,22 +10,20 @@ common_dependencies = [
 
 def get_install_requires():
     mkdocs_token = os.environ.get('MKDOCS_MATERIAL_INSIDERS')
-
     if mkdocs_token:
         mkdocs_enabled = os.environ.get('MKDOCS_MATERIAL_INSIDERS_ENABLED')
-        if not mkdocs_enabled:
-            print("\033[93m"
-                  + "Warning: 'MKDOCS_MATERIAL_INSIDERS' is set "
-                  + "but 'MKDOCS_MATERIAL_INSIDERS_ENABLED' is not set to 'true', "
-                  + "so if you serve mkdocs you will serve the normal version!"
-                  + "\033[0m")
-        return [
-            f'git+https://{mkdocs_token}@github.com/squidfunk/mkdocs-material-insiders.git@9.5.33-insiders-4.53.12',
-            'pillow',
-            'cairosvg',
-        ] + common_dependencies
-    else:
-        return ['mkdocs-material==9.5.33'] + common_dependencies
+        if mkdocs_enabled == 'true':
+            return [
+                f'git+https://{mkdocs_token}@github.com/squidfunk/mkdocs-material-insiders.git@9.5.33-insiders-4.53.12',
+                'pillow',
+                'cairosvg',
+            ] + common_dependencies
+        print("\033[93m"
+              + "Warning: 'MKDOCS_MATERIAL_INSIDERS' is set "
+              + "but 'MKDOCS_MATERIAL_INSIDERS_ENABLED' is not set to 'true', "
+              + "so if you serve mkdocs you will serve the normal version!"
+              + "\033[0m")
+    return ['mkdocs-material==9.5.33'] + common_dependencies
 
 
 subprocess.run(['pip', 'install'] + get_install_requires())
