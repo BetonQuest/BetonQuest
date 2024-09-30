b419b714fe0a8bfcb7e4e8667d8804493dcf5517
Lorias-Aegorin <Jannik.k11@gmail.com>
2024-09-30 18:07:01 +0200

    - removed the deprecated method calls and use the non deprecated - the node expiry now gets checked for the correct amount

--- src/main/java/org/betonquest/betonquest/compatibility/luckperms/LuckPermsIntegrator.java
+++ src/main/java/org/betonquest/betonquest/compatibility/luckperms/LuckPermsIntegrator.java
@@ -45,7 +45,7 @@ public class LuckPermsIntegrator implements Integrator {
             luckPermsAPI = provider.getProvider();
             tagCalculator = TagCalculatorUtils.getTagContextCalculator();
             luckPermsAPI.getContextManager().registerCalculator(tagCalculator);
-            instance.registerNonStaticEvent("luckperms", new LuckPermsEventFactory(luckPermsAPI));
+            instance.getQuestRegistries().getEventTypes().register("luckperms", new LuckPermsEventFactory(luckPermsAPI));
         }
     }
 
