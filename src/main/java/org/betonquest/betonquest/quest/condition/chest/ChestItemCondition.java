74819a8ae4b3f794cb723f02bcad2c4b508e1aab
MrGuitz <guitzmeyer@gmail.com>
2024-10-02 00:09:46 +0200

    Migrated ChestItemCondition to new API

---
+++ src/main/java/org/betonquest/betonquest/quest/condition/chest/ChestItemCondition.java
@@ -0,0 +1,71 @@
+package org.betonquest.betonquest.quest.condition.chest;
+
+import org.betonquest.betonquest.Instruction.Item;
+import org.betonquest.betonquest.api.profiles.Profile;
+import org.betonquest.betonquest.api.quest.condition.nullable.NullableCondition;
+import org.betonquest.betonquest.exceptions.QuestRuntimeException;
+import org.betonquest.betonquest.instruction.variable.location.VariableLocation;
+import org.bukkit.block.Block;
+import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Arrays;
+
+/**
+ * Checks if the chest contains specified items.
+ */
+public class ChestItemCondition implements NullableCondition {
+
+    /**
+     * Items that should be in the chest.
+     */
+    private final Item[] questItems;
+
+    /**
+     * Location of the chest.
+     */
+    private final VariableLocation loc;
+
+    /**
+     * Constructor of the ChestItemCondition.
+     *
+     * @param questItems items that should be in the chest
+     * @param loc        location of the chest
+     */
+    public ChestItemCondition(final VariableLocation loc, final Item... questItems) {
+        this.questItems = Arrays.copyOf(questItems, questItems.length);
+        this.loc = loc;
+    }
+
+    @Override
+    public boolean check(@Nullable final Profile profile) throws QuestRuntimeException {
+        final Block block = loc.getValue(profile).getBlock();
+        final InventoryHolder chest;
+        try {
+            chest = (InventoryHolder) block.getState();
+        } catch (final ClassCastException e) {
+            throw new QuestRuntimeException("Trying to check items in a chest, but there's no chest! Location: X" + block.getX() + " Y"
+                    + block.getY() + " Z" + block.getZ(), e);
+        }
+        int counter = 0;
+        for (final Item questItem : questItems) {
+            int amount = questItem.getAmount().getValue(profile).intValue();
+            final ItemStack[] inventoryItems = chest.getInventory().getContents();
+            for (final ItemStack item : inventoryItems) {
+                if (item == null) {
+                    continue;
+                }
+                if (!questItem.isItemEqual(item)) {
+                    continue;
+                }
+                amount -= item.getAmount();
+                if (amount <= 0) {
+                    counter++;
+                    break;
+                }
+            }
+        }
+        return counter == questItems.length;
+    }
+}
---
+++ src/main/java/org/betonquest/betonquest/quest/condition/chest/ChestItemConditionFactory.java
@@ -0,0 +1,50 @@
+package org.betonquest.betonquest.quest.condition.chest;
+
+import org.betonquest.betonquest.Instruction;
+import org.betonquest.betonquest.Instruction.Item;
+import org.betonquest.betonquest.api.quest.condition.PlayerCondition;
+import org.betonquest.betonquest.api.quest.condition.PlayerConditionFactory;
+import org.betonquest.betonquest.api.quest.condition.PlayerlessCondition;
+import org.betonquest.betonquest.api.quest.condition.PlayerlessConditionFactory;
+import org.betonquest.betonquest.api.quest.condition.nullable.NullableConditionAdapter;
+import org.betonquest.betonquest.exceptions.InstructionParseException;
+import org.betonquest.betonquest.instruction.variable.location.VariableLocation;
+import org.betonquest.betonquest.quest.PrimaryServerThreadData;
+import org.betonquest.betonquest.quest.condition.PrimaryServerThreadPlayerCondition;
+import org.betonquest.betonquest.quest.condition.PrimaryServerThreadPlayerlessCondition;
+
+/**
+ * Factory to create chest item conditions from {@link Instruction}s.
+ */
+public class ChestItemConditionFactory implements PlayerConditionFactory, PlayerlessConditionFactory {
+
+    /**
+     * Data used for condition check on the primary server thread.
+     */
+    private final PrimaryServerThreadData data;
+
+    /**
+     * Create the chest item condition factory.
+     *
+     * @param data the data used for checking the condition on the main thread
+     */
+    public ChestItemConditionFactory(final PrimaryServerThreadData data) {
+        this.data = data;
+    }
+
+    @Override
+    public PlayerCondition parsePlayer(final Instruction instruction) throws InstructionParseException {
+        return new PrimaryServerThreadPlayerCondition(new NullableConditionAdapter(parse(instruction)), data);
+    }
+
+    @Override
+    public PlayerlessCondition parsePlayerless(final Instruction instruction) throws InstructionParseException {
+        return new PrimaryServerThreadPlayerlessCondition(new NullableConditionAdapter(parse(instruction)), data);
+    }
+
+    private ChestItemCondition parse(final Instruction instruction) throws InstructionParseException {
+        final VariableLocation loc = instruction.getLocation();
+        final Item[] questItems = instruction.getItemList();
+        return new ChestItemCondition(loc, questItems);
+    }
+}
