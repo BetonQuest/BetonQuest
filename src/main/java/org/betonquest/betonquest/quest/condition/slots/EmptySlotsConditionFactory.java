439c8dc6d99b01c6eb05907ca9e35492933ca7d2
MrGuitz <guitzmeyer@gmail.com>
2024-10-02 00:25:44 +0200

    Migrated EmptySlotsCondition to new API

---
+++ src/main/java/org/betonquest/betonquest/quest/condition/slots/EmptySlotsConditionFactory.java
@@ -0,0 +1,48 @@
+package org.betonquest.betonquest.quest.condition.slots;
+
+import org.betonquest.betonquest.Instruction;
+import org.betonquest.betonquest.api.logger.BetonQuestLogger;
+import org.betonquest.betonquest.api.logger.BetonQuestLoggerFactory;
+import org.betonquest.betonquest.api.quest.condition.PlayerCondition;
+import org.betonquest.betonquest.api.quest.condition.PlayerConditionFactory;
+import org.betonquest.betonquest.api.quest.condition.online.OnlineConditionAdapter;
+import org.betonquest.betonquest.exceptions.InstructionParseException;
+import org.betonquest.betonquest.instruction.variable.VariableNumber;
+import org.betonquest.betonquest.quest.PrimaryServerThreadData;
+import org.betonquest.betonquest.quest.condition.PrimaryServerThreadPlayerCondition;
+
+/**
+ * Factory for {@link EmptySlotsCondition}s.
+ */
+public class EmptySlotsConditionFactory implements PlayerConditionFactory {
+
+    /**
+     * Logger factory to create a logger for conditions.
+     */
+    private final BetonQuestLoggerFactory loggerFactory;
+
+    /**
+     * Data used for condition check on the primary server thread.
+     */
+    private final PrimaryServerThreadData data;
+
+    /**
+     * Create the empty slots condition factory.
+     *
+     * @param loggerFactory the logger factory
+     * @param data          the data used for checking the condition on the main thread
+     */
+    public EmptySlotsConditionFactory(final BetonQuestLoggerFactory loggerFactory, final PrimaryServerThreadData data) {
+        this.loggerFactory = loggerFactory;
+        this.data = data;
+    }
+
+    @Override
+    public PlayerCondition parsePlayer(final Instruction instruction) throws InstructionParseException {
+        final VariableNumber required = instruction.getVarNum();
+        final boolean equal = instruction.hasArgument("equal");
+        final BetonQuestLogger log = loggerFactory.create(EmptySlotsCondition.class);
+        return new PrimaryServerThreadPlayerCondition(
+                new OnlineConditionAdapter(new EmptySlotsCondition(required, equal), log, instruction.getPackage()), data);
+    }
+}
---
+++ src/main/java/org/betonquest/betonquest/quest/condition/slots/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * {@link org.betonquest.betonquest.api.quest.condition.PlayerCondition PlayerCondition}
+ * Contains classes for slot conditions.
+ */
+package org.betonquest.betonquest.quest.condition.slots;
