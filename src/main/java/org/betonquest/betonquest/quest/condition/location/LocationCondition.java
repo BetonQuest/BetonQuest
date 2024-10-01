5331d7476eebee32dd61528cb3e138e9b8353c32
MrGuitz <guitzmeyer@gmail.com>
2024-10-01 22:42:12 +0200

    migrated LocationCondition to new API

---
+++ src/main/java/org/betonquest/betonquest/quest/condition/location/LocationCondition.java
@@ -0,0 +1,47 @@
+package org.betonquest.betonquest.quest.condition.location;
+
+import org.betonquest.betonquest.api.profiles.OnlineProfile;
+import org.betonquest.betonquest.api.quest.condition.online.OnlineCondition;
+import org.betonquest.betonquest.exceptions.QuestRuntimeException;
+import org.betonquest.betonquest.instruction.variable.VariableNumber;
+import org.betonquest.betonquest.instruction.variable.location.VariableLocation;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+/**
+ * Requires the player to be in specified distance from a location.
+ */
+public class LocationCondition implements OnlineCondition {
+
+    /**
+     * The location.
+     */
+    private final VariableLocation loc;
+
+    /**
+     * The range around the location.
+     */
+    private final VariableNumber range;
+
+    /**
+     * Creates a new location condition.
+     *
+     * @param loc   The location
+     * @param range The range around the location
+     */
+    public LocationCondition(final VariableLocation loc, final VariableNumber range) {
+        this.loc = loc;
+        this.range = range;
+    }
+
+    @Override
+    public boolean check(final OnlineProfile profile) throws QuestRuntimeException {
+        final Location location = loc.getValue(profile);
+        final Player player = profile.getPlayer();
+        if (!location.getWorld().equals(player.getWorld())) {
+            return false;
+        }
+        final double pRange = range.getValue(profile).doubleValue();
+        return player.getLocation().distanceSquared(location) <= pRange * pRange;
+    }
+}
