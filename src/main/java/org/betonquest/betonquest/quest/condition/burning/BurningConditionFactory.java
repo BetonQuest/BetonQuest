517883d52eacc042e353e37922a879be23446fa3
MrGuitz <guitzmeyer@gmail.com>
2024-10-01 23:44:08 +0200

    Migrated BurningCondition to new API

---
+++ src/main/java/org/betonquest/betonquest/quest/condition/burning/BurningConditionFactory.java
@@ -0,0 +1,45 @@
+package org.betonquest.betonquest.quest.condition.burning;
+
+import org.betonquest.betonquest.Instruction;
+import org.betonquest.betonquest.api.logger.BetonQuestLogger;
+import org.betonquest.betonquest.api.logger.BetonQuestLoggerFactory;
+import org.betonquest.betonquest.api.quest.condition.PlayerCondition;
+import org.betonquest.betonquest.api.quest.condition.PlayerConditionFactory;
+import org.betonquest.betonquest.api.quest.condition.online.OnlineConditionAdapter;
+import org.betonquest.betonquest.exceptions.InstructionParseException;
+import org.betonquest.betonquest.quest.PrimaryServerThreadData;
+import org.betonquest.betonquest.quest.condition.PrimaryServerThreadPlayerCondition;
+
+/**
+ * Factory for {@link BurningCondition}s from {@link Instruction}s.
+ */
+public class BurningConditionFactory implements PlayerConditionFactory {
+
+    /**
+     * Logger factory to create a logger for conditions.
+     */
+    private final BetonQuestLoggerFactory loggerFactory;
+
+    /**
+     * Data used for condition check on the primary server thread.
+     */
+    private final PrimaryServerThreadData data;
+
+    /**
+     * Create the burning factory.
+     *
+     * @param loggerFactory the logger factory
+     * @param data          the data used for checking the condition on the main thread
+     */
+    public BurningConditionFactory(final BetonQuestLoggerFactory loggerFactory, final PrimaryServerThreadData data) {
+        this.loggerFactory = loggerFactory;
+        this.data = data;
+    }
+
+    @Override
+    public PlayerCondition parsePlayer(final Instruction instruction) throws InstructionParseException {
+        final BetonQuestLogger log = loggerFactory.create(BurningCondition.class);
+        return new PrimaryServerThreadPlayerCondition(
+                new OnlineConditionAdapter(new BurningCondition(), log, instruction.getPackage()), data);
+    }
+}
