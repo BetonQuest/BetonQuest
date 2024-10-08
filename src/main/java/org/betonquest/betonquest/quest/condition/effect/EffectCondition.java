8b0f3c6c7208d6745f00efcc2a21450891f4d8f5
MrGuitz <guitzmeyer@gmail.com>
2024-10-08 23:11:52 +0200

    migrated EffectCondition to new API

---
+++ src/main/java/org/betonquest/betonquest/quest/condition/effect/EffectCondition.java
@@ -0,0 +1,31 @@
+package org.betonquest.betonquest.quest.condition.effect;
+
+import org.betonquest.betonquest.api.profiles.OnlineProfile;
+import org.betonquest.betonquest.api.quest.condition.online.OnlineCondition;
+import org.betonquest.betonquest.exceptions.QuestRuntimeException;
+import org.bukkit.potion.PotionEffectType;
+
+/**
+ * A condition that checks if a player has a potion effect.
+ */
+public class EffectCondition implements OnlineCondition {
+
+    /**
+     * The type of the potion effect.
+     */
+    private final PotionEffectType type;
+
+    /**
+     * Create a new effect condition.
+     *
+     * @param type the type of the potion effect
+     */
+    public EffectCondition(final PotionEffectType type) {
+        this.type = type;
+    }
+
+    @Override
+    public boolean check(final OnlineProfile profile) throws QuestRuntimeException {
+        return profile.getPlayer().hasPotionEffect(type);
+    }
+}
---
+++ src/main/java/org/betonquest/betonquest/quest/condition/effect/EffectConditionFactory.java
@@ -0,0 +1,49 @@
+package org.betonquest.betonquest.quest.condition.effect;
+
+import org.betonquest.betonquest.Instruction;
+import org.betonquest.betonquest.api.logger.BetonQuestLogger;
+import org.betonquest.betonquest.api.logger.BetonQuestLoggerFactory;
+import org.betonquest.betonquest.api.quest.condition.PlayerCondition;
+import org.betonquest.betonquest.api.quest.condition.PlayerConditionFactory;
+import org.betonquest.betonquest.api.quest.condition.online.OnlineConditionAdapter;
+import org.betonquest.betonquest.exceptions.InstructionParseException;
+import org.betonquest.betonquest.quest.PrimaryServerThreadData;
+import org.betonquest.betonquest.quest.condition.PrimaryServerThreadPlayerCondition;
+import org.betonquest.betonquest.utils.Utils;
+import org.bukkit.potion.PotionEffectType;
+
+/**
+ * Factory for {@link EffectCondition}s.
+ */
+public class EffectConditionFactory implements PlayerConditionFactory {
+
+    /**
+     * Logger factory to create a logger for conditions.
+     */
+    private final BetonQuestLoggerFactory loggerFactory;
+
+    /**
+     * Data used for condition check on the primary server thread.
+     */
+    private final PrimaryServerThreadData data;
+
+    /**
+     * Create the effect factory.
+     *
+     * @param loggerFactory the logger factory
+     * @param data          the data used for checking the condition on the main thread
+     */
+    public EffectConditionFactory(final BetonQuestLoggerFactory loggerFactory, final PrimaryServerThreadData data) {
+        this.loggerFactory = loggerFactory;
+        this.data = data;
+    }
+
+    @Override
+    public PlayerCondition parsePlayer(final Instruction instruction) throws InstructionParseException {
+        final String string = instruction.next();
+        final PotionEffectType type = Utils.getNN(PotionEffectType.getByName(string), "Effect " + string + " does not exist");
+        final BetonQuestLogger log = loggerFactory.create(EffectCondition.class);
+        return new PrimaryServerThreadPlayerCondition(
+                new OnlineConditionAdapter(new EffectCondition(type), log, instruction.getPackage()), data);
+    }
+}
