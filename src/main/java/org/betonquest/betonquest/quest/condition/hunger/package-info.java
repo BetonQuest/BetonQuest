e3a7d34d91ad72aff22066252c9f7742213b8380
MrGuitz <guitzmeyer@gmail.com>
2024-10-09 13:07:18 +0200

    migrated HungerCondition to new API

---
+++ src/main/java/org/betonquest/betonquest/quest/condition/hunger/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * {@link org.betonquest.betonquest.api.quest.condition.PlayerCondition PlayerCondition}
+ * Contains classes for hunger condition.
+ */
+package org.betonquest.betonquest.quest.condition.hunger;
