71698229f9009782f6fbcd28b3b2e4f1984bb8f8
MrGuitz <guitzmeyer@gmail.com>
2024-10-12 13:05:39 +0200

    migrated CheckCondition to new API

--- src/main/java/org/betonquest/betonquest/quest/condition/check/CheckConditionFactory.java
+++ src/main/java/org/betonquest/betonquest/quest/condition/check/CheckConditionFactory.java
@@ -1,13 +1,17 @@
-package org.betonquest.betonquest.conditions;
+package org.betonquest.betonquest.quest.condition.check;
 
 import org.betonquest.betonquest.BetonQuest;
 import org.betonquest.betonquest.Instruction;
 import org.betonquest.betonquest.api.Condition;
+import org.betonquest.betonquest.api.config.quest.QuestPackage;
 import org.betonquest.betonquest.api.logger.BetonQuestLogger;
-import org.betonquest.betonquest.api.profiles.Profile;
+import org.betonquest.betonquest.api.quest.condition.PlayerCondition;
+import org.betonquest.betonquest.api.quest.condition.PlayerConditionFactory;
+import org.betonquest.betonquest.api.quest.condition.PlayerlessCondition;
+import org.betonquest.betonquest.api.quest.condition.PlayerlessConditionFactory;
+import org.betonquest.betonquest.api.quest.condition.nullable.NullableConditionAdapter;
 import org.betonquest.betonquest.exceptions.InstructionParseException;
 import org.betonquest.betonquest.exceptions.ObjectNotFoundException;
-import org.betonquest.betonquest.exceptions.QuestRuntimeException;
 import org.betonquest.betonquest.id.NoID;
 import org.betonquest.betonquest.quest.legacy.LegacyTypeFactory;
 import org.jetbrains.annotations.Nullable;
@@ -16,39 +20,46 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Allows for checking multiple conditions with one instruction string.
+ * Factory for {@link CheckCondition}s.
  */
-public class CheckCondition extends Condition {
+public class CheckConditionFactory implements PlayerConditionFactory, PlayerlessConditionFactory {
+
     /**
      * Custom {@link BetonQuestLogger} instance for this class.
      */
     private final BetonQuestLogger log;
 
     /**
-     * Conditions that will be checked by this condition. All must be true for this condition to be true as well.
-     */
-    private final List<Condition> internalConditions = new ArrayList<>();
-
-    /**
-     * Create a check condition for the given instruction.
+     * Create the check condition factory.
      *
-     * @param instruction instruction defining this condition
-     * @throws InstructionParseException if the instruction is not a valid check condition
+     * @param log the logger
      */
-    public CheckCondition(final Instruction instruction) throws InstructionParseException {
-        super(instruction, false);
-        staticness = true;
-        persistent = true;
-        this.log = BetonQuest.getInstance().getLoggerFactory().create(getClass());
+    public CheckConditionFactory(final BetonQuestLogger log) {
+        this.log = log;
+    }
+
+    @Override
+    public PlayerCondition parsePlayer(final Instruction instruction) throws InstructionParseException {
+        return new NullableConditionAdapter(new CheckCondition(parseConditions(instruction)));
+    }
+
+    @Override
+    public PlayerlessCondition parsePlayerless(final Instruction instruction) throws InstructionParseException {
+        return new NullableConditionAdapter(new CheckCondition(parseConditions(instruction)));
+    }
+
+    private List<Condition> parseConditions(final Instruction instruction) throws InstructionParseException {
+        final List<Condition> internalConditions = new ArrayList<>();
         final String[] parts = instruction.getAllParts();
         if (parts.length == 0) {
             throw new InstructionParseException("Not enough arguments");
         }
+        final QuestPackage questPackage = instruction.getPackage();
         StringBuilder builder = new StringBuilder();
         for (final String part : parts) {
             if (!part.isEmpty() && part.charAt(0) == '^') {
                 if (!builder.isEmpty()) {
-                    internalConditions.add(createCondition(builder.toString().trim()));
+                    internalConditions.add(createCondition(builder.toString().trim(), questPackage));
                     builder = new StringBuilder();
                 }
                 builder.append(part.substring(1)).append(' ');
@@ -56,14 +67,15 @@ public class CheckCondition extends Condition {
                 builder.append(part).append(' ');
             }
         }
-        internalConditions.add(createCondition(builder.toString().trim()));
+        internalConditions.add(createCondition(builder.toString().trim(), questPackage));
+        return internalConditions;
     }
 
     /**
      * Constructs a condition with given instruction and returns it.
      */
     @Nullable
-    private Condition createCondition(final String instruction) throws InstructionParseException {
+    private Condition createCondition(final String instruction, final QuestPackage questPackage) throws InstructionParseException {
         final String[] parts = instruction.split(" ");
         if (parts.length == 0) {
             throw new InstructionParseException("Not enough arguments in internal condition");
@@ -75,25 +87,15 @@ public class CheckCondition extends Condition {
                     + " spelled correctly in internal condition");
         }
         try {
-            final Instruction innerInstruction = new Instruction(BetonQuest.getInstance().getLoggerFactory().create(Instruction.class), this.instruction.getPackage(), new NoID(this.instruction.getPackage()), instruction);
+            final Instruction innerInstruction = new Instruction(BetonQuest.getInstance().getLoggerFactory().create(Instruction.class), questPackage, new NoID(questPackage), instruction);
             return conditionFactory.parseInstruction(innerInstruction);
         } catch (final ObjectNotFoundException e) {
             if (e.getCause() instanceof InstructionParseException) {
                 throw new InstructionParseException("Error in internal condition: " + e.getCause().getMessage(), e);
             } else {
-                log.reportException(this.instruction.getPackage(), e);
+                log.reportException(questPackage, e);
             }
         }
         return null;
-    }
-
-    @Override
-    protected Boolean execute(final Profile profile) throws QuestRuntimeException {
-        for (final Condition condition : internalConditions) {
-            if (!condition.handle(profile)) {
-                return false;
-            }
-        }
-        return true;
     }
 }
