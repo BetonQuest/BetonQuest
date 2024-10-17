d83b038a1d5c0fd11801d4e21d140b9cdcc3174e
TheosRee <theosree@users.noreply.github.com>
2024-10-17 17:57:16 +0200

    Backpack and QuestCanceler cleanup move IDArgument.java into new package

---
+++ src/main/java/org/betonquest/betonquest/instruction/argument/IDArgument.java
@@ -0,0 +1,23 @@
+package org.betonquest.betonquest.instruction.argument;
+
+import org.betonquest.betonquest.api.config.quest.QuestPackage;
+import org.betonquest.betonquest.exceptions.ObjectNotFoundException;
+import org.betonquest.betonquest.id.ID;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Creates an {@link ID} from a pack and string.
+ *
+ * @param <T> the type of the id
+ */
+public interface IDArgument<T extends ID> {
+    /**
+     * Creates a new ID.
+     *
+     * @param pack       the source pack
+     * @param identifier the id name, potentially prefixed with a quest path
+     * @return the newly created id
+     * @throws ObjectNotFoundException when there is no such {@link T} in the resolved quest package
+     */
+    T convert(@Nullable QuestPackage pack, String identifier) throws ObjectNotFoundException;
+}
