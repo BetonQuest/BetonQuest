1163f57d42a8e40420520e216adf5c6d2915ea88
Wolf2323 <gabrielpatrikurban@gmail.com>
2024-12-04 15:06:28 +0100

    fix external link icons by using relative paths for the icons

--- docs/_webCode/js/externalLinkIcon.js
+++ docs/_webCode/js/externalLinkIcon.js
@@ -1,6 +1,5 @@
 const currentDomain = window.location.origin;
 const links = document.querySelectorAll(".md-content__inner a");
-console.log("currentDomain: ", currentDomain);
 
 links.forEach(link => {
   const href = link.href;
