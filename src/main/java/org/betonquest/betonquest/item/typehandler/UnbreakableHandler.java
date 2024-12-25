20f811ecccc7fbeddd2f514d3a17eb259931bfca
TheosRee <theosree@users.noreply.github.com>
2024-12-25 23:46:48 +0100

    move durability handling into DurabilityHandler; move unbreakable handling into UnbreakableHandler

--- src/main/java/org/betonquest/betonquest/item/typehandler/UnbreakableHandler.java
+++ src/main/java/org/betonquest/betonquest/item/typehandler/UnbreakableHandler.java
@@ -1,13 +1,51 @@
 package org.betonquest.betonquest.item.typehandler;
 
 import org.betonquest.betonquest.item.QuestItem.Existence;
+import org.bukkit.inventory.meta.ItemMeta;
 
 @SuppressWarnings("PMD.CommentRequired")
-public class UnbreakableHandler {
+public class UnbreakableHandler implements ItemMetaHandler<ItemMeta> {
+
+    /**
+     * The unbreakable string.
+     */
+    private static final String UNBREAKABLE = "unbreakable";
 
     private Existence unbreakable = Existence.WHATEVER;
 
     public UnbreakableHandler() {
+    }
+
+    /**
+     * Converts the item meta into QuestItem format.
+     *
+     * @param meta the meta to serialize
+     * @return parsed values with leading space or empty string
+     */
+    public static String serializeToString(final ItemMeta meta) {
+        if (meta.isUnbreakable()) {
+            return " unbreakable";
+        }
+        return "";
+    }
+
+    @Override
+    public void set(final String key, final String data) {
+        if (UNBREAKABLE.equals(data)) {
+            unbreakable = Existence.REQUIRED;
+        } else {
+            set(data);
+        }
+    }
+
+    @Override
+    public void populate(final ItemMeta meta) {
+        meta.setUnbreakable(isUnbreakable());
+    }
+
+    @Override
+    public boolean check(final ItemMeta meta) {
+        return check(meta.isUnbreakable());
     }
 
     public void set(final String string) {
@@ -29,5 +67,4 @@ public class UnbreakableHandler {
             case FORBIDDEN -> !bool;
         };
     }
-
 }
