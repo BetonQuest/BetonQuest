7e4a8c59162efa9884b5527c2802b4e872b14657
Wolf2323 <gabrielpatrikurban@gmail.com>
2025-02-19 11:38:54 +0100

    correct translation names for crowdin

--- src/main/java/org/betonquest/betonquest/config/patcher/migration/Migration.java
+++ src/main/java/org/betonquest/betonquest/config/patcher/migration/Migration.java
@@ -21,22 +21,49 @@ public interface Migration {
      *
      * @param config        The config
      * @param sectionName   The section name
-     * @param valueStartOld The old value
-     * @param valueStartNew The new value
+     * @param oldStartValue The old value
+     * @param newStartValue The new value
      * @return true if the value was replaced, false otherwise
      */
-    default boolean replaceStartValueInSection(final YamlConfiguration config, final String sectionName, final String valueStartOld, final String valueStartNew) {
+    default boolean replaceStartValueInSection(final YamlConfiguration config, final String sectionName, final String oldStartValue, final String newStartValue) {
         final ConfigurationSection section = config.getConfigurationSection(sectionName);
         if (section == null) {
             return false;
         }
-        final String valueStartOldSpace = valueStartOld + " ";
-        final String valueStartNewSpace = valueStartNew + " ";
+        final String oldStartValueSpace = oldStartValue + " ";
+        final String newStartValueSpace = newStartValue + " ";
         boolean replaced = false;
         for (final String key : section.getKeys(false)) {
             final String value = section.getString(key);
-            if (value != null && value.startsWith(valueStartOldSpace)) {
-                section.set(key, valueStartNewSpace + value.substring(valueStartOldSpace.length()));
+            if (value != null && value.startsWith(oldStartValueSpace)) {
+                section.set(key, newStartValueSpace + value.substring(oldStartValueSpace.length()));
+                replaced = true;
+            }
+        }
+        return replaced;
+    }
+
+    /**
+     * Gets the section from the config and replaces the old value with the new value.
+     *
+     * @param config      The config
+     * @param sectionName The section name
+     * @param typeName    The type name
+     * @param oldValue    The old value
+     * @param newValue    The new value
+     * @return true if the value was replaced, false otherwise
+     */
+    @SuppressWarnings("PMD.UseObjectForClearerAPI")
+    default boolean replaceValueInSection(final YamlConfiguration config, final String sectionName, final String typeName, final String oldValue, final String newValue) {
+        final ConfigurationSection section = config.getConfigurationSection(sectionName);
+        if (section == null) {
+            return false;
+        }
+        boolean replaced = false;
+        for (final String key : section.getKeys(false)) {
+            final String value = section.getString(key);
+            if (value != null && value.startsWith(typeName + " ") && value.contains(oldValue)) {
+                section.set(key, value.replace(oldValue, newValue));
                 replaced = true;
             }
         }
