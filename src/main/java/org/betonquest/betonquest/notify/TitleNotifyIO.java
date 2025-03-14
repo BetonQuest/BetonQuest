9e707c88f6653224d3ff2814bee4783fd5307c18
Wolf2323 <gabrielpatrikurban@gmail.com>
2025-03-14 23:12:26 +0100

    added method to split components in the TitleNotifyIO

--- src/main/java/org/betonquest/betonquest/notify/TitleNotifyIO.java
+++ src/main/java/org/betonquest/betonquest/notify/TitleNotifyIO.java
@@ -1,13 +1,18 @@
 package org.betonquest.betonquest.notify;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.title.Title;
 import org.betonquest.betonquest.api.config.quest.QuestPackage;
 import org.betonquest.betonquest.api.profile.OnlineProfile;
 import org.betonquest.betonquest.api.quest.QuestException;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @SuppressWarnings("PMD.CommentRequired")
 public class TitleNotifyIO extends NotifyIO {
@@ -35,7 +40,40 @@ public class TitleNotifyIO extends NotifyIO {
 
     @Override
     protected void notifyPlayer(final Component message, final OnlineProfile onlineProfile) {
-        final Title title = Title.title(message, Component.empty(), Title.Times.times(fadeIn, stay, fadeOut));
+        final List<Component> messageComponents = splitComponent(message);
+        final int size = messageComponents.size();
+        final Component titleComponent = size >= 1 ? messageComponents.get(0) : Component.empty();
+        final Component subtitleComponent = size >= 2 ? messageComponents.get(1) : Component.empty();
+        final Title title = Title.title(titleComponent, subtitleComponent, Title.Times.times(fadeIn, stay, fadeOut));
         onlineProfile.getPlayer().showTitle(title);
+    }
+
+    private List<Component> splitComponent(final Component self) {
+        final List<Component> lines = splitComponentContent(self);
+        if (self.children().isEmpty()) {
+            return lines;
+        }
+        Component parent = lines.remove(lines.size() - 1);
+        for (final Component child : self.children()) {
+            final List<Component> childSegments = splitComponent(child);
+            parent = parent.append(childSegments.get(0));
+            for (int i = 1; i < childSegments.size(); i++) {
+                lines.add(parent);
+                parent = Component.empty().style(parent.style());
+                parent = parent.append(childSegments.get(i));
+            }
+        }
+        lines.add(parent);
+        return lines;
+    }
+
+    private List<Component> splitComponentContent(final Component component) {
+        if (!(component instanceof final TextComponent text)) {
+            return new ArrayList<>(List.of(component));
+        }
+        final String[] segments = text.content().split("\n");
+        return Arrays.stream(segments)
+                .map(segment -> Component.text(segment).style(text.style()))
+                .collect(Collectors.toCollection(ArrayList::new));
     }
 }
