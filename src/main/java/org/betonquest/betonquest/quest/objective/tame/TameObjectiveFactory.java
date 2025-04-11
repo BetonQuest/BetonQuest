69e696ca266cc7eee0eedc1a5b0f70c8c115aa9e
Wolf2323 <gabrielpatrikurban@gmail.com>
2025-04-11 18:09:23 +0200

    migrate tame objective to factory api

---
+++ src/main/java/org/betonquest/betonquest/quest/objective/tame/TameObjectiveFactory.java
@@ -0,0 +1,31 @@
+package org.betonquest.betonquest.quest.objective.tame;
+
+import org.betonquest.betonquest.api.Objective;
+import org.betonquest.betonquest.api.quest.QuestException;
+import org.betonquest.betonquest.api.quest.objective.ObjectiveFactory;
+import org.betonquest.betonquest.instruction.Instruction;
+import org.betonquest.betonquest.instruction.argument.VariableArgument;
+import org.betonquest.betonquest.instruction.variable.VariableNumber;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Tameable;
+
+/**
+ * Factory for creating {@link TameObjective} instances from {@link Instruction}s.
+ */
+public class TameObjectiveFactory implements ObjectiveFactory {
+    /**
+     * Creates a new TameObjectiveFactory instance.
+     */
+    public TameObjectiveFactory() {
+    }
+
+    @Override
+    public Objective parseInstruction(final Instruction instruction) throws QuestException {
+        final EntityType type = instruction.getEnum(EntityType.class);
+        if (type.getEntityClass() == null || !Tameable.class.isAssignableFrom(type.getEntityClass())) {
+            throw new QuestException("Entity cannot be tamed: " + type);
+        }
+        final VariableNumber amount = instruction.get(VariableArgument.NUMBER_NOT_LESS_THAN_ONE);
+        return new TameObjective(instruction, type, amount);
+    }
+}
