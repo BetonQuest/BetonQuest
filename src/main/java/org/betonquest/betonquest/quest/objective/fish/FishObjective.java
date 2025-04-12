a61428a284faadedb7f5f33d762ba1115169ae2e
Wolf2323 <gabrielpatrikurban@gmail.com>
2025-04-12 20:47:18 +0200

    migrate fish objective to factory api

--- src/main/java/org/betonquest/betonquest/quest/objective/fish/FishObjective.java
+++ src/main/java/org/betonquest/betonquest/quest/objective/fish/FishObjective.java
@@ -1,4 +1,4 @@
-package org.betonquest.betonquest.objective;
+package org.betonquest.betonquest.quest.objective.fish;
 
 import org.betonquest.betonquest.BetonQuest;
 import org.betonquest.betonquest.api.CountingObjective;
@@ -6,9 +6,7 @@ import org.betonquest.betonquest.api.logger.BetonQuestLogger;
 import org.betonquest.betonquest.api.profile.OnlineProfile;
 import org.betonquest.betonquest.api.profile.Profile;
 import org.betonquest.betonquest.api.quest.QuestException;
-import org.betonquest.betonquest.id.ItemID;
 import org.betonquest.betonquest.instruction.Instruction;
-import org.betonquest.betonquest.instruction.argument.VariableArgument;
 import org.betonquest.betonquest.instruction.variable.VariableNumber;
 import org.betonquest.betonquest.instruction.variable.location.VariableLocation;
 import org.betonquest.betonquest.item.QuestItem;
@@ -28,38 +26,56 @@ import org.jetbrains.annotations.Nullable;
 /**
  * Requires the player to catch the fish.
  */
-@SuppressWarnings("PMD.CommentRequired")
 public class FishObjective extends CountingObjective implements Listener {
     /**
      * Custom {@link BetonQuestLogger} instance for this class.
      */
     private final BetonQuestLogger log;
 
+    /**
+     * Quest item to catch.
+     */
     private final QuestItem questItem;
 
+    /**
+     * Location where the fish should be caught.
+     */
     @Nullable
     private final VariableLocation hookTargetLocation;
 
+    /**
+     * Range around the location where the fish should be caught.
+     */
     @Nullable
     private final VariableNumber rangeVar;
 
-    public FishObjective(final Instruction instruction) throws QuestException {
+    /**
+     * Constructor for the FishObjective.
+     *
+     * @param instruction        the instruction that created this objective
+     * @param log                the logger for this objective
+     * @param questItem          the quest item to catch
+     * @param targetAmount       the target amount of fish to catch
+     * @param hookTargetLocation the location where the fish should be caught
+     * @param rangeVar           the range around the location where the item should be fished
+     * @throws QuestException if there is an error in the instruction
+     */
+    public FishObjective(final Instruction instruction, final BetonQuestLogger log, final QuestItem questItem,
+                         final VariableNumber targetAmount, @Nullable final VariableLocation hookTargetLocation,
+                         @Nullable final VariableNumber rangeVar) throws QuestException {
         super(instruction, "fish_to_catch");
-        this.log = BetonQuest.getInstance().getLoggerFactory().create(getClass());
-        questItem = new QuestItem(instruction.getID(ItemID::new));
-        targetAmount = instruction.get(VariableArgument.NUMBER_NOT_LESS_THAN_ONE);
-
-        final String loc = instruction.getOptional("hookLocation");
-        final String range = instruction.getOptional("range");
-        if (loc != null && range != null) {
-            hookTargetLocation = instruction.get(loc, VariableLocation::new);
-            rangeVar = instruction.get(range, VariableNumber::new);
-        } else {
-            hookTargetLocation = null;
-            rangeVar = null;
-        }
+        this.log = log;
+        this.questItem = questItem;
+        this.targetAmount = targetAmount;
+        this.hookTargetLocation = hookTargetLocation;
+        this.rangeVar = rangeVar;
     }
 
+    /**
+     * Check if the fish was caught in the right location.
+     *
+     * @param event the event that was triggered
+     */
     @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     public void onFishCatch(final PlayerFishEvent event) {
         if (event.getState() != State.CAUGHT_FISH) {
