6de2bf9bf5e9f68a3ced351bcc44d1ce77e69276
Wolf2323 <gabrielpatrikurban@gmail.com>
2025-04-13 14:40:11 +0200

    migrate kill objective to factory api

--- src/main/java/org/betonquest/betonquest/quest/objective/kill/KillPlayerObjective.java
+++ src/main/java/org/betonquest/betonquest/quest/objective/kill/KillPlayerObjective.java
@@ -1,4 +1,4 @@
-package org.betonquest.betonquest.objective;
+package org.betonquest.betonquest.quest.objective.kill;
 
 import org.betonquest.betonquest.BetonQuest;
 import org.betonquest.betonquest.api.CountingObjective;
@@ -6,7 +6,7 @@ import org.betonquest.betonquest.api.profile.OnlineProfile;
 import org.betonquest.betonquest.api.quest.QuestException;
 import org.betonquest.betonquest.id.ConditionID;
 import org.betonquest.betonquest.instruction.Instruction;
-import org.betonquest.betonquest.instruction.argument.VariableArgument;
+import org.betonquest.betonquest.instruction.variable.VariableNumber;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
@@ -16,21 +16,43 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
-@SuppressWarnings("PMD.CommentRequired")
+/**
+ * Requires the player to kill a target player.
+ */
 public class KillPlayerObjective extends CountingObjective implements Listener {
+    /**
+     * The name of the victim to kill.
+     */
     @Nullable
     private final String name;
 
+    /**
+     * The conditions of the victim that must be met for the objective to count.
+     */
     private final List<ConditionID> required;
 
-    public KillPlayerObjective(final Instruction instruction) throws QuestException {
+    /**
+     * Constructor for the KillPlayerObjective.
+     *
+     * @param instruction  the instruction that created this objective
+     * @param targetAmount the amount of players to kill
+     * @param name         the name of the player to kill, or null for any player
+     * @param required     the conditions of the victim that must be met for the objective to count
+     * @throws QuestException if there is an error in the instruction
+     */
+    public KillPlayerObjective(final Instruction instruction, final VariableNumber targetAmount,
+                               @Nullable final String name, final List<ConditionID> required) throws QuestException {
         super(instruction, "players_to_kill");
-        targetAmount = instruction.get(VariableArgument.NUMBER_NOT_LESS_THAN_ONE);
-        name = instruction.getOptional("name");
-        required = instruction.getIDList(instruction.getOptional("required"), ConditionID::new);
+        this.targetAmount = targetAmount;
+        this.name = name;
+        this.required = required;
     }
 
-    @SuppressWarnings("PMD.CyclomaticComplexity")
+    /**
+     * Check if the player is the killer of the victim.
+     *
+     * @param event the PlayerDeathEvent
+     */
     @EventHandler(ignoreCancelled = true)
     public void onKill(final PlayerDeathEvent event) {
         if (event.getEntity().getKiller() != null) {
