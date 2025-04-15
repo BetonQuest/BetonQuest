68b602a2059c7851ba0b96e35d0fde5bcc1bb4fd
Wolf2323 <gabrielpatrikurban@gmail.com>
2025-04-15 21:33:13 +0200

    migrate chestput objective to factory api

---
+++ src/main/java/org/betonquest/betonquest/quest/chestput/ChestPutObjectiveFactory.java
@@ -0,0 +1,55 @@
+package org.betonquest.betonquest.quest.chestput;
+
+import org.betonquest.betonquest.api.Objective;
+import org.betonquest.betonquest.api.logger.BetonQuestLogger;
+import org.betonquest.betonquest.api.logger.BetonQuestLoggerFactory;
+import org.betonquest.betonquest.api.quest.QuestException;
+import org.betonquest.betonquest.api.quest.objective.ObjectiveFactory;
+import org.betonquest.betonquest.config.PluginMessage;
+import org.betonquest.betonquest.instruction.Instruction;
+import org.betonquest.betonquest.instruction.Item;
+import org.betonquest.betonquest.instruction.variable.location.VariableLocation;
+import org.betonquest.betonquest.quest.condition.chest.ChestItemCondition;
+import org.betonquest.betonquest.quest.event.IngameNotificationSender;
+import org.betonquest.betonquest.quest.event.NotificationLevel;
+import org.betonquest.betonquest.quest.event.chest.ChestTakeEvent;
+
+/**
+ * Factory for creating {@link ChestPutObjective} instances from {@link Instruction}s.
+ */
+public class ChestPutObjectiveFactory implements ObjectiveFactory {
+    /**
+     * Logger factory to create a logger for the objectives.
+     */
+    private final BetonQuestLoggerFactory loggerFactory;
+
+    /**
+     * The {@link PluginMessage} instance.
+     */
+    private final PluginMessage pluginMessage;
+
+    /**
+     * Creates a new instance of the ChestPutObjectiveFactory.
+     *
+     * @param loggerFactory the logger factory to create a logger for the objectives
+     * @param pluginMessage the {@link PluginMessage} instance
+     */
+    public ChestPutObjectiveFactory(final BetonQuestLoggerFactory loggerFactory, final PluginMessage pluginMessage) {
+        this.loggerFactory = loggerFactory;
+        this.pluginMessage = pluginMessage;
+    }
+
+    @Override
+    public Objective parseInstruction(final Instruction instruction) throws QuestException {
+        final VariableLocation loc = instruction.get(VariableLocation::new);
+        final Item[] items = instruction.getItemList();
+        final boolean multipleAccess = Boolean.parseBoolean(instruction.getOptional("multipleaccess"));
+        final ChestItemCondition chestItemCondition = new ChestItemCondition(loc, items);
+        final ChestTakeEvent chestTakeEvent = instruction.hasArgument("items-stay") ? null : new ChestTakeEvent(loc, items);
+        final BetonQuestLogger log = loggerFactory.create(ChestPutObjective.class);
+        final IngameNotificationSender occupiedSender = new IngameNotificationSender(log, pluginMessage, instruction.getPackage(),
+                instruction.getID().getFullID(), NotificationLevel.INFO, "chest_occupied");
+        return new ChestPutObjective(instruction, log, chestItemCondition, chestTakeEvent, loc, occupiedSender,
+                multipleAccess);
+    }
+}
