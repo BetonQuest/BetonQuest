79d284c87d0964bc7c826d9e526d82134465f6e6
Wolf2323 <gabrielpatrikurban@gmail.com>
2025-04-15 22:46:07 +0200

    migrate breed objective to factory api

--- src/main/java/org/betonquest/betonquest/quest/objective/breed/BreedObjective.java
+++ src/main/java/org/betonquest/betonquest/quest/objective/breed/BreedObjective.java
@@ -1,11 +1,11 @@
-package org.betonquest.betonquest.objective;
+package org.betonquest.betonquest.quest.objective.breed;
 
 import org.betonquest.betonquest.BetonQuest;
 import org.betonquest.betonquest.api.CountingObjective;
 import org.betonquest.betonquest.api.profile.OnlineProfile;
 import org.betonquest.betonquest.api.quest.QuestException;
 import org.betonquest.betonquest.instruction.Instruction;
-import org.betonquest.betonquest.instruction.argument.VariableArgument;
+import org.betonquest.betonquest.instruction.variable.VariableNumber;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
@@ -14,17 +14,34 @@ import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityBreedEvent;
 
-@SuppressWarnings("PMD.CommentRequired")
+/**
+ * Requires the player to breed a specific type of animal.
+ */
 public class BreedObjective extends CountingObjective implements Listener {
-
+    /**
+     * The type of animal to breed.
+     */
     private final EntityType type;
 
-    public BreedObjective(final Instruction instruction) throws QuestException {
+    /**
+     * Constructor for the BreedObjective.
+     *
+     * @param instruction  the instruction that created this objective
+     * @param type         the type of animal to breed
+     * @param targetAmount the target amount of animals to breed
+     * @throws QuestException if there is an error in the instruction
+     */
+    public BreedObjective(final Instruction instruction, final EntityType type, final VariableNumber targetAmount) throws QuestException {
         super(instruction, "animals_to_breed");
-        type = instruction.getEnum(EntityType.class);
-        targetAmount = instruction.get(VariableArgument.NUMBER_NOT_LESS_THAN_ONE);
+        this.targetAmount = targetAmount;
+        this.type = type;
     }
 
+    /**
+     * Check if the player is breeding the right type of animal.
+     *
+     * @param event the event that triggered the breeding
+     */
     @EventHandler(ignoreCancelled = true)
     public void onBreeding(final EntityBreedEvent event) {
         if (event.getEntityType() == type && event.getBreeder() instanceof Player) {
