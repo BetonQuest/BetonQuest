2cda9cc71d9e6b065bfc740e0bae4a11d4de0940
TheosRee <theosree@users.noreply.github.com>
2025-06-06 15:04:10 +0200

    remove fasseg/exp4j from JDocs (moved to discussion)

--- src/main/java/org/betonquest/betonquest/util/math/Tokenizer.java
+++ src/main/java/org/betonquest/betonquest/util/math/Tokenizer.java
@@ -20,8 +20,7 @@ import java.util.regex.Pattern;
 /**
  * Helps the {@link MathVariable} with parsing mathematical expressions.
  *
- * @deprecated This should be replaced in BQ 2.0 with a real expression parsing lib like
- * <a href="https://github.com/fasseg/exp4j">fasseg/exp4j</a>
+ * @deprecated This should be replaced with a real expression parsing lib
  */
 @Deprecated
 public class Tokenizer {
