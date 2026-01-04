6bebd650a4a23ddd98ee7965b3a52b485a5452f0
Jimin Seo <aa090402@naver.com>
2026-01-04 23:23:31 +0900

    feat: FancyHolograms

---
+++ code/compatibility/src/main/java/org/betonquest/betonquest/compatibility/holograms/fancyholograms/FancyHologramsHologram.java
@@ -0,0 +1,103 @@
+package org.betonquest.betonquest.compatibility.holograms.fancyholograms;
+
+import net.kyori.adventure.text.Component;
+import org.betonquest.betonquest.compatibility.holograms.BetonHologram;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+/**
+ * FancyHolograms specific implementation of BetonHologram.
+ */
+public class FancyHologramsHologram implements BetonHologram {
+
+    @Override
+    public void appendLine(final ItemStack item) {
+
+    }
+
+    @Override
+    public void appendLine(final Component text) {
+
+    }
+
+    @Override
+    public void setLine(final int index, final ItemStack item) {
+
+    }
+
+    @Override
+    public void setLine(final int index, final Component text) {
+
+    }
+
+    @Override
+    public void createLines(final int startingIndex, final int linesToBeAdded) {
+
+    }
+
+    @Override
+    public void removeLine(final int index) {
+
+    }
+
+    @Override
+    public void show(final Player player) {
+
+    }
+
+    @Override
+    public void hide(final Player player) {
+
+    }
+
+    @Override
+    public void move(final Location location) {
+
+    }
+
+    @Override
+    public void showAll() {
+
+    }
+
+    @Override
+    public void hideAll() {
+
+    }
+
+    @Override
+    public void delete() {
+
+    }
+
+    @Override
+    public boolean isDisabled() {
+        return false;
+    }
+
+    @Override
+    public void disable() {
+
+    }
+
+    @Override
+    public void enable() {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public Location getLocation() {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+}
