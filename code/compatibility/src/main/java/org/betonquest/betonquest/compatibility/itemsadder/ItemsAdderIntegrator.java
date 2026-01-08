9cd3cb6e9f2d1913065c934e4b69c8b28d04af9b
mrjimin <aa090402@naver.com>
2026-01-09 00:24:42 +0900

    commit

--- code/compatibility/src/main/java/org/betonquest/betonquest/compatibility/itemsadder/ItemsAdderIntegrator.java
+++ code/compatibility/src/main/java/org/betonquest/betonquest/compatibility/itemsadder/ItemsAdderIntegrator.java
@@ -3,9 +3,15 @@ package org.betonquest.betonquest.compatibility.itemsadder;
 import org.betonquest.betonquest.api.BetonQuestApi;
 import org.betonquest.betonquest.compatibility.HookException;
 import org.betonquest.betonquest.compatibility.Integrator;
+import org.betonquest.betonquest.compatibility.UnsupportedVersionException;
 import org.betonquest.betonquest.compatibility.itemsadder.item.ItemsAdderItemFactory;
 import org.betonquest.betonquest.compatibility.itemsadder.item.ItemsAdderQuestItemSerializer;
 import org.betonquest.betonquest.item.ItemRegistry;
+import org.betonquest.betonquest.versioning.UpdateStrategy;
+import org.betonquest.betonquest.versioning.Version;
+import org.betonquest.betonquest.versioning.VersionComparator;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
 
 /**
  * Handles integration with ItemsAdder.
@@ -21,6 +27,8 @@ public class ItemsAdderIntegrator implements Integrator {
 
     @Override
     public void hook(final BetonQuestApi api) throws HookException {
+        validateVersion();
+
         final ItemRegistry itemRegistry = api.getFeatureRegistries().item();
         itemRegistry.register("itemsAdder", new ItemsAdderItemFactory());
         itemRegistry.registerSerializer("itemsAdder", new ItemsAdderQuestItemSerializer());
@@ -34,5 +42,19 @@ public class ItemsAdderIntegrator implements Integrator {
     @Override
     public void close() {
         // Empty
+    }
+
+    /**
+     * Aborts the hooking process if the installed version of ItemsAdder is invalid.
+     *
+     * @throws UnsupportedVersionException if the installed version of ItemsAdder is < 4.0.10.
+     */
+    private void validateVersion() throws UnsupportedVersionException {
+        final Plugin itemsAdder = Bukkit.getPluginManager().getPlugin("ItemsAdder");
+        final Version itemsAdderVersion = new Version(itemsAdder.getDescription().getVersion());
+        final VersionComparator comparator = new VersionComparator(UpdateStrategy.MAJOR);
+        if (comparator.isOlderThan(itemsAdderVersion, new Version("4.0.10"))) {
+            throw new UnsupportedVersionException(itemsAdder, "4.0.10+");
+        }
     }
 }
