50fc5599cef0e053948f7334beec04af21fb9b7a
Wolf2323 <gabrielpatrikurban@gmail.com>
2026-01-14 16:58:12 +0100

    fix missing isPrimaryThreadEnforced method in CheckCondition

--- code/core/src/main/java/org/betonquest/betonquest/quest/condition/check/CheckCondition.java
+++ code/core/src/main/java/org/betonquest/betonquest/quest/condition/check/CheckCondition.java
@@ -16,24 +16,29 @@ public class CheckCondition implements NullableCondition {
     /**
      * Conditions that will be checked by this condition. All must be true for this condition to be true as well.
      */
-    private final List<ConditionAdapter> internalConditions;
+    private final List<ConditionAdapter> conditions;
 
     /**
      * Create a check condition for the given instruction.
      *
-     * @param internalConditions conditions that will be checked by this condition
+     * @param conditions conditions that will be checked by this condition
      */
-    public CheckCondition(final List<ConditionAdapter> internalConditions) {
-        this.internalConditions = internalConditions;
+    public CheckCondition(final List<ConditionAdapter> conditions) {
+        this.conditions = conditions;
     }
 
     @Override
     public boolean check(@Nullable final Profile profile) throws QuestException {
-        for (final ConditionAdapter condition : internalConditions) {
+        for (final ConditionAdapter condition : conditions) {
             if (!condition.check(profile)) {
                 return false;
             }
         }
         return true;
+    }
+
+    @Override
+    public boolean isPrimaryThreadEnforced() {
+        return conditions.stream().anyMatch(ConditionAdapter::isPrimaryThreadEnforced);
     }
 }
