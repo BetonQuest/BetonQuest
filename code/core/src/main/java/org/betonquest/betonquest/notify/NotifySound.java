6fb4a43c22612cef70b6047ef9b36c3f3ebaa5f5
TheosRee <theosree@users.noreply.github.com>
2026-02-02 21:05:32 +0100

    allow variables in rest of NotifySound

--- code/core/src/main/java/org/betonquest/betonquest/notify/NotifySound.java
+++ code/core/src/main/java/org/betonquest/betonquest/notify/NotifySound.java
@@ -4,10 +4,10 @@ import org.betonquest.betonquest.api.QuestException;
 import org.betonquest.betonquest.api.common.function.QuestConsumer;
 import org.betonquest.betonquest.api.config.quest.QuestPackage;
 import org.betonquest.betonquest.api.instruction.Argument;
+import org.betonquest.betonquest.api.instruction.ValueParser;
 import org.betonquest.betonquest.api.instruction.argument.parser.LocationParser;
 import org.betonquest.betonquest.api.instruction.argument.parser.VectorParser;
 import org.betonquest.betonquest.api.profile.OnlineProfile;
-import org.betonquest.betonquest.api.profile.Profile;
 import org.betonquest.betonquest.api.quest.Placeholders;
 import org.betonquest.betonquest.lib.instruction.argument.DefaultArgument;
 import org.bukkit.Bukkit;
@@ -21,10 +21,12 @@ import org.jetbrains.annotations.Nullable;
 import java.util.Arrays;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * Plays a sound to a player.
  */
+@SuppressWarnings("PMD.CouplingBetweenObjects")
 class NotifySound {
 
     /**
@@ -96,58 +98,66 @@ class NotifySound {
         }
 
         final Argument<Location> location = getLocationArgument(data);
-        final SoundCategory soundCategory = getSoundCategory(data);
+        final Argument<SoundCategory> soundCategory = getSoundCategory(data);
         final Argument<Number> volume = notify.getNumberData(KEY_SOUND_VOLUME, 1);
         final Argument<Number> pitch = notify.getNumberData(KEY_SOUND_PITCH, 1);
 
         final String playerOffsetString = data.get(KEY_SOUND_PLAYER_OFFSET);
-        Float playerOffsetDistance = null;
-        Argument<Vector> playerOffset = null;
-        try {
-            playerOffsetDistance = getPlayerOffsetDistance(playerOffsetString);
-        } catch (final QuestException e) {
-            playerOffset = getPlayerOffset(playerOffsetString);
+        final Argument<Optional<Float>> playerOffsetDistance;
+        final Argument<Optional<Vector>> playerOffset;
+        if (playerOffsetString == null) {
+            playerOffsetDistance = p -> Optional.empty();
+            playerOffset = p -> Optional.empty();
+        } else {
+            playerOffsetDistance = getArgument(playerOffsetString, this::getPlayerOffsetDistance);
+            playerOffset = getArgument(playerOffsetString, this::getPlayerOffset);
         }
 
-        String soundString = data.get(KEY_SOUND);
-        if (soundString == null) {
+        final String rawSoundString = data.get(KEY_SOUND);
+        if (rawSoundString == null) {
             throw new QuestException("Missing sound value!");
         }
-        final Sound sound = getSound(soundString);
-        if (sound == null) {
-            soundString = soundString.toLowerCase(Locale.ROOT);
-        }
+        final Argument<Optional<Sound>> sound = getArgument(rawSoundString, this::getSound);
+        final Argument<String> soundString = getArgument(rawSoundString, resolved -> resolved.toLowerCase(Locale.ROOT));
 
         soundPlayer = getSoundPlayer(sound, soundString, location, playerOffset, playerOffsetDistance, soundCategory, volume, pitch);
     }
 
+    private <T> Argument<T> getArgument(final String raw, final ValueParser<T> parser) throws QuestException {
+        return new DefaultArgument<>(placeholders, pack, raw, parser);
+    }
+
     private QuestConsumer<OnlineProfile> getSoundPlayer(
-            @Nullable final Sound sound, final String soundString, @Nullable final Argument<Location> location,
-            @Nullable final Argument<Vector> playerOffset, @Nullable final Float playerOffsetDistance,
-            final SoundCategory soundCategory, final Argument<Number> volume, final Argument<Number> pitch) {
+            final Argument<Optional<Sound>> sound, final Argument<String> soundString, @Nullable final Argument<Location> location,
+            final Argument<Optional<Vector>> playerOffset, final Argument<Optional<Float>> playerOffsetDistance,
+            final Argument<SoundCategory> soundCategory, final Argument<Number> volume, final Argument<Number> pitch) {
         return (onlineProfile) -> {
             final Location finalLocation = getLocation(onlineProfile, location, playerOffset, playerOffsetDistance);
             final float resolvedVolume = volume.getValue(onlineProfile).floatValue();
             final float resolvedPitch = pitch.getValue(onlineProfile).floatValue();
             final Player player = onlineProfile.getPlayer();
-            if (sound == null) {
-                player.playSound(finalLocation, soundString, soundCategory, resolvedVolume, resolvedPitch);
+            final Optional<Sound> resolvedSound = sound.getValue(onlineProfile);
+            final SoundCategory resolvedCategory = soundCategory.getValue(onlineProfile);
+            if (resolvedSound.isEmpty()) {
+                player.playSound(finalLocation, soundString.getValue(onlineProfile), resolvedCategory, resolvedVolume, resolvedPitch);
             } else {
-                player.playSound(finalLocation, sound, soundCategory, resolvedVolume, resolvedPitch);
+                player.playSound(finalLocation, resolvedSound.get(), resolvedCategory, resolvedVolume, resolvedPitch);
             }
         };
     }
 
     private Location getLocation(final OnlineProfile onlineProfile, @Nullable final Argument<Location> location,
-                                 @Nullable final Argument<Vector> playerOffset,
-                                 @Nullable final Float playerOffsetDistance) throws QuestException {
+                                 final Argument<Optional<Vector>> playerOffset,
+                                 final Argument<Optional<Float>> playerOffsetDistance) throws QuestException {
         final Location resolvedLocation = location == null ? onlineProfile.getPlayer().getLocation() : location.getValue(onlineProfile);
 
-        if (playerOffsetDistance != null && onlineProfile.getPlayer().getLocation().distance(resolvedLocation) > playerOffsetDistance) {
-            return getLocationRelativeDistance(resolvedLocation, onlineProfile.getPlayer(), playerOffsetDistance);
+        final Optional<Float> offsetDistance = playerOffsetDistance.getValue(onlineProfile);
+        if (offsetDistance.isPresent() && onlineProfile.getPlayer().getLocation().distance(resolvedLocation) > offsetDistance.get()) {
+            return getLocationRelativeDistance(resolvedLocation, onlineProfile.getPlayer(), offsetDistance.get());
         }
-        if (playerOffset != null) {
-            return getLocationRelativeVector(resolvedLocation, onlineProfile.getPlayer(), onlineProfile, playerOffset);
+        final Optional<Vector> offset = playerOffset.getValue(onlineProfile);
+        if (offset.isPresent()) {
+            return getLocationRelativeVector(resolvedLocation, onlineProfile.getPlayer(), offset.get());
         }
 
         return resolvedLocation;
@@ -159,8 +169,7 @@ class NotifySound {
         return player.getLocation().add(directionVector);
     }
 
-    private Location getLocationRelativeVector(final Location location, final Player player, final Profile profile, final Argument<Vector> playerOffset) throws QuestException {
-        final Vector relative = playerOffset.getValue(profile);
+    private Location getLocationRelativeVector(final Location location, final Player player, final Vector relative) {
         final Location playerLoc = player.getLocation();
 
         relative.rotateAroundY(-Math.toRadians(playerLoc.getYaw()));
@@ -185,48 +194,44 @@ class NotifySound {
     @Nullable
     private Argument<Location> getLocationArgument(final Map<String, String> data) throws QuestException {
         final String locationString = data.get(KEY_SOUND_LOCATION);
-        return locationString == null ? null : new DefaultArgument<>(placeholders, pack, locationString, new LocationParser(Bukkit.getServer()));
+        return locationString == null ? null : getArgument(locationString, new LocationParser(Bukkit.getServer()));
     }
 
-    private SoundCategory getSoundCategory(final Map<String, String> data) throws QuestException {
+    private Argument<SoundCategory> getSoundCategory(final Map<String, String> data) throws QuestException {
         final String soundCategoryString = data.get(KEY_SOUND_CATEGORY);
-        try {
-            return soundCategoryString == null ? SoundCategory.MASTER : SoundCategory.valueOf(soundCategoryString.toUpperCase(Locale.ROOT));
-        } catch (final IllegalArgumentException exception) {
-            throw new QuestException(String.format(NotifyIO.CATCH_MESSAGE_TYPE, KEY_SOUND_CATEGORY, soundCategoryString.toUpperCase(Locale.ROOT)), exception);
+        if (soundCategoryString == null) {
+            return p -> SoundCategory.MASTER;
         }
-    }
-
-    @Nullable
-    private Argument<Vector> getPlayerOffset(@Nullable final String playerOffsetString) throws QuestException {
-        if (playerOffsetString != null) {
+        return getArgument(soundCategoryString, resolved -> {
             try {
-                return new DefaultArgument<>(placeholders, pack, playerOffsetString, new VectorParser());
-            } catch (final QuestException e) {
-                throw new QuestException(String.format("%s '%s' couldn't be parsed: " + e.getMessage(), KEY_SOUND_PLAYER_OFFSET, playerOffsetString), e);
+                return SoundCategory.valueOf(soundCategoryString.toUpperCase(Locale.ROOT));
+            } catch (final IllegalArgumentException exception) {
+                throw new QuestException(String.format(NotifyIO.CATCH_MESSAGE_TYPE, KEY_SOUND_CATEGORY, soundCategoryString.toUpperCase(Locale.ROOT)), exception);
             }
-        }
-        return null;
+        });
     }
 
-    @Nullable
-    private Float getPlayerOffsetDistance(@Nullable final String playerOffsetString) throws QuestException {
-        if (playerOffsetString != null) {
-            try {
-                return Float.parseFloat(playerOffsetString);
-            } catch (final NumberFormatException e) {
-                throw new QuestException(e);
-            }
-        }
-        return null;
-    }
-
-    @Nullable
-    private Sound getSound(final String soundString) {
+    private Optional<Vector> getPlayerOffset(final String playerOffsetString) throws QuestException {
         try {
-            return Sound.valueOf(soundString);
+            return Optional.of(new VectorParser().apply(playerOffsetString));
+        } catch (final QuestException e) {
+            throw new QuestException(String.format("%s '%s' couldn't be parsed: " + e.getMessage(), KEY_SOUND_PLAYER_OFFSET, playerOffsetString), e);
+        }
+    }
+
+    private Optional<Float> getPlayerOffsetDistance(final String playerOffsetString) {
+        try {
+            return Optional.of(Float.parseFloat(playerOffsetString));
+        } catch (final NumberFormatException e) {
+            return Optional.empty();
+        }
+    }
+
+    private Optional<Sound> getSound(final String soundString) {
+        try {
+            return Optional.of(Sound.valueOf(soundString));
         } catch (final IllegalArgumentException exception) {
-            return null;
+            return Optional.empty();
         }
     }
 
