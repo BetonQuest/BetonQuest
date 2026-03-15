f121989e5c5273274d0cc30d8972f16be6ffdb67
Juyas <juyasetgames@gmail.com>
2026-03-15 15:38:48 +0100

    Add IntegrationService interface to provide instances of Integrations for specific plugins and an optional minimal version

---
+++ code/api/src/main/java/org/betonquest/betonquest/api/integration/IntegrationService.java
@@ -0,0 +1,37 @@
+package org.betonquest.betonquest.api.integration;
+
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * The {@link IntegrationService} provides an instance of the {@link Integrations}.
+ */
+public interface IntegrationService {
+
+    /**
+     * Create a new {@link Integrations} instance to register {@link Integration}s.
+     *
+     * @param pluginName           the name to identify the plugin with
+     * @param minimalPluginVersion the optional minimal required version of the plugin
+     * @return a new {@link Integrations} instance
+     */
+    Integrations integrator(String pluginName, @Nullable String minimalPluginVersion);
+
+    /**
+     * Create a new {@link Integrations} instance to register {@link Integration}s.
+     *
+     * @param pluginClass          the class of the plugin
+     * @param minimalPluginVersion the optional minimal required version of the plugin
+     * @return a new {@link Integrations} instance
+     */
+    Integrations integrator(Class<? extends Plugin> pluginClass, @Nullable String minimalPluginVersion);
+
+    /**
+     * Create a new {@link Integrations} instance to register {@link Integration}s.
+     *
+     * @param pluginInstance       the plugin instance
+     * @param minimalPluginVersion the optional minimal required version of the plugin
+     * @return a new {@link Integrations} instance
+     */
+    Integrations integrator(Plugin pluginInstance, @Nullable String minimalPluginVersion);
+}
