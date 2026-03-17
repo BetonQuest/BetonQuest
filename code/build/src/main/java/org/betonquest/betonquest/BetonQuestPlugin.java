fd7bbf33c049cc6b2b95cbe760324785fe272c7f
TheosRee <theosree@users.noreply.github.com>
2026-03-17 03:49:19 +0100

    use new register for Integration

--- code/build/src/main/java/org/betonquest/betonquest/BetonQuestPlugin.java
+++ code/build/src/main/java/org/betonquest/betonquest/BetonQuestPlugin.java
@@ -1,12 +1,13 @@
 package org.betonquest.betonquest;
 
+import org.betonquest.betonquest.api.integration.IntegrationService;
 import org.betonquest.betonquest.compatibility.BundledCompatibility;
-import org.betonquest.betonquest.compatibility.Compatibility;
-import org.betonquest.betonquest.database.GlobalData;
 import org.betonquest.betonquest.kernel.processor.quest.PlaceholderProcessor;
+import org.betonquest.betonquest.lib.integration.policy.Policies;
 import org.betonquest.betonquest.mc_1_20_6.BundledMC_1_20_6;
 import org.betonquest.betonquest.mc_1_21_4.BundledMC_1_21_4;
 
+import java.util.Objects;
 import java.util.Set;
 
 /**
@@ -44,13 +45,16 @@ public class BetonQuestPlugin extends BetonQuest {
             getServer().getPluginManager().disablePlugin(this);
             return;
         }
-        final Compatibility compatibility = getCompatibility();
-        compatibility.registerVanilla("1.20.6", () -> new BundledMC_1_20_6(this));
-        compatibility.registerVanilla("1.21.4", () -> new BundledMC_1_21_4(this));
-        BundledCompatibility.registerCompatiblePlugins(getLoggerFactory(), getLoggerFactory().create(BundledCompatibility.class),
-                compatibility, getBetonQuestApi().instructions(), getBetonQuestApi().identifiers(), getComponentLoader().get(GlobalData.class), getComponentLoader().get(PlaceholderProcessor.class), this);
-        compatibility.init();
-        getServer().getPluginManager().registerEvents(compatibility, this);
+
+        final IntegrationService integrationService = getServer().getServicesManager().load(IntegrationService.class);
+        Objects.requireNonNull(integrationService);
+        integrationService.withPolicies(Policies.minimalVanillaVersion("1.20.6")).register(this,
+                () -> new BundledMC_1_20_6(this));
+        integrationService.withPolicies(Policies.minimalVanillaVersion("1.21.4")).register(this,
+                () -> new BundledMC_1_21_4(this));
+        new BundledCompatibility(getLoggerFactory().create(BundledCompatibility.class), getPluginConfig(), this, integrationService)
+                .registerCompatiblePlugins(getLoggerFactory(), getServer().getServicesManager(),
+                        getBetonQuestApi().instructions(), getBetonQuestApi().identifiers(), getComponentLoader().get(PlaceholderProcessor.class));
     }
 
     private boolean isPaper() {
