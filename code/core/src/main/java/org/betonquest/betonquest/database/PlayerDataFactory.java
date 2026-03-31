6ea3653e1b18022442cf951664c223b25f20947f
Wolf2323 <gabrielpatrikurban@gmail.com>
2026-03-31 17:53:15 +0200

    Refactor `PlayerData` to use dependency injection for `Connector` and `ConfigAccessor` (#3896)

--- code/core/src/main/java/org/betonquest/betonquest/database/PlayerDataFactory.java
+++ code/core/src/main/java/org/betonquest/betonquest/database/PlayerDataFactory.java
@@ -1,5 +1,6 @@
 package org.betonquest.betonquest.database;
 
+import org.betonquest.betonquest.api.config.ConfigAccessor;
 import org.betonquest.betonquest.api.logger.BetonQuestLoggerFactory;
 import org.betonquest.betonquest.api.profile.Profile;
 import org.betonquest.betonquest.api.service.identifier.Identifiers;
@@ -30,6 +31,11 @@ public class PlayerDataFactory {
     private final Server server;
 
     /**
+     * The database connector.
+     */
+    private final Connector connector;
+
+    /**
      * Identifier registry to resolve identifiers.
      */
     private final Identifiers identifierRegistry;
@@ -38,6 +44,11 @@ public class PlayerDataFactory {
      * The objective manager.
      */
     private final ObjectiveManager objectiveManager;
+
+    /**
+     * The config accessor to get the plugin config.
+     */
+    private final ConfigAccessor config;
 
     /**
      * Factory to create a new Journal.
@@ -50,18 +61,23 @@ public class PlayerDataFactory {
      * @param loggerFactory      the logger factory to create class-specific logger
      * @param saver              the saver to persist data changes
      * @param server             the server to determine if an event should be stated as async
+     * @param connector          the database connector to use
      * @param identifierRegistry the identifier registry to resolve identifiers
      * @param objectiveManager   the objective manager
+     * @param config             the config accessor to get the plugin config
      * @param journalFactory     the supplier for the journal factory to use
      */
     public PlayerDataFactory(final BetonQuestLoggerFactory loggerFactory, final Saver saver, final Server server,
-                             final Identifiers identifierRegistry, final ObjectiveManager objectiveManager,
+                             final Connector connector, final Identifiers identifierRegistry,
+                             final ObjectiveManager objectiveManager, final ConfigAccessor config,
                              final Supplier<JournalFactory> journalFactory) {
+        this.connector = connector;
         this.identifierRegistry = identifierRegistry;
         this.loggerFactory = loggerFactory;
         this.saver = saver;
         this.server = server;
         this.objectiveManager = objectiveManager;
+        this.config = config;
         this.journalFactory = journalFactory;
     }
 
@@ -72,6 +88,7 @@ public class PlayerDataFactory {
      * @return the newly created player data
      */
     public PlayerData createPlayerData(final Profile profile) {
-        return new PlayerData(loggerFactory.create(PlayerData.class), saver, server, identifierRegistry, objectiveManager, journalFactory.get(), profile);
+        return new PlayerData(loggerFactory.create(PlayerData.class), saver, server, connector, identifierRegistry,
+                objectiveManager, config, journalFactory.get(), profile);
     }
 }
