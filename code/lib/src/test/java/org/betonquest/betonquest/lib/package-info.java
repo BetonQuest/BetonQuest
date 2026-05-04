f02d5e158e6a32512d6b6a8c02cce05b1afef2df
TheosRee <theosree@users.noreply.github.com>
2026-05-04 20:08:12 +0200

    add package-info to lib tests

---
+++ code/lib/src/test/java/org/betonquest/betonquest/lib/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * The test package for the lib module.
+ */
+@DefaultQualifier(value = NotNull.class, locations = {FIELD, PARAMETER, RETURN})
+package org.betonquest.betonquest.lib;
+
+import org.checkerframework.framework.qual.DefaultQualifier;
+import org.jetbrains.annotations.NotNull;
+
+import static org.checkerframework.framework.qual.TypeUseLocation.FIELD;
+import static org.checkerframework.framework.qual.TypeUseLocation.PARAMETER;
+import static org.checkerframework.framework.qual.TypeUseLocation.RETURN;
