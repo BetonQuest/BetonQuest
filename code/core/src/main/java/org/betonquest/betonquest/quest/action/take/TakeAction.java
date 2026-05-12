2157c49e0e841837a21a1026a5b75297fcac321a
TheosRee <theosree@users.noreply.github.com>
2026-05-12 23:24:38 +0200

    rework take action to only modify inventories when the content changes

--- code/core/src/main/java/org/betonquest/betonquest/quest/action/take/TakeAction.java
+++ code/core/src/main/java/org/betonquest/betonquest/quest/action/take/TakeAction.java
@@ -1,28 +1,42 @@
 package org.betonquest.betonquest.quest.action.take;
 
 import net.kyori.adventure.text.Component;
-import org.apache.commons.lang3.tuple.Pair;
 import org.betonquest.betonquest.api.QuestException;
 import org.betonquest.betonquest.api.common.component.VariableReplacement;
 import org.betonquest.betonquest.api.instruction.Argument;
 import org.betonquest.betonquest.api.instruction.type.ItemWrapper;
 import org.betonquest.betonquest.api.item.QuestItem;
 import org.betonquest.betonquest.api.profile.OnlineProfile;
-import org.betonquest.betonquest.api.profile.Profile;
+import org.betonquest.betonquest.api.quest.action.OnlineAction;
 import org.betonquest.betonquest.data.PlayerDataStorage;
 import org.betonquest.betonquest.quest.action.NotificationSender;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 /**
  * Removes items from player's inventory and/or backpack.
  */
-public class TakeAction extends AbstractTakeAction {
+public class TakeAction implements OnlineAction {
+
+    /**
+     * The storage for player data.
+     */
+    private final PlayerDataStorage playerDataStorage;
+
+    /**
+     * The notification sender to send notifications to the player.
+     */
+    private final NotificationSender notificationSender;
 
     /**
      * The items to be removed.
@@ -30,58 +44,201 @@ public class TakeAction extends AbstractTakeAction {
     private final Argument<List<ItemWrapper>> questItems;
 
     /**
-     * A map to keep track of the needed deletions for each player.
+     * The order in which the checks should be performed to remove the item.
      */
-    private final Map<UUID, Pair<QuestItem, Integer>> neededDeletions = new ConcurrentHashMap<>();
+    private final Argument<List<CheckType>> checkOrder;
 
     /**
      * Constructs a new TakeAction.
      *
-     * @param playerDataStorage  the player data storage
+     * @param playerDataStorage  the storage for player data
+     * @param notificationSender the notification sender to use
      * @param questItems         the items to be removed
      * @param checkOrder         the order in which the checks should be performed
-     * @param notificationSender the notification sender to use
      */
-    public TakeAction(final PlayerDataStorage playerDataStorage, final Argument<List<ItemWrapper>> questItems,
-                      final Argument<List<CheckType>> checkOrder, final NotificationSender notificationSender) {
-        super(playerDataStorage, checkOrder, notificationSender);
+    public TakeAction(final PlayerDataStorage playerDataStorage, final NotificationSender notificationSender,
+                      final Argument<List<ItemWrapper>> questItems, final Argument<List<CheckType>> checkOrder) {
+        this.playerDataStorage = playerDataStorage;
+        this.checkOrder = checkOrder;
+        this.notificationSender = notificationSender;
         this.questItems = questItems;
     }
 
     @Override
     public void execute(final OnlineProfile profile) throws QuestException {
+        final Map<QuestItem, Integer> neededDeletions = new HashMap<>();
         for (final ItemWrapper item : questItems.getValue(profile)) {
             final QuestItem questItem = item.getItem(profile);
             final int deleteAmount = item.getAmount().getValue(profile).intValue();
-            neededDeletions.put(profile.getProfileUUID(), Pair.of(questItem, deleteAmount));
-
-            checkSelectedTypes(profile);
+            neededDeletions.compute(questItem, (ignored, integer) -> (integer == null ? 0 : integer) + deleteAmount);
+        }
+        final Session session = new Session(profile, neededDeletions);
+        session.checkSelectedTypes();
+        neededDeletions.forEach((questItem, deleteAmount) -> {
+            final Integer notTaken = session.stillToTake.getOrDefault(questItem, 0);
             notificationSender.sendNotification(profile,
                     new VariableReplacement("item", questItem.getName()),
-                    new VariableReplacement("amount", Component.text(deleteAmount - neededDeletions.get(profile.getProfileUUID()).getRight())));
-        }
+                    new VariableReplacement("amount", Component.text(deleteAmount - notTaken)));
+        });
     }
 
-    @Override
-    protected ItemStack[] takeDesiredAmount(final Profile profile, final ItemStack... items) {
-        final Pair<QuestItem, Integer> pair = Objects.requireNonNull(neededDeletions.get(profile.getProfileUUID()));
-        final QuestItem questItem = pair.getLeft();
-        int desiredDeletions = pair.getRight();
+    /**
+     * Take session which stores changed inventories and items to still remove.
+     */
+    @SuppressWarnings("PMD.ReturnEmptyCollectionRatherThanNull")
+    private final class Session {
 
-        for (int i = 0; i < items.length && desiredDeletions > 0; i++) {
-            final ItemStack item = items[i];
-            if (item != null && questItem.matches(item)) {
-                if (item.getAmount() <= desiredDeletions) {
-                    items[i] = null;
-                    desiredDeletions = desiredDeletions - item.getAmount();
-                } else {
-                    item.setAmount(item.getAmount() - desiredDeletions);
-                    desiredDeletions = 0;
+        /**
+         * Profile of the player.
+         */
+        private final OnlineProfile profile;
+
+        /**
+         * The player to take the items from.
+         */
+        private final Player player;
+
+        /**
+         * Items which still need to be taken.
+         */
+        private final Map<QuestItem, Integer> stillToTake;
+
+        /**
+         * The inventory storage contents to set, if changed.
+         */
+        private ItemStack @Nullable [] newInventory;
+
+        /**
+         * The armor to set, if changed.
+         */
+        private ItemStack @Nullable [] newArmor;
+
+        /**
+         * The offhand, if changed.
+         */
+        private ItemStack @Nullable [] newOffhand;
+
+        /**
+         * The BetonQuest backpack, if changed.
+         */
+        private @Nullable List<ItemStack> newBackpack;
+
+        private Session(final OnlineProfile profile, final Map<QuestItem, Integer> neededDeletions) {
+            this.profile = profile;
+            this.player = profile.getPlayer();
+            this.stillToTake = new HashMap<>(neededDeletions);
+        }
+
+        private void checkSelectedTypes() throws QuestException {
+            for (final CheckType type : checkOrder.getValue(profile)) {
+                switch (type) {
+                    case INVENTORY -> checkInventory();
+                    case ARMOR -> newArmor = takeDesiredAmount(player.getInventory().getArmorContents());
+                    case MAINHAND -> checkMainHandEquipmentSlot();
+                    case OFFHAND -> newOffhand = takeDesiredAmount(player.getInventory().getItemInOffHand());
+                    case BACKPACK -> newBackpack = takeDesiredAmount(playerDataStorage.get(profile).getBackpack());
                 }
+                if (stillToTake.isEmpty()) {
+                    break;
+                }
+            }
+
+            setNewContents();
+        }
+
+        private void setNewContents() {
+            if (newInventory != null) {
+                player.getInventory().setStorageContents(newInventory);
+            }
+            if (newOffhand != null) {
+                player.getInventory().setItemInOffHand(newOffhand[0]);
+            }
+            if (newArmor != null) {
+                player.getInventory().setArmorContents(newArmor);
+            }
+            if (newBackpack != null) {
+                playerDataStorage.get(profile).setBackpack(newBackpack);
             }
         }
 
-        neededDeletions.put(profile.getProfileUUID(), Pair.of(questItem, desiredDeletions));
-        return items;
+        /**
+         * Removes the items from the general inventory with respect to changes on the main hand.
+         */
+        private void checkInventory() {
+            final ItemStack[] inventory = newInventory == null ? player.getInventory().getStorageContents() : newInventory;
+            final ItemStack[] changed = takeDesiredAmount(inventory);
+            if (changed != null) {
+                newInventory = changed;
+            }
+        }
+
+        /**
+         * Removes the items from the main hand with respect to changes on the general inventory.
+         */
+        private void checkMainHandEquipmentSlot() {
+            final ItemStack[] inventory = newInventory == null ? player.getInventory().getStorageContents() : newInventory;
+            final int heldItemSlot = player.getInventory().getHeldItemSlot();
+            final ItemStack[] changed = takeDesiredAmount(inventory[heldItemSlot]);
+            if (changed != null) {
+                if (newInventory == null) {
+                    newInventory = inventory;
+                }
+                newInventory[heldItemSlot] = changed[0];
+            }
+        }
+
+        /**
+         * List to Array (and reverse) conversion for usage with the {@link #takeDesiredAmount(ItemStack...)} method.
+         */
+        @Nullable
+        private List<ItemStack> takeDesiredAmount(final List<ItemStack> items) {
+            final ItemStack[] itemArray = items.toArray(new ItemStack[0]);
+            final ItemStack[] remainingItems = takeDesiredAmount(itemArray);
+            if (remainingItems == null) {
+                return null;
+            }
+            return Arrays.stream(remainingItems)
+                    .filter(Objects::nonNull)
+                    .collect(Collectors.toCollection(ArrayList::new));
+        }
+
+        /**
+         * Method to take the desired amount of items.
+         *
+         * @param items the items to take from
+         * @return the remaining items after taking the desired amount, or null if no item was taken at all
+         */
+        @SuppressWarnings("PMD.CognitiveComplexity")
+        private ItemStack @Nullable [] takeDesiredAmount(final ItemStack... items) {
+            boolean changed = false;
+            final Iterator<Map.Entry<QuestItem, Integer>> iterator = stillToTake.entrySet().iterator();
+            while (iterator.hasNext()) {
+                final Map.Entry<QuestItem, Integer> entry = iterator.next();
+                final QuestItem questItem = entry.getKey();
+                int desiredDeletions = entry.getValue();
+
+                for (int i = 0; i < items.length && desiredDeletions > 0; i++) {
+                    final ItemStack item = items[i];
+                    if (item != null && questItem.matches(item)) {
+                        changed = true;
+                        if (item.getAmount() <= desiredDeletions) {
+                            items[i] = null;
+                            desiredDeletions = desiredDeletions - item.getAmount();
+                        } else {
+                            item.setAmount(item.getAmount() - desiredDeletions);
+                            desiredDeletions = 0;
+                        }
+                    }
+                }
+                entry.setValue(desiredDeletions);
+                if (desiredDeletions <= 0) {
+                    iterator.remove();
+                }
+            }
+            if (changed) {
+                return items;
+            }
+            return null;
+        }
     }
 }
