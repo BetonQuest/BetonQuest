22b3c9b49767d00739b9ec006173ee81a7dc4f89
Hjalmar Gunnarsson <officialhjalmar.gunnarsson@outlook.com>
2026-06-06 09:47:52 +0200

    feat(integration): TheBrewingProject integration

---
+++ code/compatibility/src/main/java/org/betonquest/betonquest/compatibility/tbp/TbpIntegrator.java
@@ -0,0 +1,43 @@
+package org.betonquest.betonquest.compatibility.tbp;
+
+import dev.jsinco.brewery.bukkit.api.TheBrewingProjectApi;
+import org.betonquest.betonquest.api.BetonQuestApi;
+import org.betonquest.betonquest.compatibility.tbp.action.DrunkenEventActionFactory;
+import org.betonquest.betonquest.compatibility.tbp.condition.ModifierCondition;
+import org.betonquest.betonquest.compatibility.tbp.item.TbpItemFactory;
+import org.betonquest.betonquest.compatibility.tbp.item.TbpItemSerializer;
+import org.betonquest.betonquest.lib.integration.IntegrationTemplate;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.RegisteredServiceProvider;
+
+public class TbpIntegrator extends IntegrationTemplate {
+
+    /**
+     * The minimum required version of TheBrewingProject.
+     */
+    public static final String REQUIRED_VERSION = "3.2.0";
+
+    @Override
+    public void enable(final BetonQuestApi api) {
+        RegisteredServiceProvider<TheBrewingProjectApi> tbpProvider = Bukkit.getServicesManager().getRegistration(TheBrewingProjectApi.class);
+        if (tbpProvider == null) {
+            return;
+        }
+        TheBrewingProjectApi tbpApi = tbpProvider.getProvider();
+        playerCondition("drunken_modifier", new ModifierCondition(tbpApi));
+        playerAction("drunken_event", new DrunkenEventActionFactory(tbpApi));
+        item("brew", new TbpItemFactory(tbpApi), new TbpItemSerializer());
+
+        registerFeatures(api);
+    }
+
+    @Override
+    public void postEnable(final BetonQuestApi api) {
+        // NO-OP
+    }
+
+    @Override
+    public void disable() {
+        // NO-OP
+    }
+}
---
+++ code/compatibility/src/main/java/org/betonquest/betonquest/compatibility/tbp/item/TbpItemSerializer.java
@@ -0,0 +1,48 @@
+package org.betonquest.betonquest.compatibility.tbp.item;
+
+import dev.jsinco.brewery.api.brew.BrewQuality;
+import org.betonquest.betonquest.api.QuestException;
+import org.betonquest.betonquest.api.item.QuestItemSerializer;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Locale;
+
+public class TbpItemSerializer implements QuestItemSerializer {
+
+    @Override
+    public String serialize(final ItemStack itemStack) throws QuestException {
+        final PersistentDataContainer container = itemStack.getItemMeta().getPersistentDataContainer();
+        final String recipeName = container.get(new NamespacedKey("brewery", "tag"), PersistentDataType.STRING);
+        if (recipeName == null) {
+            throw new QuestException("Item not completed brew");
+        }
+        final Double score = container.get(new NamespacedKey("brewery", "score"), PersistentDataType.DOUBLE);
+        final BrewQuality quality;
+        if (score == null) {
+            quality = BrewQuality.EXCELLENT;
+        } else {
+            quality = quality(score);
+        }
+        if (quality == null) {
+            throw new QuestException("Brew is failed");
+        }
+        return String.format("\"%s\" %s", recipeName, quality.name().toLowerCase(Locale.ROOT));
+    }
+
+    private @Nullable BrewQuality quality(double score) {
+        if (score >= 0.8) {
+            return BrewQuality.EXCELLENT;
+        }
+        if (score >= 0.6) {
+            return BrewQuality.GOOD;
+        }
+        if (score > 0) {
+            return BrewQuality.BAD;
+        }
+        return null;
+    }
+}
