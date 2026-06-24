770a20563570de411d65e5a9336ac113a82091af
Hjalmar Gunnarsson <officialhjalmar.gunnarsson@outlook.com>
2026-06-24 19:52:34 +0200

    feat(integration): Make brew item quality optional

--- code/compatibility/src/main/java/org/betonquest/betonquest/compatibility/thebrewingproject/item/BrewItem.java
+++ code/compatibility/src/main/java/org/betonquest/betonquest/compatibility/thebrewingproject/item/BrewItem.java
@@ -4,6 +4,7 @@ import dev.jsinco.brewery.api.brew.Brew;
 import dev.jsinco.brewery.api.brew.BrewManager;
 import dev.jsinco.brewery.api.brew.BrewQuality;
 import dev.jsinco.brewery.api.recipe.Recipe;
+import dev.jsinco.brewery.api.recipe.RecipeMatcherResult;
 import net.kyori.adventure.text.Component;
 import org.betonquest.betonquest.api.QuestException;
 import org.betonquest.betonquest.api.item.QuestItem;
@@ -16,40 +17,49 @@ import java.util.List;
 /**
  * A TheBrewingProject brew item.
  *
- * @param recipe     the recipe this brew item completes
- * @param recipeName the name of the recipe
- * @param quality    the brew quality
- * @param api        the api needed to craft brew
+ * @param recipe      the recipe this brew item completes
+ * @param recipeName  the name of the recipe
+ * @param quality     the brew quality, or null if not specified
+ * @param brewManager the brew manager provided by TheBrewingProject
  */
-public record BrewItem(Recipe<ItemStack> recipe, String recipeName, BrewQuality quality,
-                       BrewManager<ItemStack> api) implements QuestItem {
+public record BrewItem(Recipe<ItemStack> recipe, String recipeName, @Nullable BrewQuality quality,
+                       BrewManager<ItemStack> brewManager) implements QuestItem {
 
     @Override
     public Component getName() {
-        return recipe.getRecipeResult(quality).displayName();
+        return recipe.getRecipeResult(quality == null ? BrewQuality.EXCELLENT : quality).displayName();
     }
 
     @Override
     public List<Component> getLore() {
-        return recipe.getRecipeResult(quality).staticLore();
+        return recipe.getRecipeResult(quality == null ? BrewQuality.EXCELLENT : quality).staticLore();
     }
 
     @Override
     public ItemStack generate(final int stackSize, @Nullable final Profile profile) throws QuestException {
-        final ItemStack output = api.matcher()
+        final RecipeMatcherResult<ItemStack> match = brewManager.matcher()
                 .matchAgainstOnly(recipe)
                 .disallowStepVariations()
                 .build()
-                .match(api.createBrew(recipe.getSteps()))
-                .toItem(new Brew.State.Seal(null));
+                .match(brewManager.createBrew(recipe.getSteps()));
+        final ItemStack output;
+        if (quality == null) {
+            output = match.toItem(new Brew.State.Seal(null));
+        } else {
+            output = match.toItem(new Brew.State.Seal(null), quality);
+        }
         output.setAmount(stackSize);
         return output;
     }
 
     @Override
     public boolean matches(@Nullable final ItemStack item) {
-        return item != null && api.brewName(item)
-                .filter(recipeName::equals)
-                .isPresent();
+        if (item == null) {
+            return false;
+        }
+        if (quality != null && brewManager.brewQuality(item).filter(quality::equals).isEmpty()) {
+            return false;
+        }
+        return brewManager.brewName(item).filter(recipeName::equals).isPresent();
     }
 }
