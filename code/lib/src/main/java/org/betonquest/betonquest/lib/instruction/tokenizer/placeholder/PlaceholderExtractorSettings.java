2926b7fda3077f5a7c301f28dfdf0a3057265571
Juyas <juyasetgames@gmail.com>
2026-07-31 22:55:21 +0200

    Extend `PlaceholderExtractor` settings for parsing non-placeholder words and integrate with `DefaultArgument` to parse placeholders correctly

--- code/lib/src/main/java/org/betonquest/betonquest/lib/instruction/tokenizer/placeholder/PlaceholderExtractorSettings.java
+++ code/lib/src/main/java/org/betonquest/betonquest/lib/instruction/tokenizer/placeholder/PlaceholderExtractorSettings.java
@@ -3,16 +3,22 @@ package org.betonquest.betonquest.lib.instruction.tokenizer.placeholder;
 /**
  * Settings for the placeholder extractor.
  *
- * @param placeholderBrackets the brackets that surround the placeholder
- * @param escapeCharacter     the escape character
- * @param openQuote           the opening quote character
- * @param closeQuote          the closing quote character
+ * @param placeholderBrackets      the brackets that surround the placeholder
+ * @param escapeCharacter          the escape character
+ * @param openQuote                the opening quote character
+ * @param closeQuote               the closing quote character
+ * @param parseNonPlaceholderWords if non-placeholder words should be parsed
  */
 public record PlaceholderExtractorSettings(char placeholderBrackets, char escapeCharacter, char openQuote,
-                                           char closeQuote) {
+                                           char closeQuote, boolean parseNonPlaceholderWords) {
 
     /**
      * The default settings for the placeholder extractor.
      */
-    public static final PlaceholderExtractorSettings DEFAULT = new PlaceholderExtractorSettings('%', '\\', '{', '}');
+    public static final PlaceholderExtractorSettings DEFAULT = new PlaceholderExtractorSettings('%', '\\', '{', '}', true);
+
+    /**
+     * The settings for the placeholder extractor that only extracts placeholders.
+     */
+    public static final PlaceholderExtractorSettings ONLY_PLACEHOLDERS = new PlaceholderExtractorSettings('%', '\\', '{', '}', false);
 }
--- code/lib/src/test/java/org/betonquest/betonquest/lib/instruction/tokenizer/placeholder/PlaceholderExtractorTest.java
+++ code/lib/src/test/java/org/betonquest/betonquest/lib/instruction/tokenizer/placeholder/PlaceholderExtractorTest.java
@@ -16,14 +16,23 @@ import static org.junit.jupiter.api.Assertions.*;
 class PlaceholderExtractorTest {
 
     private static Stream<Arguments> validInstructions() {
+        final PlaceholderExtractorSettings only = PlaceholderExtractorSettings.ONLY_PLACEHOLDERS;
+        final PlaceholderExtractorSettings def = PlaceholderExtractorSettings.DEFAULT;
         return Stream.of(
-                Arguments.of("Simple! This is text with a %placeholder.value% and suffix", List.of("%placeholder.value%")),
-                Arguments.of("Quoting! %ph.{%sub.value%}.extra% is a test.", List.of("%ph.{%sub.value%}.extra%")),
-                Arguments.of("Escaping! %some.\\{in brackets\\}% is a test.", List.of("%some.\\{in brackets\\}%")),
-                Arguments.of("Ignoring! This contains no {placeholder} 100\\%!", List.of()),
-                Arguments.of("Nesting! %some.{nested {%deeply.{even more}.{and {more}%}}}% and that's a test.", List.of("%some.{nested {%deeply.{even more}.{and {more}%}}}%")),
-                Arguments.of("Multiple! %one% and %two% and %more.complex.{with quote}%", List.of("%one%", "%two%", "%more.complex.{with quote}%")),
-                Arguments.of("Complex! %one%\\% and %two%\\% = {%more.complex.{with quote}%}", List.of("%one%", "%two%", "%more.complex.{with quote}%"))
+                Arguments.of(only, "Simple! This is text with a %placeholder.value% and suffix", List.of("%placeholder.value%")),
+                Arguments.of(def, "Simple! This is text with a %placeholder.value% and suffix", List.of("Simple! This is text with a ", "%placeholder.value%", " and suffix")),
+                Arguments.of(only, "Quoting! %ph.{%sub.value%}.extra% is a test.", List.of("%ph.{%sub.value%}.extra%")),
+                Arguments.of(def, "Quoting! %ph.{%sub.value%}.extra% is a test.", List.of("Quoting! ", "%ph.{%sub.value%}.extra%", " is a test.")),
+                Arguments.of(only, "Escaping! %some.\\{in brackets\\}% is a test.", List.of("%some.\\{in brackets\\}%")),
+                Arguments.of(def, "Escaping! %some.\\{in brackets\\}% is a test.", List.of("Escaping! ", "%some.\\{in brackets\\}%", " is a test.")),
+                Arguments.of(only, "Ignoring! This contains no {placeholder} 100\\%!", List.of()),
+                Arguments.of(def, "Ignoring! This contains no {placeholder} 100\\%!", List.of("Ignoring! This contains no {placeholder} 100\\%!")),
+                Arguments.of(only, "Nesting! %some.{nested {%deeply.{even more}.{and {more}%}}}% and that's a test.", List.of("%some.{nested {%deeply.{even more}.{and {more}%}}}%")),
+                Arguments.of(def, "Nesting! %some.{nested {%deeply.{even more}.{and {more}%}}}% and that's a test.", List.of("Nesting! ", "%some.{nested {%deeply.{even more}.{and {more}%}}}%", " and that's a test.")),
+                Arguments.of(only, "Multiple! %one% and %two% and %more.complex.{with quote}%", List.of("%one%", "%two%", "%more.complex.{with quote}%")),
+                Arguments.of(def, "Multiple! %one% and %two% and %more.complex.{with quote}%", List.of("Multiple! ", "%one%", " and ", "%two%", " and ", "%more.complex.{with quote}%")),
+                Arguments.of(only, "Complex! %one%\\% and %two%\\% = {%more.complex.{with quote}%}", List.of("%one%", "%two%", "%more.complex.{with quote}%")),
+                Arguments.of(def, "Complex! %one%\\% and %two%\\% = {%more.complex.{with quote}%}", List.of("Complex! ", "%one%", "\\% and ", "%two%", "\\% = {", "%more.complex.{with quote}%", "}"))
         );
     }
 
@@ -39,8 +48,8 @@ class PlaceholderExtractorTest {
 
     @ParameterizedTest
     @MethodSource("validInstructions")
-    void strings_are_tokenized_correctly(final String instruction, final List<String> expected) throws TokenizerException {
-        final Tokenizer tokenizer = new PlaceholderExtractor();
+    void strings_are_tokenized_correctly(final PlaceholderExtractorSettings settings, final String instruction, final List<String> expected) throws TokenizerException {
+        final Tokenizer tokenizer = new PlaceholderExtractor(settings);
         final Token[] parsed = tokenizer.tokens(instruction);
 
         final String[] mappedParsed = Stream.of(parsed).map(Token::value).toArray(String[]::new);
@@ -51,7 +60,7 @@ class PlaceholderExtractorTest {
     @ParameterizedTest
     @MethodSource("invalidInstructions")
     void invalid_strings_throw_tokenizer_exception(final String instruction) {
-        final Tokenizer tokenizer = new PlaceholderExtractor();
+        final Tokenizer tokenizer = new PlaceholderExtractor(PlaceholderExtractorSettings.ONLY_PLACEHOLDERS);
         assertThrows(TokenizerException.class, () -> tokenizer.tokens(instruction), "Expected tokenizing to fail for instruction: " + instruction);
     }
 }
