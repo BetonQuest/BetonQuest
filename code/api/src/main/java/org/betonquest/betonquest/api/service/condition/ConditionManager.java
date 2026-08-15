f12374e73fbc17672cbae8a5e84f09bfd94fbbec
TheosRee <theosree@users.noreply.github.com>
2026-08-15 04:17:11 +0200

    move test strategy to own file

--- code/api/src/main/java/org/betonquest/betonquest/api/service/condition/ConditionManager.java
+++ code/api/src/main/java/org/betonquest/betonquest/api/service/condition/ConditionManager.java
@@ -6,7 +6,6 @@ import org.betonquest.betonquest.api.profile.Profile;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
-import java.util.Optional;
 
 /**
  * The ConditionManager is responsible for evaluating conditions that are loaded by BetonQuest.
@@ -108,31 +107,4 @@ public interface ConditionManager {
      * @since 3.2.0
      */
     boolean test(@Nullable Profile profile, Collection<ConditionIdentifier> conditionIdentifiers, TestStrategy testStrategy);
-
-    /**
-     * Strategy to check if a certain combination of condition evaluations is valid.
-     *
-     * @since 3.2.0
-     */
-    @FunctionalInterface
-    interface TestStrategy {
-
-        /**
-         * Checks the given condition evaluations to determine a combined result.
-         * <br> <br>
-         * If the result is definite, an optional with the result should be returned.
-         * After that no more condition may be evaluated.
-         * <br>
-         * If the result is unclear, an empty optional should be returned.
-         * <br> <br>
-         * If {@code remaining} is {@code 0} and there is no definite result, it should return {@code false}.
-         *
-         * @param positive  the amount of conditions which evaluated to {@code true}
-         * @param negative  the amount of conditions which evaluated to {@code false}
-         * @param remaining the amount of conditions which have not been evaluated yet
-         * @return the definite result of the test, or an empty optional if it is indefinite
-         * @since 3.2.0
-         */
-        Optional<Boolean> getResult(int positive, int negative, int remaining);
-    }
 }
---
+++ code/api/src/main/java/org/betonquest/betonquest/api/service/condition/TestStrategy.java
@@ -0,0 +1,31 @@
+package org.betonquest.betonquest.api.service.condition;
+
+import java.util.Optional;
+
+/**
+ * Strategy to check if a certain combination of condition evaluations is valid.
+ *
+ * @since 3.2.0
+ */
+@FunctionalInterface
+public
+interface TestStrategy {
+
+    /**
+     * Checks the given condition evaluations to determine a combined result.
+     * <br> <br>
+     * If the result is definite, an optional with the result should be returned.
+     * After that no more condition may be evaluated.
+     * <br>
+     * If the result is unclear, an empty optional should be returned.
+     * <br> <br>
+     * If {@code remaining} is {@code 0} and there is no definite result, it should return {@code false}.
+     *
+     * @param positive  the amount of conditions which evaluated to {@code true}
+     * @param negative  the amount of conditions which evaluated to {@code false}
+     * @param remaining the amount of conditions which have not been evaluated yet
+     * @return the definite result of the test, or an empty optional if it is indefinite
+     * @since 3.2.0
+     */
+    Optional<Boolean> getResult(int positive, int negative, int remaining);
+}
