689333443cf7aa7d6a62da7a56bae4a769a8da60
TheosRee <theosree@users.noreply.github.com>
2026-08-21 19:03:29 +0200

    UpdatedCustomModelDataHandler

--- code/mc_1_21_4/src/main/java/org/betonquest/betonquest/mc_1_21_4/item/UpdatedCustomModelDataHandler.java
+++ code/mc_1_21_4/src/main/java/org/betonquest/betonquest/mc_1_21_4/item/UpdatedCustomModelDataHandler.java
@@ -6,11 +6,16 @@ import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.util.Strings;
 import org.betonquest.betonquest.api.QuestException;
 import org.betonquest.betonquest.api.instruction.Argument;
+import org.betonquest.betonquest.api.instruction.FlagArgument;
+import org.betonquest.betonquest.api.instruction.FlagState;
 import org.betonquest.betonquest.api.instruction.Instruction;
 import org.betonquest.betonquest.api.instruction.argument.parser.BooleanParser;
+import org.betonquest.betonquest.api.profile.Profile;
 import org.betonquest.betonquest.item.typehandler.Existence;
 import org.betonquest.betonquest.item.typehandler.ExistenceArgument;
 import org.betonquest.betonquest.item.typehandler.ItemMetaHandler;
+import org.betonquest.betonquest.lib.instruction.argument.DefaultArgument;
+import org.betonquest.betonquest.lib.instruction.argument.DefaultFlagArgument;
 import org.bukkit.Color;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -32,34 +37,19 @@ import java.util.stream.Collectors;
 public class UpdatedCustomModelDataHandler implements ItemMetaHandler<ItemMeta> {
 
     /**
-     * The required custom model data existence.
+     * If 'item_model' is forbidden.
      */
-    private Existence existence = Existence.WHATEVER;
+    private FlagArgument<Boolean> noData = new DefaultFlagArgument<>(false, FlagState.UNDEFINED);
 
     /**
-     * The CustomModelData floats.
+     * The CustomModelData with existence.
      */
-    private List<Float> floats = List.of();
-
-    /**
-     * The CustomModelData flags.
-     */
-    private List<Boolean> flags = List.of();
-
-    /**
-     * The CustomModelData strings.
-     */
-    private List<String> strings = List.of();
-
-    /**
-     * The CustomModelData colors.
-     */
-    private List<Color> colors = List.of();
+    private Argument<CustomModelData> data = new DefaultArgument<>(new CustomModelData());
 
     /**
      * The required 'item_model' existence.
      */
-    private Existence modelE = Existence.WHATEVER;
+    private FlagArgument<Boolean> noModel = new DefaultFlagArgument<>(false, FlagState.UNDEFINED);
 
     /**
      * The 'item_model' set.
@@ -105,114 +95,138 @@ public class UpdatedCustomModelDataHandler implements ItemMetaHandler<ItemMeta> 
     }
 
     @Override
-    @SuppressWarnings("PMD.CyclomaticComplexity")
-    public void set(Instruction instruction) throws QuestException {
-        this.model = ExistenceArgument.apply("item-model", instruction.namespacedKey())
-        switch (key) {
-            case "custom-model-data" -> {
-                this.existence = Existence.REQUIRED;
-                try {
-                    setCmd(data);
-                } catch (final QuestException e) {
-                    throw new QuestException("Could not parse custom-model-data '" + data + "': " + e.getMessage(), e);
-                }
+    public void set(final Instruction instruction) throws QuestException {
+        this.data = instruction.parse(resolvedString -> {
+            try {
+                return CustomModelData.parseCmd(resolvedString);
+            } catch (final QuestException e) {
+                throw new QuestException("Could not parse custom-model-data '" + data + "': " + e.getMessage(), e);
             }
-            case "no-custom-model-data" -> this.existence = Existence.FORBIDDEN;
-            case "item-model" -> {
-                this.modelE = Existence.REQUIRED;
-                this.model = NamespacedKey.fromString(data);
-                if (this.model == null) {
-                    throw new QuestException("The item-model '%s' could not be parsed!".formatted(data));
-                }
-            }
-            case "no-item-model" -> this.modelE = Existence.FORBIDDEN;
-            default -> throw new QuestException("Unknown custom model data key: " + key);
-        }
+        }).get("custom-model-data", new CustomModelData());
+        this.noData = instruction.bool().getFlag("no-custom-model-data", true);
+        this.model = ExistenceArgument.apply("item-model", instruction.namespacedKey());
+        this.noModel = instruction.bool().getFlag("no-item-model", true);
     }
 
     @Override
-    public void populate(final ItemMeta meta) {
-        if (existence == Existence.REQUIRED) {
+    public void populate(final ItemMeta meta, @Nullable final Profile profile) throws QuestException {
+        final CustomModelData data = this.data.getValue(profile);
+        if (data.existence() == Existence.REQUIRED) {
             final CustomModelDataComponent cmd = meta.getCustomModelDataComponent();
-            cmd.setFloats(floats);
-            cmd.setFlags(flags);
-            cmd.setStrings(strings);
-            cmd.setColors(colors);
+            cmd.setFloats(data.floats());
+            cmd.setFlags(data.flags());
+            cmd.setStrings(data.strings());
+            cmd.setColors(data.colors());
             meta.setCustomModelDataComponent(cmd);
         }
-        if (modelE == Existence.REQUIRED) {
-            meta.setItemModel(model);
+        final Pair<Existence, @Nullable NamespacedKey> model = this.model.getValue(profile);
+        if (model.getLeft() == Existence.REQUIRED) {
+            meta.setItemModel(model.getRight());
         }
     }
 
     @Override
-    public boolean check(final ItemMeta data) {
-        if (!(modelE == Existence.WHATEVER
-                || modelE == Existence.FORBIDDEN && !data.hasItemModel()
-                || modelE == Existence.REQUIRED && data.hasItemModel() && Objects.equals(model, data.getItemModel()))) {
+    public boolean check(final ItemMeta meta, @Nullable final Profile profile) throws QuestException {
+        if (noModel.getValue(profile).orElse(false) && !meta.hasItemModel()) {
             return false;
         }
-        return existence == Existence.WHATEVER
-                || existence == Existence.FORBIDDEN && checkEmpty(data.getCustomModelDataComponent())
-                || existence == Existence.REQUIRED && check(data.getCustomModelDataComponent());
-    }
-
-    private boolean checkEmpty(final CustomModelDataComponent cmd) {
-        return cmd.getFloats().isEmpty() && cmd.getFlags().isEmpty() && cmd.getStrings().isEmpty() && cmd.getColors().isEmpty();
-    }
-
-    private boolean check(final CustomModelDataComponent cmd) {
-        return floats.equals(cmd.getFloats()) && flags.equals(cmd.getFlags()) && strings.equals(cmd.getStrings())
-                && checkColors(cmd);
+        final Pair<Existence, @Nullable NamespacedKey> pair = model.getValue(profile);
+        final Existence modelE = pair.getLeft();
+        if (!(modelE == Existence.WHATEVER
+                || modelE == Existence.FORBIDDEN && !meta.hasItemModel()
+                || modelE == Existence.REQUIRED && meta.hasItemModel() && Objects.equals(pair.getRight(), meta.getItemModel()))) {
+            return false;
+        }
+        final CustomModelData data = this.data.getValue(profile);
+        return data.check(meta, noData.getValue(profile).orElse(false));
     }
 
     /**
-     * The color gets modified inside the component (alpha channel) so it is not equal to the actual given color.
+     * The resolved custom-model-data argument.
+     *
+     * @param existence The required custom model data existence.
+     * @param floats    The CustomModelData floats.
+     * @param flags     The CustomModelData flags.
+     * @param strings   The CustomModelData strings.
+     * @param colors    The CustomModelData colors.
      */
-    private boolean checkColors(final CustomModelDataComponent cmd) {
-        final List<Color> current = cmd.getColors();
-        cmd.setColors(this.colors);
-        final List<Color> wanted = cmd.getColors();
-        return wanted.equals(current);
-    }
+    private record CustomModelData(Existence existence, List<Float> floats, List<Boolean> flags,
+                                   List<String> strings, List<Color> colors) {
 
-    @SuppressFBWarnings("SF_SWITCH_FALLTHROUGH")
-    @SuppressWarnings({"PMD.ImplicitSwitchFallThrough", "PMD.CyclomaticComplexity"})
-    private void setCmd(final String data) throws QuestException {
-        final String[] split = data.split(";", -1);
-        switch (split.length) {
-            case 4:
-                final String[] colors = StringUtils.split(split[3], ",");
-                this.colors = new ArrayList<>(colors.length);
-                for (final String part : colors) {
-                    try {
-                        this.colors.add(Color.fromARGB(Integer.parseUnsignedInt(part, 16)));
-                    } catch (final IllegalArgumentException e) {
-                        throw new QuestException("Invalid color: " + part, e);
+        private CustomModelData() {
+            this(Existence.WHATEVER, List.of(), List.of(), List.of(), List.of());
+        }
+
+        @SuppressFBWarnings("SF_SWITCH_FALLTHROUGH")
+        @SuppressWarnings({"PMD.ImplicitSwitchFallThrough", "PMD.CyclomaticComplexity"})
+        private static CustomModelData parseCmd(final String data) throws QuestException {
+            final List<Float> floatList;
+            List<Boolean> flagList = List.of();
+            List<String> stringList = List.of();
+            List<Color> colorList = List.of();
+            final String[] split = data.split(";", -1);
+            switch (split.length) {
+                case 4:
+                    final String[] colors = StringUtils.split(split[3], ",");
+                    colorList = new ArrayList<>(colors.length);
+                    for (final String part : colors) {
+                        try {
+                            colorList.add(Color.fromARGB(Integer.parseUnsignedInt(part, 16)));
+                        } catch (final IllegalArgumentException e) {
+                            throw new QuestException("Invalid color: " + part, e);
+                        }
                     }
-                }
-            case 3:
-                this.strings = Arrays.asList(StringUtils.split(split[2], ","));
-            case 2:
-                final String[] booleans = StringUtils.split(split[1], ",");
-                this.flags = new ArrayList<>(booleans.length);
-                final BooleanParser booleanParser = new BooleanParser();
-                for (final String part : booleans) {
-                    this.flags.add(booleanParser.apply(part));
-                }
-            case 1:
-                final String[] floats = StringUtils.split(split[0], ",");
-                this.floats = new ArrayList<>(floats.length);
-                for (final String part : floats) {
-                    try {
-                        this.floats.add(Float.parseFloat(part));
-                    } catch (final NumberFormatException e) {
-                        throw new QuestException("Could not parse number: " + part, e);
+                case 3:
+                    stringList = Arrays.asList(StringUtils.split(split[2], ","));
+                case 2:
+                    final String[] booleans = StringUtils.split(split[1], ",");
+                    flagList = new ArrayList<>(booleans.length);
+                    final BooleanParser booleanParser = new BooleanParser();
+                    for (final String part : booleans) {
+                        flagList.add(booleanParser.apply(part));
                     }
-                }
-                break;
-            default:
-                throw new QuestException("Invalid length: " + split.length);
+                case 1:
+                    final String[] floats = StringUtils.split(split[0], ",");
+                    floatList = new ArrayList<>(floats.length);
+                    for (final String part : floats) {
+                        try {
+                            floatList.add(Float.parseFloat(part));
+                        } catch (final NumberFormatException e) {
+                            throw new QuestException("Could not parse number: " + part, e);
+                        }
+                    }
+                    return new CustomModelData(Existence.REQUIRED, floatList, flagList, stringList, colorList);
+                default:
+                    throw new QuestException("Invalid length: " + split.length);
+            }
+        }
+
+        private boolean check(final ItemMeta meta, final boolean noData) {
+            if (noData) {
+                return checkEmpty(meta.getCustomModelDataComponent());
+            }
+            return existence == Existence.WHATEVER
+                    || existence == Existence.FORBIDDEN && checkEmpty(meta.getCustomModelDataComponent())
+                    || existence == Existence.REQUIRED && checkRequired(meta.getCustomModelDataComponent());
+        }
+
+        private boolean checkEmpty(final CustomModelDataComponent cmd) {
+            return cmd.getFloats().isEmpty() && cmd.getFlags().isEmpty() && cmd.getStrings().isEmpty() && cmd.getColors().isEmpty();
+        }
+
+        private boolean checkRequired(final CustomModelDataComponent cmd) {
+            return floats.equals(cmd.getFloats()) && flags.equals(cmd.getFlags()) && strings.equals(cmd.getStrings())
+                    && checkColors(cmd);
+        }
+
+        /**
+         * The color gets modified inside the component (alpha channel) so it is not equal to the actual given color.
+         */
+        private boolean checkColors(final CustomModelDataComponent cmd) {
+            final List<Color> current = cmd.getColors();
+            cmd.setColors(this.colors);
+            final List<Color> wanted = cmd.getColors();
+            return wanted.equals(current);
         }
     }
 }
